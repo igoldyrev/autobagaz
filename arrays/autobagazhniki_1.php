70893637679bd51e7cce7fe2f0547d8015f10b85
igoldyrev <goldirev12@gmail.com>
2017-08-24 22:17:04 +0500

    Отдельный фпйл с массивом данных для страницы Автобагажники. Предназначается в будущем для редактирования остальными членами команды

---
+++ arrays/autobagazhniki_1.php
@@ -0,0 +1,34 @@
+<?php
+$autobagazhniki = [
+		[
+		'id' => '1',
+		'name' => 'Багажник для рейлингов',
+		'desc' => 'Рейлинги - продольные направляющие вдоль кузова автомобиля. Багажник для них представляет собой две поперечные дуги, которые крепятся с помощью зажимов с регулируемой шириной захвата.',
+		'img1' => '<img class="img_main" src="/images/autobagazhniki/1.jpg" srcset="/images/autobagazhniki/1.jpg 200w" alt="багажник для рейлингов" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'price' => 'Цена от 1500 рублей',
+		],
+		[
+		'id' => '2',
+		'name' => 'Багажник в штатные места',
+		'desc' => 'Такой багажник крепится в специально разработанные производителем посадочные места, как правило под резьбу М6-М8',
+		'img1' => '<img class="img_main" src="/images/autobagazhniki/2.jpg" srcset="/images/autobagazhniki/2.jpg 200w" alt="багажник в штатные места" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'img2' => '<img class="img_main" src="/images/autobagazhniki/3.jpg" srcset="/images/autobagazhniki/3.jpg 200w" alt="багажник в штатные места" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'price' => 'Цена от 1800 рублей',
+		],
+		[
+		'id' => '3',
+		'name' => 'Багажник на плоскую крышу',
+		'desc' => 'Данный вид багажников крепится за проем в арках дверей, специально разработанным захватом, так называемый кит, который полностью повторяет геометрию крыши, защищенный специальным полимером, который не повредит ЛКП вашего автомобиля',
+		'img1' => '<img class="img_main" src="/images/autobagazhniki/4.jpg" srcset="/images/autobagazhniki/4.jpg 200w" alt="багажник на плоскую крышу" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'img2' => '<img class="img_main" src="/images/autobagazhniki/5.jpg" srcset="/images/autobagazhniki/5.jpg 200w" alt="багажник на плоскую крышу" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'price' => 'Цена от 2500 рублей',
+		],
+		[
+		'id' => '4',
+		'name' => 'Багажник для водостоков',
+		'desc' => 'Водостоки - желобки, расположенные вдоль кузова автомобиля, как правило это у семейства автомобилей ВАЗ, ГАЗ, УАЗ',
+		'img1' => '<img class="img_main" src="/images/autobagazhniki/6.jpg" srcset="/images/autobagazhniki/6.jpg 200w" alt="багажник для водостоков" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'img2' => '<img class="img_main" src="/images/autobagazhniki/7.jpg" srcset="/images/autobagazhniki/7.jpg 200w" alt="багажник для водостоков" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+		'price' => 'Цена от 1000 рублей',
+		],
+];?>
