c99f3f347ccf3e3000dc625b57d48328faae6a3d
igoldyrev <goldirev12@gmail.com>
2017-08-24 22:20:57 +0500

    Стандартный файл с мета-тегами и стилями. В отличие от старой версии на предыдущем сайте здесь добавлена поддержка php-сессий

---
+++ frames/headtags.php
@@ -0,0 +1,31 @@
+<?php session_start(); ?>
+
+<!-- meta-tags -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="yandex-verification" content="febcf56753a9e525" />
+<meta name="robots" content="index,follow" />
+
+<!-- meta-property -->
+<meta property="place:location:latitude" content="58.03593904782619"/>
+<meta property="place:location:longitude" content="56.2000031453342"/>
+<meta property="business:contact_data:street_address" content="Ул. Спешилова 102/29"/>
+<meta property="business:contact_data:locality" content="Пермь"/>
+<meta property="business:contact_data:email" content="autobagaz@yandex.ru"/>
+<meta property="business:contact_data:website" content="https://autobagaz.ru"/>
+<meta property="og:url" content="https://autobagaz.ru"/>
+<meta property="og:site_name" content="Автобагаж"/>
+
+<!-- Иконка -->
+
+
+<!-- Основная таблица стилей -->   
+<link href="/styles/style.css" rel="stylesheet">
+
+<!-- Таблицы стилей для различных экранов --> 
+
+
+
+<!-- Таблицы стилей для блоков сайта -->
+<link href="/styles/goods.css" rel="stylesheet">
+<link href="/styles/form.css" rel="stylesheet">
