3da0b1c2c7c98c5169ca025c428c497b14ece9a8
igoldyrev <goldirev12@gmail.com>
2017-08-25 17:54:14 +0500

    Подключен файл подвала сайта

--- frames/headtags.php
+++ frames/headtags.php
@@ -30,6 +30,7 @@
 <link href="/styles/goods.css" rel="stylesheet">
 <link href="/styles/form.css" rel="stylesheet">
 <link href="/styles/header.css" rel="stylesheet">
+<link href="/styles/footer.css" rel="stylesheet">
 <link href="/styles/modalcall.css" rel="stylesheet">
 <link href="/styles/stylemenu.css" rel="stylesheet">
 
