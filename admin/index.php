d131d9a447c6a34c655293edf5cb9efd85364f7a
igoldyrev <goldirev12@gmail.com>
2017-08-27 01:18:39 +0500

    Главная страница админ-панели

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<title>Админка сайта Автобагаж.ру!</title>";
+echo "<h1>Админка сайта Автобагаж.ру!</h1>";
+echo "<h2>Панель управления отзывами</h2>";
+	
+?>
+
+<a href="/admin/guestbook/update/guestupdate.php">Обновить</a>
+<a href="/admin/guestbook/delete/guestdelete.php">Delete</a>
