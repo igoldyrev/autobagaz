f3fb2f530966dfbe83c6188949010a6f41419437
igoldyrev <goldirev12@gmail.com>
2017-08-27 01:18:18 +0500

    Файл с формой, в которой можно редактировать текст отзывов

---
+++ admin/guestbook/update/guestwrite.php
@@ -0,0 +1,21 @@
+<?php echo "<title>Внесение изменений в элемент</title>";
+
+include ($_SERVER["DOCUMENT_ROOT"]."/admin/connect.php");
+$id = $_REQUEST['rewiew'];
+$select_sql = "SELECT * FROM guestbook WHERE id = $id";
+$result = mysql_query($select_sql);
+$row = mysql_fetch_array($result);
+printf("<form action='/admin/guestbook/update/update.php' method='post'>
+<input type='hidden' name='id'  value='%s'><br/>
+<label for='name'>Имя человека, оставившего отзыв</label><br/>
+<input type='text' name='name' size='50' value='%s'><br/>
+<label for='phone'>Телефон человека, оставившего отзыв</label><br/>
+<input type='text' name='phone' size='50' value='%s'><br/>
+<label for='rewiew'>Сам текст отзыва</label><br/>
+<input type='text' name='rewiew' size='50' value='%s'><br/>
+<br/>
+<input id='submit' type='submit' value='Редактировать отзыв'><br/>
+</form>",$row['id'], $row['name'], $row['phone'], $row['rewiew']);
+?>
+<a href="/admin/guestbook/update/guestupdate.php">Вернуться к выбору отзыва для редактирования</a><br>
+<a href="/admin/index.php">Вернуться на главную админки</a>
