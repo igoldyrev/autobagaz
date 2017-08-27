21a12eca358e66885fa77db28d13e740c845d6af
igoldyrev <goldirev12@gmail.com>
2017-08-27 23:50:47 +0500

    Файл, который соединяется с таблицей из БД новостей для админки

---
+++ admin/connect_news.php
@@ -0,0 +1,6 @@
+<?php
+$sql = mysql_connect('localhost', '9082410193', 'GfhjkmDatabase');
+mysql_select_db('9082410193_news', $sql);
+
+mysql_query ("set_client='utf8'");//Следующие 2 строки решают проблему с кодировкой.
+mysql_query ("SET NAMES utf8"); ?>
