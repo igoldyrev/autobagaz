38de5b7e2f6738dedc5bfafbb07a69ac9fcc009b
igoldyrev <goldirev12@gmail.com>
2017-08-27 12:07:49 +0500

    Изменен заголовок страницы и вывод результата на странице, добавлен файл, в котором определены базовые таблицы стилей для админки

--- admin/guestbook/delete/delete.php
+++ admin/guestbook/delete/delete.php
@@ -1,12 +1,12 @@
-<?php echo "<title>Запись удалена</title>";
-echo "<h1>Запись удалена</h1>";
+<?php echo "<title>Отзыв удален!</title>";
 
+include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html");
 include ($_SERVER["DOCUMENT_ROOT"]."/admin/connect.php");
 
 $id=$_REQUEST['rewiew'];
 $delete_sql = "DELETE FROM guestbook WHERE id=$id";
 mysql_query($delete_sql) or die("<p>При удалении произошла ошибка</p>". mysql_error());
-echo "<p>Запись была успешно удалена!</p>";
+echo "<h3>Отзыв успешно удален!</h3>";
 ?>
 
 <a href="/admin/guestbook/delete/guestdelete.php">Вернуться к выбору отзывов</a><br>
