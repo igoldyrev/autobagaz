aed26bb24333ca53c096680dfa7a7e509ffbf2f5
igoldyrev <goldirev12@gmail.com>
2017-08-27 23:51:53 +0500

    Страница для админки, позволяющая выбрать новость для удаления

---
+++ admin/news/newsdelete.php
@@ -0,0 +1,19 @@
+<?php echo "<title>Выбор новости для удаления</title>";
+echo "<h1>Выбор новости для удаления</h1>";
+
+include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/admin/connect_news.php"); ?>
+<form action="/admin/guestbook/delete/delete.php" method="post">
+<?php
+$select_sql = "SELECT news_id, news_title, news_link, news_text FROM  news";
+$result = mysql_query($select_sql);
+$row = mysql_fetch_array($result);
+do
+{
+printf("<input type='radio' name='news' value='%s'>%s<br/><br/>", $row['news_id'], $row['news_title']);	
+}
+while($row = mysql_fetch_array($result))
+?>
+<input type="submit" value="Удалить новость с сайта">
+</form>
+<a href="/admin/index.php">Вернуться на главную админки</a>
