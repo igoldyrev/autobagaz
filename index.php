0a6d2a612ea3daf342247f7446caee32caf5ef75
igoldyrev <goldirev12@gmail.com>
2017-08-27 17:18:48 +0500

    Начато создание ссылок-рамок на главной странице

--- index.php
+++ index.php
@@ -14,9 +14,71 @@ if (!isset($page)) {
 	echo "<title> $titleconst"; echo $keywords[0][title]; echo "</title>";
 	echo "<meta name='description' content='"; echo $keywords[0][description]; echo "'/>";
 	echo "<meta name='keywords' content='"; echo $keywords[0][keywords]; echo "'/>";
+	echo "Тут позже будут новости"; ?>
+	
+	<div class="catalog">
+		<div class="menu_item">
+			<div class="item_image">
+			<a href="/autobagazhniki"><img src="/images/index/1_autobagazniki.jpg" alt="autobagazhniki" width="175px"></a>
+			</div>
+			<div class="item_link">
+			<a href="/autobagazhniki">Автобагажники</a>
+			</div>
+		</div>
+		<div class="menu_item">
+			<div class="item_image">
+			<a href="/autobagazhniki"><img src="/images/index/2_autobox.jpg" alt="autobox" width="175px"></a>
+			</div>
+			<div class="item_link">
+			<a href="/autobox">Автомобильные боксы</a>
+			</div>
+		</div>
+		<div class="menu_item">
+			<div class="item_image">
+			<a href="/autobagazhniki"><img src="/images/index/3_velokreplenya.jpg" alt="velokreplenya" width="175px"></a>
+			</div>
+			<div class="item_link">
+			<a href="/velokreplenya">Велокрепления</a>
+			</div>
+		</div>
+		<div class="menu_item">
+		<div class="item_image">
+			<a href="/autobagazhniki"><img src="/images/index/4_lyzh_kreplenya.jpg" alt="lyzhnye kreplenya" width="175px"></a>
+			</div>
+			<div class="item_link">
+			<a href="/kreplenya-dlya-lyzh-shoubord">Крепления для лыж и сноубордов</a>
+			</div>
+		</div>
+		<div class="menu_item">
+		<div class="item_image">
+			<a href="/autobagazhniki"><img src="/images/index/6_reelings.jpg" alt="reelings" width="175px"></a>
+			</div>
+			<div class="item_link">
+			<a href="/reelings">Рейлинги</a>
+			</div>
+		</div>
+		<div class="menu_item">
+		<div class="item_image">
+			<a href="/autobagazhniki"><img src="/images/index/7_braslet.jpg" alt="braslet" width="175px"></a>
+			</div>
+			<div class="item_link">
+			<a href="/braslets">Браслеты противоскольжения</a>
+			</div>
+		</div>
+		<div class="menu_item">
+		<div class="item_image">
+			<a href="/autobagazhniki"><img src="/images/index/8_farkops.jpg" alt="farkops" width="175px"></a>
+			</div>
+			<div class="item_link">
+			<a href="/farkops">Фаркопы</a>
+			</div>
+		</div>
+		
+	</div>
 
-	echo "Это главная страница";
 
+
+<?php
 	} elseif ($page == 'contacts') {
 	echo "<title> $titleconst"; echo $keywords[6][title]; echo "</title>";
 	echo "<meta name='description' content='"; echo $keywords[6][description]; echo "'/>";
@@ -38,5 +100,4 @@ if (!isset($page)) {
 	}
 ?>
 </div>
-<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/footer.html");	
-?>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/footer.html"); ?>
