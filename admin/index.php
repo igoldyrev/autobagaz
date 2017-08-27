9d04c5d5b3a96b2ac01e56b7e2cf0ab8a18cc398
igoldyrev <goldirev12@gmail.com>
2017-08-27 23:53:00 +0500

    Добавлен блок, позволяющий удалять новости с сайта

--- admin/index.php
+++ admin/index.php
@@ -8,3 +8,10 @@ echo "<h1>Админка сайта Автобагаж.ру!</h1>"; ?>
 <a href="/admin/guestbook/update/guestupdate.php">Редактировать отзывы</a><br>
 <a href="/admin/guestbook/delete/guestdelete.php">Удалить отзывы</a>
 </div>
+
+<div class="good">
+<?php echo "<h3>Панель управления новостями</h3>"; ?>	
+<a href="/admin/news/newsdelete.php">Удалить новости</a>
+</div>
+
+<a href="/">Вернуться на главную страницу сайта</a>
