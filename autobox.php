01b0075c40f40093406df36c76c9698866d90bb1
igoldyrev <goldirev12@gmail.com>
2017-09-01 14:05:43 +0500

    Добавлены страницы для автобоксов Турино и Люкс

--- autobox.php
+++ autobox.php
@@ -55,8 +55,58 @@ if (!isset($autobox)) {
 		<div class="button">
 		<button class="buy_button"><a href="/buy/<?php echo $yuago['id']; ?>">Заказать</a></button>		
 		</div>
-	</div> <?php
+	</div> <?php 	
+} elseif ($autobox == 'turino') {
+	echo "<title> $titleconst"; echo $keywords[14][title]; echo "</title>";
+	echo "<meta name='description' content='"; echo $keywords[14][description]; echo "'/>";
+	echo "<meta name='keywords' content='"; echo $keywords[14][keywords]; echo "'/>";
 	
+	$_SESSION['turino'] = $turino; 
+	$_SESSION['url'] = $_SERVER['REQUEST_URI'];
+	
+	echo "<h1>"; echo $turino[name]; echo "</h1>";
+	echo "<p>"; echo $turino[desc1]; echo "</p>";
+	echo "<p>"; echo $turino[desc2]; echo "</p>"; ?>
+	<div class="img_div"> <?php
+	echo $turino[img1]; echo $turino[img2]; echo $turino[img3]; echo $turino[img4]; echo $turino[img5]; echo $turino[img6]; ?>
+	</div>
+	<div class="price_info">
+		<div class="price">
+	<?php echo "<p>"; echo $turino[price]; echo "</p>";	?>
+		</div>
+		<div class="button">
+		<button class="buy_button"><a href="/buy/<?php echo $turino['id']; ?>">Заказать</a></button>		
+		</div>
+	</div> <?php
+} elseif ($autobox == 'lux') {
+	echo "<title> $titleconst"; echo $keywords[15][title]; echo "</title>";
+	echo "<meta name='description' content='"; echo $keywords[15][description]; echo "'/>";
+	echo "<meta name='keywords' content='"; echo $keywords[15][keywords]; echo "'/>";
+	
+	$_SESSION['lux'] = $lux; 
+	$_SESSION['url'] = $_SERVER['REQUEST_URI'];
+	
+	echo "<h1>Lux, Россия</h1>";
+	echo "<p>Боксы на крышу LUX произведены в России, из высококачественного российского АБС пластика , с применением фурнитуры, разработанной, специально для наших суровых условий эксплуатации.</p>";
+	echo "<p>Впервые в России, в багажных боксах использована система центрального замка с двухсторонним открыванием и многоточечным запиранием, полностью отечественного производства.</p>";
+	echo "<p>Крепления бокса к багажнику, отличаются простотой, удобством и высочайшей надёжностью. Массивные крепёжные скобы, с внутренней шириной 82 мм, позволяют закрепить бокс LUX на поперечинах всех европейских производителей багажных систем.</p>";
+	echo "<p>Грузоподъёмность боксов Люкс соответствует европейскому стандарту, и составляет 50 кг распределенного груза.</p>"; ?>
+	<div class="img_div">
+		<img class="img_main" src="/images/lux/lux_1.jpg" srcset="/images/lux/lux_1.jpg 300w" alt="Lux, Россия" sizes="(max-width: 2000px) 150px, 300px, 350px">
+		<img class="img_main" src="/images/lux/lux_2.jpg" srcset="/images/lux/lux_2.jpg 300w" alt="Lux, Россия" sizes="(max-width: 2000px) 150px, 300px, 350px">
+	</div>
+	<table cellspacing="0" align="center">
+		<tr><td class="producttablecaption" align="left">Наименование, характеристики</td><td class="producttablecaption" align="center">Цена</td><td class="producttablecaption"></td></tr>
+		<tr><td class="producttable" align="left"><?php echo $lux[name1]; ?></td><td class="producttable" align="center"><?php echo $lux[price1]; ?></td><td class="producttable"><button class="buy_button"><a href="/buy/<?php echo $lux['id1']; ?>">Заказать</a></button></td></tr>
+		<tr><td class="producttable" align="left"><?php echo $lux[name2]; ?></td><td class="producttable" align="center"><?php echo $lux[price2]; ?></td><td class="producttable"><button class="buy_button"><a href="/buy/<?php echo $lux['id2']; ?>">Заказать</a></button></td></tr>
+		<tr><td class="producttable" align="left"><?php echo $lux[name3]; ?></td><td class="producttable" align="center"><?php echo $lux[price3]; ?></td><td class="producttable"><button class="buy_button"><a href="/buy/<?php echo $lux['id3']; ?>">Заказать</a></button></td></tr>
+		<tr><td class="producttable" align="left"><?php echo $lux[name4]; ?></td><td class="producttable" align="center"><?php echo $lux[price4]; ?></td><td class="producttable"><button class="buy_button"><a href="/buy/<?php echo $lux['id4']; ?>">Заказать</a></button></td></tr>
+		<tr><td class="producttable" align="left"><?php echo $lux[name5]; ?></td><td class="producttable" align="center"><?php echo $lux[price5]; ?></td><td class="producttable"><button class="buy_button"><a href="/buy/<?php echo $lux['id5']; ?>">Заказать</a></button></td></tr>
+		<tr><td class="producttable" align="left"><?php echo $lux[name6]; ?></td><td class="producttable" align="center"><?php echo $lux[price6]; ?></td><td class="producttable"><button class="buy_button"><a href="/buy/<?php echo $lux['id6']; ?>">Заказать</a></button></td></tr>
+	</table>
+
+
+	<?php
 }
 
 
