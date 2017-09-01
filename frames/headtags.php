13f6e5577b96c432a4b69dcf09d244a8e25fa802
igoldyrev <goldirev12@gmail.com>
2017-09-01 23:12:50 +0500

    Добавлен параметр асинхронной загрузки к скриптам

--- frames/headtags.php
+++ frames/headtags.php
@@ -62,5 +62,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 
-<script src="/js/nav-menu.js"></script>  
-<script src="/js/popup.js"></script>
+<script async src="/js/nav-menu.js"></script>  
+<script async src="/js/popup.js"></script>
