e78918a968e5460fb65bcca052dcdb42ed977839
igoldyrev <goldirev12@gmail.com>
2017-09-05 23:04:12 +0500

    Добавлена документация к массиву для других членов команды

--- arrays/autobox_array.php
+++ arrays/autobox_array.php
@@ -2,16 +2,20 @@
 //Автобоксы Ветлан
 $vetlan = [
 	[
+	//Имена автобоксов и номера для создания ссылок
 	'id1' => '400Mblack',
 	'id2' => '400Mgray',
 	'id3' => '400Mwhite',
 	'name1' => 'Ветлан 400М черный',
 	'name2' => 'Ветлан 400М серый',
 	'name3' => 'Ветлан 400М белый',
+	//Заголовок, который отображается на странице
 	'title' => 'Автобоксы серии 400M',
+	//Изображения автобоксов
 	'img1' => '<img class="img_main" src="/images/vetlan/400/1.jpg" srcset="/images/vetlan/400/1.jpg 800w" alt="Vetlan 400M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img2' => '<img class="img_main" src="/images/vetlan/400/2.jpg" srcset="/images/vetlan/400/2.jpg 800w" alt="Vetlan 400M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img3' => '<img class="img_main" src="/images/vetlan/400/3.jpg" srcset="/images/vetlan/400/3.jpg 800w" alt="Vetlan 400M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+	//Данные таблицы
 	'size' => '165х80х35см',
 	'volume' => '400 л',
 	'material' => 'ABC',
@@ -20,22 +24,27 @@ $vetlan = [
 	'color' => 'Черный, серый, белый',
 	'desc1' => 'Vetlan 400M оснащен новой системой запирания.',
 	'desc2' => 'Замок с тремя точками фиксации автобокса, как у импортных производителей.',
+	//Цены за 400М, можно редактировать
 	'price_black' => '9 500 рублей',
 	'price_gray' => '10 000 рублей',
 	'price_white' => '10 500 рублей',
 	],
 	[
+	//Имена автобоксов и номера для создания ссылок
 	'id1' => '430Mblack',
 	'id2' => '430Mgray',
 	'id3' => '430Mwhite',
 	'name1' => 'Ветлан 430М черный',
 	'name2' => 'Ветлан 430М серый',
 	'name3' => 'Ветлан 430М белый',
+	//Заголовок, который отображается на странице
 	'title' => 'Автобоксы серии 430M',
+	//Изображения автобоксов
 	'img1' => '<img class="img_main" src="/images/vetlan/430M/1.jpg" srcset="/images/vetlan/430M/1.jpg 800w" alt="Vetlan 430M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img2' => '<img class="img_main" src="/images/vetlan/430M/2.jpg" srcset="/images/vetlan/430M/2.jpg 800w" alt="Vetlan 430M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img3' => '<img class="img_main" src="/images/vetlan/430M/3.jpg" srcset="/images/vetlan/430M/3.jpg 800w" alt="Vetlan 430M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img4' => '<img class="img_main" src="/images/vetlan/430M/4.jpg" srcset="/images/vetlan/430M/4.jpg 800w" alt="Vetlan 430M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+	//Данные таблицы
 	'size' => '175х77х45см',
 	'volume' => '430 л',
 	'material' => 'ABC',
@@ -44,22 +53,27 @@ $vetlan = [
 	'color' => 'Черный, серый, белый',
 	'desc1' => 'Vetlan 430M оснащен новой системой запирания.',
 	'desc2' => 'Замок с тремя точками фиксации автобокса, как у импортных производителей.',
+	//Цены за 430М, можно редактировать
 	'price_black' => '11 500 рублей',
 	'price_gray' => '12 000 рублей',
 	'price_white' => '12 500 рублей',
 	],
 	[
+	//Имена автобоксов и номера для создания ссылок
 	'id1' => '550Mblack',
 	'id2' => '550Mgray',
 	'id3' => '550Mwhite',
 	'name1' => 'Ветлан 550М черный',
 	'name2' => 'Ветлан 550М серый',
 	'name3' => 'Ветлан 550М белый',
+	//Заголовок, который отображается на странице
 	'title' => 'Автобоксы серии 550M',
+	//Изображения автобоксов
 	'img1' => '<img class="img_main" src="/images/vetlan/550M/1.jpg" srcset="/images/vetlan/550M/1.jpg 800w" alt="Vetlan 550M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img2' => '<img class="img_main" src="/images/vetlan/550M/2.jpg" srcset="/images/vetlan/550M/2.jpg 800w" alt="Vetlan 550M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img3' => '<img class="img_main" src="/images/vetlan/550M/3.jpg" srcset="/images/vetlan/550M/3.jpg 800w" alt="Vetlan 550M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img4' => '<img class="img_main" src="/images/vetlan/550M/4.jpg" srcset="/images/vetlan/550M/4.jpg 800w" alt="Vetlan 550M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+	//Данные таблицы
 	'size' => '205х84х45см',
 	'volume' => '550 л',
 	'material' => 'ABC',
@@ -68,6 +82,7 @@ $vetlan = [
 	'color' => 'Черный, серый, белый',
 	'desc1' => 'Vetlan 550M оснащен новой системой запирания.',
 	'desc2' => 'Замок с тремя точками фиксации автобокса, как у импортных производителей.',
+	//Цены за 550М, можно редактировать
 	'price_black' => '14 500 рублей',
 	'price_gray' => '14 800 рублей',
 	'price_white' => '15 000 рублей',
@@ -76,22 +91,30 @@ $vetlan = [
 
 //Автобоксы Яго
 $yuago = [
+	//id для создания ссылки
 	'id' => 'yuago',
+	//Заголовок на странице
 	'name' => 'Аэродинамический Бокс-Багажник (Автобокс) на крышу автомобиля YUAGO(ЯГО) Cosmo 210 объемом 485 литров',
+	//Изображение
 	'img1' => '<img class="img_main" src="images/yuago/yuago.jpg" srcset="images/yuago/yuago.jpg 350w" alt="yuago" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+	//Данные таблицы
 	'size' => '2180 х 730 х 300 мм',
 	'volume' => '485 литров',
 	'material' => 'ABC пластик, толщина листа - 4 мм',
 	'color' => 'Черный, серый, белый',
 	'lock' => 'Одноточечный с дополнительными местами фиксации спереди и сзади',
 	'bagazh' => 'Одностороннее открытие',
+	//Цена
 	'price' => '14 000 рублей',	
 ];
 
 //Автобоксы Турино
 $turino = [
+	//id для создания ссылки
 	'id' => 'turino',
+	//Заголовок на странице
 	'name' => 'Автобоксы Турино, производство Тольятти',
+	//Описания и изображения, которые отображаются на странице
 	'desc1' => 'Пожалуй, самые сбалансированные боксы по параметрам: цена/качество, выполненные из высококачественного ABS пластика, снабженный современным 3-х точечным замком, надежно сохранит ваш груз.',
 	'desc2' => 'Цвет: серый, черный, белый - матовый, одностороннее открывание,3-х точечный замок Размер внешний 175 х 82 х 45 см, внутренний 169 х 76х40 см, объем 460 л.',
 	'img1' => '<img class="img_main" src="/images/turino/turino_1.jpg" srcset="/images/turino/turino_1.jpg 200w" alt="Автобоксы Турино" sizes="(max-width: 2000px) 150px, 300px, 350px">',
@@ -100,41 +123,51 @@ $turino = [
 	'img4' => '<img class="img_main" src="/images/turino/turino_4.jpg" srcset="/images/turino/turino_4.jpg 1200w" alt="Автобоксы Турино" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img5' => '<img class="img_main" src="/images/turino/turino_5.jpg" srcset="/images/turino/turino_5.jpg 1200w" alt="Автобоксы Турино" sizes="(max-width: 2000px) 150px, 300px, 350px">',
 	'img6' => '<img class="img_main" src="/images/turino/turino_6.jpg" srcset="/images/turino/turino_6.jpg 1200w" alt="Автобоксы Турино" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+	//Цена
 	'price' => 'Цена 14000/16000 (матовый/глянцевый)',
 ];
 
 //Автобоксы Атлант Discovery
 $discoveryclassic = [
+	//Discovery CLASSIC 320
 	'id1' => 'dc320',
 	'name1' => 'Бокс CLASSIC 320 (133*73*40 см.)',
 	'price1' => '14 000 руб.',
+	//Discovery CLASSIC 430
 	'id2' => 'dc430',
 	'name2' => 'Бокс CLASSIC 430 (171*73*36) черный',
 	'price2' => '17 000 руб.',
+	//Discovery CLASSIC 500
 	'id3' => 'dc500',
 	'name3' => 'Бокс CLASSIC 500 (216*73*38) черный',
 	'price3' => '19 000 руб.',
 ];
 $discoverysport = [
+	//Discovery SPORT 431
 	'id1' => 'ds431',
 	'name1' => 'SPORT 431 (171*73*39) темно-серый',
 	'price1' => '22 000 руб.',
+	//Discovery SPORT 501
 	'id2' => 'ds501',
 	'name2' => 'SPORT 501 (216*73*38) темно-серый',
 	'price2' => '25 000 руб.',
 ];
 $dynamic = [
+	//Atlant DYNAMIC 434
 	'id1' => 'dynamic434',
 	'name1' => 'DYNAMIC 434 (171*73*36 см.) серебристый металлик',
 	'price1' => '25 000 руб.',
+	//Atlant DYNAMIC 504
 	'id2' => 'dynamic504',
 	'name2' => 'DYNAMIC 504 (216*73*38 см.) серебристый металлик',
 	'price2' => '28 000 руб.',
 ];
 $airtek = [
+	//Atlant AIRTEK 435
 	'id1' => 'airtek435',
 	'name1' => 'AIRTEK 435 (171*73*36 см.) чёрный металлик',
 	'price1' => '25 000 руб.',
+	//Atlant AIRTEK 505
 	'id2' => 'airtek505',
 	'name2' => 'AIRTEK 505 (216*73*38 см.) чёрный металлик',
 	'price2' => '28 000 руб.',
@@ -142,23 +175,28 @@ $airtek = [
 
 //Автобоксы LUX
 $lux = [
+	//LUX600 440L металлик
 	'id1' => 'lux600blackmetal',
 	'name1' => 'Бокс LUX600 440L черный, серый металлик 1600х920х400 с двух стор. откр.',
 	'price1' => '19 000 рублей',
+	//LUX600 440L матовый
 	'id2' => 'lux600blackfrosted',
 	'name2' => 'Бокс LUX600 440L черный, серый матовый 1600х920х400 с друстор. откр.',
 	'price2' => '19 000 рублей',
+	//LUX600 440L белый глянец
 	'id3' => 'lux600white',
 	'name3' => 'Бокс LUX600 440L белый глянец 1600х920х400 с двух стор. откр.',
 	'price3' => '19 000 рублей',
+	//LUX960 480L черный матовый
 	'id4' => 'lux960blackfrosted',
 	'name4' => 'Бокс LUX960 480L черный, серый матовый 1960х780х420 с друстор. откр.',
 	'price4' => '24 000 рублей',
+	//LUX960 480L черный металлик
 	'id5' => 'lux960blackmetal',
 	'name5' => 'Бокс LUX960 480L черный, серый металлик 1960х780х420 с двух стор. откр.',
 	'price5' => '24 000 рублей',
+	//LUX960 480L белый глянец
 	'id6' => 'lux960white',
 	'name6' => 'Бокс LUX960 480L белый глянец 1960х780х420 с двустор. откр.',
 	'price6' => '29 900 рублей',
-];
-?>
+]; ?>
