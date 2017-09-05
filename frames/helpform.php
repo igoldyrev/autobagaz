0bf4a5295d8b701fc3f684e56599394b60343b9f
igoldyrev <goldirev12@gmail.com>
2017-09-05 20:55:06 +0500

    В форму помощи добавлено поле ввода сообщения

--- frames/helpform.php
+++ frames/helpform.php
@@ -12,6 +12,11 @@ echo "<h3>Не можете определиться с выбором? Запо
   <input type="text" name="phone" class="better-placeholder__input" required="required" pattern="[0-9]{10,11}" placeholder="Введите номер телефона">
   <label for="phone" class="better-placeholder__label">Введите номер телефона</label>
 </div><br>
+<span class="label_top">Ваше сообщение</span>
+<div class="better-placeholder">
+<textarea name="message" class="better-placeholder__input" pattern="^[A-Za-zА-Яа-яЁё0-9\s]+$" placeholder="Введите Ваше сообщение"></textarea>
+  <label for="message" class="better-placeholder__label">Введите Ваше сообщение</label>
+</div><br>
 <div align="center">
 <input class="input__button" type="submit" value="Помогите с выбором!">
 </div></form>
