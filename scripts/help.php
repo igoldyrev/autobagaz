516158bf17b63f248e669064367a0569273ab29c
igoldyrev <goldirev12@gmail.com>
2017-09-05 20:55:38 +0500

    В обработчик добавлена обработка поля ввода сообщения

--- scripts/help.php
+++ scripts/help.php
@@ -10,22 +10,27 @@ $url = $_SESSION['url'];
 //В файле на первом этапе нужно принять данные из пост массива. Для этого создаем переменные
 $name = $_POST['name'];
 $phone = $_POST['phone'];
+$message = $_POST['message'];
 
 //Первая функция преобразует все символы, которые пользователь попытается добавить в форму
 $name = htmlspecialchars($name);
 $phone = htmlspecialchars($phone);
+$message = htmlspecialchars($message);
 
 //Вторая функция декодирует url, если пользователь попытается его добавить в форму
 $name = urldecode($name);
 $phone = urldecode($phone);
+$message = urldecode($message);
 
 //Третьей функцией мы удалим пробелы с начала и конца строки, если таковые имеются
 $name = trim($name);
 $phone = trim($phone);
+$message = trim($message);
 
 //Заносим данные из формы в переменные
 $name = $_REQUEST['name'];
 $phone = $_REQUEST['phone'];
+$message = $_REQUEST['message'];
 
 //Создаем запрос в базу данных
 //$sql_insert = "INSERT INTO zakaz (name, phone, auto, kuzov, year, text)" . 
@@ -38,12 +43,13 @@ $phone = $_REQUEST['phone'];
 
 
 
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){
 
 if (mail("goldirev12@yandex.ru", "Человек не может определиться с оборудованием!", 
 "Страница с которой пришло письмо ".$_SESSION['url'].";
 Имя:".$name.";
 Телефон: ".$phone.";
+Сообщение: ".$message.";
 
 Техническая информация:
 Примерный user-agent: ".$_SERVER['HTTP_USER_AGENT'].";
