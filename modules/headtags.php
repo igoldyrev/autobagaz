c6c8ba80623c65dc9f6a1488bad4edd530b24d32
igoldyrev <goldirev12@gmail.com>
2017-10-26 22:51:04 +0500

    Файл с тегами для шапки сайта

---
+++ modules/headtags.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+
+<!-- meta tags -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="yandex-verification" content="febcf56753a9e525">
+<meta name="robots" content="index,follow">
+
+<!-- meta property -->
+<meta property="place:location:latitude" content="58.03593904782619">
+<meta property="place:location:longitude" content="56.2000031453342">
+<meta property="business:contact_data:street_address" content="Ул. Спешилова 102/29">
+<meta property="business:contact_data:locality" content="Пермь">
+<meta property="business:contact_data:email" content="autobagaz@yandex.ru">
+<meta property="business:contact_data:website" content="https://autobagaz.ru">
+<meta property="og:url" content="https://autobagaz.ru">
+<meta property="og:site_name" content="Автобагаж">
+
+<!-- Иконка -->
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+<link rel="stylesheet" href="build/style.css">
