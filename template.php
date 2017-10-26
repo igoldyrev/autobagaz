4fd2668a61afd69635f87d23c184c602066944dd
igoldyrev <goldirev12@gmail.com>
2017-10-26 22:59:27 +0500

    Файл шаблона для всех страниц

---
+++ template.php
@@ -0,0 +1,9 @@
+<?php
+include ($_SERVER["DOCUMENT_ROOT"]."/modules/headtags.php"); ?>
+<div class="wrapper">
+    <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/header/header.html");?>
+    <div class="wrapper-content">
+        <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/left-nav/left-nav.html"); ?>
+    </div>
+    <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/footer/footer.html");?>
+</div>
