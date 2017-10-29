504611eead479425970a70e7aac0a1bbe073cd63
igoldyrev <goldirev12@gmail.com>
2017-10-29 20:06:00 +0500

    Основной js-файл, импортирующий в себя другие файлы

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import {log} from './notification';
+
+//notify('Here is my mesage');
+log('Это сайт компании Автобагаж');
+log('Сейчас сайт работает нормально');
+
+import {navmenu} from './nav-menu.js';
