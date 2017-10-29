f3e1917323c4b482b2117fe08b4c7f63208f0095
igoldyrev <goldirev12@gmail.com>
2017-10-29 20:03:01 +0500

    Обновлена конфигурация проекта

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,103 @@
-var path = require('path');
 var webpack = require('webpack');
+var path = require('path');
 var glob = require('glob');
-var  ExtractTextPlugin = require('extract-text-webpack-plugin');
+var glob = require('glob-all');
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var PurifyCSSPlugin = require('purifycss-webpack');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
-var inProduction = (process.env.NODE_ENV === 'production');
+var InProduction = (process.env.NODE_ENV === 'production');
 
+module.exports = {
+    entry: {
 
-var config = {
-    // context: path.resolve(__dirname),
+        autobagaz: [
+                './js/main.js',
+                './modules/style.scss'
 
-   // devtool: 'source-map',
+        ],
+        //vendor: ['jquery']
 
-    entry: [
-        // vendor: ['jquery'],
-      //  './js/main.js',
-        './modules/style.scss'
-    ],
+    },
+
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js'
+
     },
+
     module: {
+
         rules: [
+
             {
                 test: /\.s[ac]ss$/,
-                use: ExtractTextPlugin.extract(
-                    {
-                        fallback: 'style-loader',
-                        use: [ 'css-loader', 'sass-loader']
-                    })
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback: 'style-loader'
+
+                })
+
             },
+
+            {
+                test: /\.css$/,
+                use: ['css-loader']
+            },
+
+            {
+
+                test: /\.(svg|eot|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'build/fonts/[name].[ext]'
+                }
+            },
+
+            {
+
+                test: /\.(png|jpe?g|gif)$/,
+                loaders: [
+                    {
+
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]'
+
+                        }
+                    },
+
+                    'img-loader'
+
+                ]
+
+            },
+
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
-            // {
-            //     test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            //     loader: 'url-loader',
-            //     options:
-            //         {
-            //             limit: 500
-            //         }
-            // }
-
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                loader: 'file-loader',
-            },
-            {
-                test: /\.(png|je?pg|gif)$/,
-
-                loaders: [
-                    {
-                        loader: 'file-loader',
-                        options:
-                            {
-                                name: '../[path][name].[ext]'
-                            }
-                    },
-
-                    'img-loader'
-                ]
-
-
-                
-            }
         ]
     },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new ExtractTextPlugin(
-        {
-            filename: 'style.css',
-            allChunks: true
-        }),
 
-      /*  new PurifyCSSPlugin({
+    plugins: [
+
+        new ExtractTextPlugin('[name].css'),
+
+        /*new PurifyCSSPlugin({
             // Give paths to parse for rules. These should be absolute!
-            paths: glob.sync(path.join(__dirname, 'index.html')),
-            minimize: inProduction
+            paths: glob.sync([
+
+                path.join(__dirname, '*.html'),
+                path.join(__dirname, '*.php'),
+
+        ]),
+            minimize: InProduction
+
         }),*/
+
         new CleanWebpackPlugin(['build'], {
             root: __dirname,
             verbose: true,
@@ -93,16 +105,15 @@ var config = {
         }),
 
         new webpack.LoaderOptionsPlugin({
-            minimize: inProduction
+
+            minimize: InProduction
         })
+
     ]
 
 };
 
-module.exports = config;
-
-if (inProduction)
-{
+if (InProduction) {
     module.exports.plugins.push(
         new webpack.optimize.UglifyJsPlugin()
     );
