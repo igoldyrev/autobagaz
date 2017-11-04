efc39e5c86cbab789e0d8ff51ae237ef5ddc9476
igoldyrev <goldirev12@gmail.com>
2017-11-04 17:00:58 +0500

    Файл с формой для заказа товаров

--- modules/forms/buyform.php
+++ modules/forms/buyform.php
@@ -1,21 +1,21 @@
 <div class="form__container">
     <form action="/zakaz" method="post" class="form">
-        <span class="form__label">Ваше имя</span>
+        <span class="form__label">Ваше имя:</span>
         <div class="form__input-block">
             <input type="text" name="name" required class="form__input" placeholder="Введите Ваше имя">
             <label for="name" class="form__label--shown">Введите Ваше имя</label>
         </div>
-        <span class="form__label">Ваш телефон</span>
+        <span class="form__label">Ваш телефон:</span>
         <div class="form__input-block">
             <input type="text" name="phone" required class="form__input" placeholder="Введите номер телефона">
             <label for="phone" class="form__label--shown">Введите номер телефона</label>
         </div>
-        <span class="form__label">Марка машины</span>
+        <span class="form__label">Марка машины:</span>
         <div class="form__input-block">
             <input type="text" name="auto" class="form__input" placeholder="Введите марку автомобиля">
             <label for="auto" class="form__label--shown">Введите марку автомобиля</label>
         </div>
-        <span class="form__label">Тип кузова</span>
+        <span class="form__label">Тип кузова:</span>
         <div class="form__input-block">
             <select name="kuzov" size="1" class="form__select">
                 <option value="Не выбрано" selected>Не указано</option>
@@ -24,15 +24,16 @@
                 <option value="Универсал">Универсал</option>
             </select>
         </div>
-        <span class="form__label">Год выпуска</span>
+        <span class="form__label">Год выпуска:</span>
         <div class="form__input-block">
             <input type="text" name="year" class="form__input" placeholder="Введите год выпуска автомобиля">
             <label for="year" class="form__label--shown">Введите год выпуска автомобиля</label>
         </div>
-        <span class="form__label">Дополнительная информация</span>
+        <span class="form__label">Дополнительная информация:</span>
         <div class="form__input-block">
             <textarea name="text" class="form__input form__textarea" placeholder="Введите какую-либо дополнительную информацию"></textarea>
             <label for="text" class="form__label--shown">Введите какую-либо дополнительную информацию</label>
         </div>
+        <button class="button__zakaz">Отправить заказ</button>
     </form>
 </div>
