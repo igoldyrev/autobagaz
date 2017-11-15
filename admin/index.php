65340155179080d5a6fa003c10ebd4a59b8d859c
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2017-11-15 18:42:00 +0500

    Верстка админки сайта

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,22 @@
 <?php
 include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html");
 echo "<title>Админка сайта Автобагаж.ру!</title>";
-echo "<h1>Админка сайта Автобагаж.ру!</h1>"; ?>
+echo "<h1 class='page__title-h1'>Админка сайта Автобагаж.ру!</h1>"; ?>
 
-<div class="good">
-<?php echo "<h3>Панель управления отзывами</h3>"; ?>	
-<a href="/admin/guestbook/update/guestupdate.php">Редактировать отзывы</a><br>
-<a href="/admin/guestbook/delete/guestdelete.php">Удалить отзывы</a>
+<div class="admin">
+<?php echo "<h3 class='page__title-h3'>Панель управления отзывами</h3>"; ?>
+<a class="admin__link" href="/admin/guestbook/update/guestupdate.php">Редактировать отзывы</a>
+<a class="admin__link" href="/admin/guestbook/delete/guestdelete.php">Удалить отзывы</a>
 </div>
 
-<div class="good">
-<?php echo "<h3>Панель управления новостями</h3>"; ?>	
-<a href="/admin/news/newsdelete.php">Удалить новости</a>
+<div class="admin">
+<?php echo "<h3 class='page__title-h3'>Панель управления новостями</h3>"; ?>
+<a class="admin__link" href="/admin/news/newsdelete.php">Удалить новости</a>
 </div>
 
-<div class="good">
-<?php echo "<h3>Скачать данные сайта</h3>"; ?>
-<a href="/admin/files/keywords.zip">Скачать</a>
+<div class="admin">
+<?php echo "<h3 class='page__title-h3'>Скачать данные сайта</h3>"; ?>
+<a class="admin__link" href="/admin/files/keywords.zip">Скачать</a>
 </div>
 
-<a href="/">Вернуться на главную страницу сайта</a>
+<a class="admin__link admin__link--back" href="/">Вернуться на главную страницу сайта</a>
