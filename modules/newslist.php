d89d50d6c57eef3f5a06d82a99a9aa80e4f1ad44
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2017-11-17 17:36:24 +0500

    Файл подключения новостей для главной страницы

--- modules/newslist.php
+++ modules/newslist.php
@@ -14,20 +14,26 @@ if(!mysql_select_db($sdd_db_name, $conn)) // выбор бд
 {
     throw new Exception("Не удалось выбрать базу данных {$ssd_db_name}!");
 }
-$result = mysql_query('SELECT * FROM `news` ORDER BY news_id DESC LIMIT 5', $conn); // запрос на выборку
+$result = mysql_query('SELECT * FROM `news` ORDER BY news_id DESC LIMIT 4', $conn); // запрос на выборку
 if(!$result)
 {
     throw new Exception(sprintf('Не удалось выполнить запрос к БД, код ошибки %d, текст ошибки: %s', mysql_errno($conn), mysql_error($conn)));
 }
 
-while($row = mysql_fetch_array($result))
+echo "<h2 class='page__title-h2'>Новости</h2>";
+echo "<div class='news'>";
+while ($row = mysql_fetch_assoc($result))
 {
-    echo '<table class="newslist">
-			<tr>		
-				<td width="100px" class="newslist">'.$row['news_date'].'</td>
-				<td class="newslist"><a href="'.$row['news_link'].'">'.$row['news_title'].'</a></td>	
-			</tr>
-		</table>';// выводим данные
+    echo "<div class='news__item news__item--index''>";
+    echo "<div class='news__info'>";
+    echo "<span class='news__date page__text'>".$row['news_date']."</span>";
+    echo "<span class='page__text'>Автор: ".$row['news_author']."</span>";
+    echo "</div>";
+    echo "<a href='".$row['news_link']."' class='news__title page__link'>".$row['news_title']."</a>";
+    echo "<p class='news__annotation page__text'>".$row['news_annotation']."</p>";
+    echo "<a href='".$row['news_link']."' class='news__link page__link'>Читать далее</a>";
+    echo "</div>";
 }
-echo '<center><a href="/news">Архив новостей</a></center>';
+echo "</div>";
+echo '<a class="left-nav__link left-nav__link--rewiew" href="/news">Все новости</a>';
 ?>
