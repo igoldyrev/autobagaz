da044358afabd3f85f955db8178412e11db7302e
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2017-11-29 10:43:19 +0500

    Хлебные крошки переписаны в зависимости от того, сколько частей содержится в урл. Каждая часть начинается со знака /

--- modules/breadcrumbs/breadcrumbs.php
+++ modules/breadcrumbs/breadcrumbs.php
@@ -1,15 +1,13 @@
 <?php
 $get_url = $_SERVER['REQUEST_URI'];
-
 $url_parts = explode( '/', parse_url($get_url, PHP_URL_PATH) );
+$count = count($url_parts);
 
-if (!isset($get_url)) {
-
-} elseif ($get_url == '/news') { ?>
-    <a class="page__link" href="<?php echo '/'; ?>">Каталог</a>
-    <a class="page__link" href="<?php echo '/'; echo $url_parts[1]; ?>">Новости</a> <?php
-} elseif ($get_url == '/news/postuplenya_amos') { ?>
-    <a class="page__link" href="<?php echo '/'; ?>">Каталог</a>
+if ($count == '3') { ?>
+    <a class="page__link" href="/">Каталог</a>
     <a class="page__link" href="<?php echo '/'; echo $url_parts[1]; ?>">Новости</a>
-    <a class="page__link" href="<?php echo '/'; echo $url_parts[1]; echo '/'; echo $url_parts[2]; ?>"><?php echo $news[0]['title']; ?></a>' <?php
+    <a class="page__link" href="<?php echo '/'; echo $url_parts[1]; echo '/'; echo $url_parts[2]; ?>"><?php echo $news[0]['title']?></a> <?php
+} elseif ($count == '2') { ?>
+    <a class="page__link" href="/">Каталог</a>
+    <a class="page__link" href="<?php echo '/'; echo $url_parts[1]; ?>">Новости</a> <?php
 }
