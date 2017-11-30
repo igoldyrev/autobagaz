392323cfd76fd4d348f0c75124466fc032992b7c
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2017-11-30 19:03:00 +0500

    Примерный вариант хлебных крошек со стилями.

--- modules/breadcrumbs/breadcrumbs.php
+++ modules/breadcrumbs/breadcrumbs.php
@@ -1,13 +1,23 @@
 <?php
+$base_link = "<a class='breadcrumbs__link' href='/'>Главная страница</a>";
+
 $get_url = $_SERVER['REQUEST_URI'];
 $url_parts = explode( '/', parse_url($get_url, PHP_URL_PATH) );
 $count = count($url_parts);
 
-if ($count == '3') { ?>
-    <a class="page__link" href="/">Каталог</a>
-    <a class="page__link" href="<?php echo '/'; echo $url_parts[1]; ?>">Новости</a>
-    <a class="page__link" href="<?php echo '/'; echo $url_parts[1]; echo '/'; echo $url_parts[2]; ?>"><?php echo $news[0]['title']?></a> <?php
-} elseif ($count == '2') { ?>
-    <a class="page__link" href="/">Каталог</a>
-    <a class="page__link" href="<?php echo '/'; echo $url_parts[1]; ?>">Новости</a> <?php
+if ($get_url == '/news/postuplenya_amos') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <a class="breadcrumbs__link" href="<?php echo '/'; echo $url_parts[1]; ?>"><?php echo $keywords[25]['title']?></a>&#8594;
+        <span class="breadcrumbs__text"><?php echo $news[0]['title']?></span>
+    </div>
+     <?php
+} elseif ($get_url == '/news/reelings_xray') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+    <a class="breadcrumbs__link" href="<?php echo '/'; echo $url_parts[1]; ?>"><?php echo $keywords[25]['title']?></a>&#8594;
+    <span class="breadcrumbs__text"><?php echo $news[1]['title']?></span>
+    </div>
+    <?php
 }
+
