50b9f6991b76c688eb873b31bcdd7e62a7bb726d
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2017-12-01 11:51:00 +0500

    Правила сгруппированы по разделам

--- modules/breadcrumbs/breadcrumbs.php
+++ modules/breadcrumbs/breadcrumbs.php
@@ -6,6 +6,7 @@ $get_url = $_SERVER['REQUEST_URI'];
 $url_parts = explode( '/', parse_url($get_url, PHP_URL_PATH) );
 $count = count($url_parts);
 
+//Страницы новостей
 if ($get_url == '/news/postuplenya_amos') { ?>
     <div class="breadcrumbs">
         <?php echo $base_link; ?>&#8594;
@@ -89,5 +90,79 @@ if ($get_url == '/news/postuplenya_amos') { ?>
         <?php echo $base_link; ?>&#8594;
         <a class="breadcrumbs__link" href="<?php echo '/'; echo $url_parts[1]; ?>"><?php echo $keywords[25]['title']?></a>&#8594;
         <span class="breadcrumbs__text"><?php echo $news[13]['title']?></span>
+    </div> <?php
+} elseif ($get_url == '/news/1') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[25]['title']?></span>
+    </div> <?php
+} elseif ($get_url == '/news/2') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[25]['title']?></span>
+    </div> <?php
+} elseif ($get_url == '/news') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[25]['title']?></span>
+    </div> <?php
+}
+//Страницы каталога
+  elseif ($get_url == '/autobagazhniki') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[7]['title']?></span>
+    </div> <?php
+} elseif ($get_url == '/kreplenya-dlya-lyzh-shoubord') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[19]['title']?></span>
+    </div> <?php
+} elseif ($get_url == '/reelings') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[20]['title']?></span>
+    </div> <?php
+} elseif ($get_url == '/braslets') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[21]['title']?></span>
+    </div> <?php
+} elseif ($get_url == '/farkops') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[22]['title']?></span>
+    </div> <?php
+}
+
+//Автобоксы
+elseif ($get_url == '/autobox') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[8]['title']?></span>
+    </div> <?php
+}
+
+//Велокрепления
+elseif ($get_url == '/velokreplenya') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[16]['title']?></span>
+    </div> <?php
+}
+
+//Раздел Инно
+elseif ($get_url == '/inno') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[30]['title']?></span>
+    </div> <?php
+}
+
+//Раздел такелаж
+elseif ($get_url == '/takelazh') { ?>
+    <div class="breadcrumbs">
+        <?php echo $base_link; ?>&#8594;
+        <span class="breadcrumbs__text"><?php echo $keywords[35]['title']?></span>
     </div> <?php
 }
