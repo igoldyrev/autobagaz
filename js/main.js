98f65186c42d91de51d1dc95adb3a1c7a16b57ab
igoldyrev <goldirev12@gmail.com>
2017-12-10 20:51:38 +0500

    JS-скрипты для вкладок

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ log('Это сайт компании Автобагаж');
 log('Сейчас сайт работает нормально');
 
 //import {topmenu} from './js/nav-menu.js';
+
+$(function()
+{
+    $('.guestbook__tab-add').click(function()
+    {
+        $('.page__header-tab').removeClass('page__header-tab--active');
+        $('.guestbook__tab').removeClass('guestbook__tab--active');
+
+        $('.guestbook__tab-add').addClass('page__header-tab--active');
+        $('.guestbook__rewiews-add').addClass('guestbook__tab--active');
+    });
+
+    $('.guestbook__tab-rewiew').click(function()
+    {
+        $('.guestbook__tab-add').removeClass('page__header-tab--active');
+        $('.guestbook__rewiews-add').removeClass('guestbook__tab--active');
+
+        $('.guestbook__tab-rewiew').addClass('page__header-tab--active');
+        $('.guestbook__rewiews').addClass('guestbook__tab--active');
+    });
+});
+
