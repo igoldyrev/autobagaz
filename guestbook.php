84366ab6ff05b55f918b3af53a71f170e94a2dc6
igoldyrev <goldirev12@gmail.com>
2017-12-10 20:50:57 +0500

    Верстка и стили для вкладок на странице отзывов

--- guestbook.php
+++ guestbook.php
@@ -1,107 +1,108 @@
 <?php
 include ($_SERVER["DOCUMENT_ROOT"]."/modules/keywords.php");
 include ($_SERVER["DOCUMENT_ROOT"]."/modules/headtags.php");
-	echo "<title> $titleconst"; echo $keywords[24][title]; echo "</title>";
-	echo "<meta name='description' content='"; echo $keywords[24][description]; echo "'/>";
-	echo "<meta name='keywords' content='"; echo $keywords[24][keywords]; echo "'/>"; ?>
-
+echo "<title> $titleconst"; echo $keywords[24][title]; echo "</title>";
+echo "<meta name='description' content='"; echo $keywords[24][description]; echo "'/>";
+echo "<meta name='keywords' content='"; echo $keywords[24][keywords]; echo "'/>"; ?>
 
 <div class="wrapper">
     <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/header/header.php"); ?>
     <div class="wrapper-content">
         <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/left-nav/left-nav.html"); ?>
         <div class="content">
+            <div class="page__header">
+                <div class="page__header-tab guestbook__tab-rewiew page__header-tab--active">Все отзывы</div>
+                <div class="page__header-tab guestbook__tab-add">Оставить отзыв</div>
+            </div>
+            <div class="guestbook__tab guestbook__rewiews guestbook__tab--active">
+                <h1 class="page__title-h1">Нам важно Ваше мнение!</h1>
+                <p class="page__text">На данной странице Вы можете оставить отзыв о нашей проделанной работе, либо написать нам какие-либо пожелания. А также посмотреть другие отзывы о нас.</p>
+                <?php
+                // Соединение с БД MySQL
+                $sql = mysql_connect('localhost', '9082410193', 'GfhjkmDatabase');
+                mysql_select_db('9082410193_zakaz', $sql);
 
-            <?php $guestbook = $_GET['guestbook']; ?>
+                mysql_query ("set_client='utf8'");//Следующие 2 строки решают проблему с кодировкой.
+                mysql_query ("SET NAMES utf8");
 
-            <h1 class="page__title-h1">Нам важно Ваше мнение!</h1>
-            <p class="page__text">На данной странице Вы можете оставить отзыв о нашей проделанной работе, либо написать нам какие-либо пожелания. А также посмотреть другие отзывы о нас.</p>
+                // Количество новостей на странице
+                $on_page = 10;
 
-           <?php if (!isset($guestbook)) {
-            // Соединение с БД MySQL
-            $sql = mysql_connect('localhost', '9082410193', 'GfhjkmDatabase');
-            mysql_select_db('9082410193_zakaz', $sql);
+                // Получаем количество записей таблицы
+                $query = "SELECT COUNT(*) FROM `guestbook`";
+                $res = mysql_query($query);
+                $count_records = mysql_fetch_row($res);
+                $count_records = $count_records[0];
 
-            mysql_query ("set_client='utf8'");//Следующие 2 строки решают проблему с кодировкой.
-            mysql_query ("SET NAMES utf8");
+                // Получаем количество страниц
+                // Делим количество всех записей на количество записей на странице
+                // и округляем в большую сторону
+                $num_pages = ceil($count_records / $on_page);
 
-           // Количество новостей на странице
-           $on_page = 10;
+                // Текущая страница из GET-параметра page
+                // Если параметр не определен, то текущая страница равна 1
+                $current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 
-           // Получаем количество записей таблицы
-           $query = "SELECT COUNT(*) FROM `guestbook`";
-           $res = mysql_query($query);
-           $count_records = mysql_fetch_row($res);
-           $count_records = $count_records[0];
-
-           // Получаем количество страниц
-           // Делим количество всех записей на количество записей на странице
-           // и округляем в большую сторону
-           $num_pages = ceil($count_records / $on_page);
-
-           // Текущая страница из GET-параметра page
-           // Если параметр не определен, то текущая страница равна 1
-           $current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-
-           // Если текущая страница меньше единицы, то страница равна 1
-           if ($current_page < 1)
-           {
-               $current_page = 1;
-           }
-// Если текущая страница больше общего количества страница, то
-// текущая страница равна количеству страниц
-           elseif ($current_page > $num_pages)
-           {
-               $current_page = $num_pages;
-           }
-
-           // Начать получение данных от числа (текущая страница - 1) * количество записей на странице
-           $start_from = ($current_page - 1) * $on_page;
-
-           // Формат оператора LIMIT <ЗАПИСЬ ОТ>, <КОЛИЧЕСТВО ЗАПИСЕЙ>
-           $query = "SELECT `name`, `phone`, `rewiew` FROM `guestbook` ORDER BY `id` DESC LIMIT $start_from, $on_page";
-           $res = mysql_query($query);
-
-        if ($count_records == 0) {
-            echo '<div class="good_message">';
-            echo 'На сайте пока не оставлено ни одного отзыва :( Вы можете сделать это первым!';
-            echo '</div>';
-        } elseif ($count_records <> 0){
-
-            // Вывод результатов
-            while ($row = mysql_fetch_assoc($res))
-            {
-                echo '<div class="rewiew">';
-                echo '<div class="rewiew__name">';
-                echo '<span>'.$row['name'].'</span>';
-                echo '</div>';
-                echo '<div class="rewiew__text">';
-                echo '<p class="page__text page__text--rewiew">'.$row['rewiew'].'</p>';
-                echo '</div>';
-                echo '</div>';
-            }
-
-            // Вывод списка страниц
-            echo '<p class="page__text page__text--guestbook">';
-            for ($page = 1; $page <= $num_pages; $page++)
-            {
-                if ($page == $current_page)
+                // Если текущая страница меньше единицы, то страница равна 1
+                if ($current_page < 1)
                 {
-                    echo '<strong>'.$page.'</strong> &nbsp;';
+                    $current_page = 1;
                 }
-                else
+                // Если текущая страница больше общего количества страница, то
+                // текущая страница равна количеству страниц
+                elseif ($current_page > $num_pages)
                 {
-                    echo '<a class="page__link" href="guestbook.php?page='.$page.'">'.$page.'</a> &nbsp;';
+                    $current_page = $num_pages;
                 }
-            } ?>
-            <a href="/guestbook/add" class="left-nav__link left-nav__link--rewiew">Добавить отзыв на сайт</a>
-        <?php
-        }
-           echo '</p>';
-           } elseif ($guestbook == 'add') { ?>
-           <h2 class="page__title-h2">Оставить свой отзыв о нас!</h2>
-           <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/forms/rewiewform.php");
-           }  ?>
+
+                // Начать получение данных от числа (текущая страница - 1) * количество записей на странице
+                $start_from = ($current_page - 1) * $on_page;
+
+                // Формат оператора LIMIT <ЗАПИСЬ ОТ>, <КОЛИЧЕСТВО ЗАПИСЕЙ>
+                $query = "SELECT `name`, `phone`, `rewiew` FROM `guestbook` ORDER BY `id` DESC LIMIT $start_from, $on_page";
+                $res = mysql_query($query);
+
+                if ($count_records == 0) {
+                    echo '<div class="good_message">';
+                    echo 'На сайте пока не оставлено ни одного отзыва :( Вы можете сделать это первым!';
+                    echo '</div>';
+                } elseif ($count_records <> 0){
+
+                    // Вывод результатов
+                    while ($row = mysql_fetch_assoc($res))
+                    {
+                        echo '<div class="rewiew">';
+                        echo '<div class="rewiew__name">';
+                        echo '<span>'.$row['name'].'</span>';
+                        echo '</div>';
+                        echo '<div class="rewiew__text">';
+                        echo '<p class="page__text page__text--rewiew">'.$row['rewiew'].'</p>';
+                        echo '</div>';
+                        echo '</div>';
+                    }
+
+                    // Вывод списка страниц
+                    echo '<p class="page__text page__text--guestbook">';
+                    for ($page = 1; $page <= $num_pages; $page++)
+                    {
+                        if ($page == $current_page)
+                        {
+                            echo '<strong>'.$page.'</strong> &nbsp;';
+                        }
+                        else
+                        {
+                            echo '<a class="page__link" href="guestbook.php?page='.$page.'">'.$page.'</a> &nbsp;';
+                        }
+                    } ?>
+                    <a href="/guestbook/add" class="left-nav__link left-nav__link--rewiew">Добавить отзыв на сайт</a>
+                    <?php
+                }
+                echo '</p>'; ?></div>
+
+            <div class="guestbook__tab guestbook__rewiews-add">
+                <h2 class="page__title-h2">Оставить свой отзыв о нас!</h2>
+                <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/forms/rewiewform.php"); ?>
+            </div>
         </div>
     </div>
     <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/footer/footer.html");
