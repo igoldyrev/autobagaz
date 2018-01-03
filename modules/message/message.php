4ea748c21a3fce104d2c32ed251c7e3b6d320595
igoldyrev <goldirev12@gmail.com>
2018-01-04 00:08:31 +0500

    Модуль переписан на синтаксисе mysqli

--- modules/message/message.php
+++ modules/message/message.php
@@ -1,30 +1,16 @@
 <?php
-$sdd_db_host='localhost'; // хост
-$sdd_db_name='9082410193_news'; // бд
-$sdd_db_user='9082410193'; // пользователь бд
-$sdd_db_pass='GfhjkmDatabase'; // пароль к бд
-$conn = mysql_connect($sdd_db_host,$sdd_db_user,$sdd_db_pass); // коннект с сервером бд
-mysql_query ("set_client='utf8'");//Следующие 2 строки решают проблему с кодировкой.
-mysql_query ("SET NAMES utf8");
-if(!$conn)
-{
-    throw new Exception('Не удалось подключиться к базе данных! Проверьте параметры подключения');
-}
-if(!mysql_select_db($sdd_db_name, $conn)) // выбор бд
-{
-    throw new Exception("Не удалось выбрать базу данных {$ssd_db_name}!");
-}
-$result = mysql_query('SELECT * FROM `message` ORDER BY id DESC', $conn); // запрос на выборку
-if(!$result)
-{
-    throw new Exception(sprintf('Не удалось выполнить запрос к БД, код ошибки %d, текст ошибки: %s', mysql_errno($conn), mysql_error($conn)));
-}
+$dbname = "9082410193_news";
 
-while ($row = mysql_fetch_assoc($result))
-{ ?>
-    <div class="index-message">
-        <h2 class="index-message__title"><?php echo $row['title'] ?></h2>
-        <p class="index-message__text"><?php echo $row['msg'] ?></p>
-        <p class="index-message__holiday"><?php echo $row['holiday'] ?></p>
-    </div><?php
+include ($_SERVER["DOCUMENT_ROOT"]."/modules/connectdb.php");
+
+$query = "SELECT * FROM `message` ORDER BY id DESC";
+
+if ($result = mysqli_query($connect, $query)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<div class="index-message">';
+        echo '<h2 class="index-message__title">'.$row['title'].'</h2>';
+            echo '<p class="index-message__text">'.$row['msg'].'</p>';
+            echo '<p class="index-message__holiday">'.$row['holiday'].'</p>';
+        echo '</div>';
+    }
 }
