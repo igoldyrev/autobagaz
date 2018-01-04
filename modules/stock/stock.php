84d86d75699dd35fd86ee0f1c01188e5d7650a45
igoldyrev <goldirev12@gmail.com>
2018-01-04 13:01:38 +0500

    Модуль переписан на синтаксисе mysqli

--- modules/stock/stock.php
+++ modules/stock/stock.php
@@ -1,38 +1,23 @@
 <?php
-$sdd_db_host='localhost'; // хост
-$sdd_db_name='9082410193_news'; // бд
-$sdd_db_user='9082410193'; // пользователь бд
-$sdd_db_pass='GfhjkmDatabase'; // пароль к бд
-$conn = mysql_connect($sdd_db_host,$sdd_db_user,$sdd_db_pass); // коннект с сервером бд
-mysql_query ("set_client='utf8'");//Следующие 2 строки решают проблему с кодировкой.
-mysql_query ("SET NAMES utf8");
-if(!$conn)
-{
-    throw new Exception('Не удалось подключиться к базе данных! Проверьте параметры подключения');
-}
-if(!mysql_select_db($sdd_db_name, $conn)) // выбор бд
-{
-    throw new Exception("Не удалось выбрать базу данных {$ssd_db_name}!");
-}
-$result = mysql_query('SELECT * FROM `news` ORDER BY news_id DESC LIMIT 1', $conn); // запрос на выборку
-if(!$result)
-{
-    throw new Exception(sprintf('Не удалось выполнить запрос к БД, код ошибки %d, текст ошибки: %s', mysql_errno($conn), mysql_error($conn)));
-}
+$dbname = "9082410193_news";
 
-while ($row = mysql_fetch_assoc($result))
-{ ?>
-    <div class="stock__container">
-    <div class="stock__bg"></div>
-    <div class="stock">
-        <div class="stock__block">
-            <h2 class="stock__title"><?php echo $row['news_title'] ?></h2>
-            <p class="stock__description"><?php echo $row['news_annotation'] ?></p>
-            <a onclick="yaCounter40650914.reachGoal('banner_click'); return true" class="stock__link" href="<?php echo $row['news_link'] ?>">Участвовать в розыгрыше</a>
-        </div>
-        <div class="stock__block">
-            <img class="stock__image" src="/images/stock/new-year.jpg">
-        </div>
-    </div>
-    </div><?php
+include ($_SERVER["DOCUMENT_ROOT"]."/modules/connectdb.php");
+
+$query = "SELECT * FROM `news` ORDER BY news_id DESC LIMIT 1";
+
+
+if ($result = mysqli_query($connect, $query)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<div class="stock__container">';
+        echo '<div class="stock__bg"></div>';
+            echo '<div class="stock">';
+                echo '<div class="stock__block">';
+                echo '<h2 class="stock__title">'.$row['news_title'].'</h2>';
+                echo '<p class="stock__description">'.$row['news_annotation'].'</p>';
+                echo '<a onclick="yaCounter40650914.reachGoal(\'banner_click\'); return true" class="stock__link" href="'.$row['news_link'].'">Участвовать в розыгрыше</a>';
+            echo '</div>';
+        echo '<div class="stock__block">';
+            echo '<img class="stock__image" src="/images/stock/new-year.jpg">';
+        echo '</div></div></div>';
+    }
 }
