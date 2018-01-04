d7505f5f32a608f1a9a06a8087649fa2580ca432
igoldyrev <goldirev12@gmail.com>
2018-01-04 18:07:48 +0500

    Странице добавлены стили

--- admin/news/delete.php
+++ admin/news/delete.php
@@ -1,15 +1,18 @@
 <?php echo "<title>Новость удалена!</title>";
+include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
 
-$dbname = "9082410193_news";
+<div class="admin__container">
+    <?php
+    $dbname = "9082410193_news";
 
-include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html");
-include ($_SERVER["DOCUMENT_ROOT"]."/modules/connectdb.php");
+    include ($_SERVER["DOCUMENT_ROOT"]."/modules/connectdb.php");
 
-$id=$_REQUEST['news'];
-$delete_sql = "DELETE FROM news WHERE news_id=$id";
-mysqli_query($connect, $delete_sql) or die("<p>При удалении новости произошла ошибка</p>". mysqli_error());
-echo "<h3>Новость успешно удалена!</h3>";
-?>
-
-<a href="/admin/news/newsdelete.php">Вернуться к выбору новостей</a><br>
-<a href="/admin/index.php">Вернуться на главную админки</a>
+    $id=$_REQUEST['news'];
+    $delete_sql = "DELETE FROM news WHERE news_id=$id";
+    mysqli_query($connect, $delete_sql) or die("<p class='page__text'>При удалении новости произошла ошибка</p>". mysqli_error());
+    echo "<h3 class='page__title-h3'>Новость успешно удалена!</h3>"; ?>
+    <div class="admin__link-down clearfix">
+        <a class="admin__link" href="/admin/news/newsdelete.php">Вернуться к выбору новостей</a>
+        <a class="admin__link" href="/admin/index.php">Вернуться на главную админки</a>
+    </div>
+</div>
