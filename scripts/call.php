5f84022a57499330b9297ea712d177b1b4d6aed8
igoldyrev <goldirev12@gmail.com>
2018-01-08 16:31:09 +0500

    Обработчик для отправки заявок для проката переписан на mysqli, а также ему добавлены стили

--- scripts/call.php
+++ scripts/call.php
@@ -1,57 +1,75 @@
 <?php
 session_start();
-$url = $_SESSION['url']; 
-// Соединение с БД MySQL
-$sql = mysql_connect('localhost', '9082410193', 'GfhjkmDatabase');
-mysql_select_db('9082410193_zakaz', $sql);
-mysql_query ("set_client='utf8'");//Следующие 2 строки решают проблему с кодировкой.
-mysql_query ("SET NAMES utf8");
+include($_SERVER["DOCUMENT_ROOT"] . "/modules/keywords.php");
+include($_SERVER["DOCUMENT_ROOT"] . "/admin/headtags.html");
+echo "<title>".$keywords[41][title]; echo "</title>";?>
 
-//В файле на первом этапе нужно принять данные из пост массива. Для этого создаем переменные
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-//Первая функция преобразует все символы, которые пользователь попытается добавить в форму
-$name = htmlspecialchars($name);
-$phone = htmlspecialchars($phone);
-//Вторая функция декодирует url, если пользователь попытается его добавить в форму
-$name = urldecode($name);
-$phone = urldecode($phone);
-//Третьей функцией мы удалим пробелы с начала и конца строки, если таковые имеются
-$name = trim($name);
-$phone = trim($phone);
-//Заносим данные из формы в переменные
-$name = $_REQUEST['name'];
-$phone = $_REQUEST['phone'];
+<div class="wrapper">
+    <div class="wrapper-content">
+        <div class="content">
+            <?php
+            $url = $_SESSION['url'];
 
-//Создаем запрос в базу данных
-$sql_insert = "INSERT INTO calls (name, phone)" . 
-"VALUES('{$name}', '{$phone}');";
-mysql_query($sql_insert);
+            // Соединение с БД MySQL
+            $dbname = "9082410193_zakaz";
 
-$sql_users = "INSERT INTO users (name, phone)" . 
-"VALUES('{$name}', '{$phone}');";
-mysql_query($sql_users);
+            include($_SERVER["DOCUMENT_ROOT"] . "/modules/connectdb.php");
 
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){
+            //В файле на первом этапе нужно принять данные из пост массива. Для этого создаем переменные
+            $name = $_POST['name'];
+            $phone = $_POST['phone'];
+            //Первая функция преобразует все символы, которые пользователь попытается добавить в форму
+            $name = htmlspecialchars($name);
+            $phone = htmlspecialchars($phone);
+            //Вторая функция декодирует url, если пользователь попытается его добавить в форму
+            $name = urldecode($name);
+            $phone = urldecode($phone);
+            //Третьей функцией мы удалим пробелы с начала и конца строки, если таковые имеются
+            $name = trim($name);
+            $phone = trim($phone);
+            //Заносим данные из формы в переменные
+            $name = $_REQUEST['name'];
+            $phone = $_REQUEST['phone'];
 
-if (mail("autobagaz@yandex.ru", "Звонок с сайта!!!",
-"Страница с которой пришло письмо ".$_SESSION['url'].";
-Имя:".$name.";
-Телефон: ".$phone.";
+            //Создаем запрос в базу данных
+//            $sql_insert = "INSERT INTO calls (name, phone)" .
+//                "VALUES('{$name}', '{$phone}');";
+//            mysqli_query($sql_insert);
+//
+//            $sql_users = "INSERT INTO users (name, phone)" .
+//                "VALUES('{$name}', '{$phone}');";
+//            mysqli_query($sql_users);
 
-Техническая информация:
-Примерный user-agent: ".$_SERVER['HTTP_USER_AGENT'].";
-ip-адрес:" .$_SERVER['REMOTE_ADDR'].";
-Ссылка на скрипт, с которого пришло письмо:" .$_SERVER['REQUEST_URI'] ,
-"From: autobagaz@yandex.ru \r\n"))
- {     	echo "<center><b>Мы Вам обязательно перезвоним!</b><br><br><center>Через 3 секунды Вы будете перенаправлены на предыдущую страницу<br><br>Если этого не произошло, то нажмите на ссылку:<br><a href='"; echo $_SESSION['url']; echo"'>Вернуться назад</a>"; 
-header('Refresh: 3; URL='.$url);
-} 
-else { 
-    echo "<center>При отправке данных возникли проблемы :(<br><a href='"; echo $_SESSION['url']; echo "'>Вернуться назад</a>";
-}}
-else {
-	echo "<center>Вы не заполнили одно из обязательных полей формы, вернитесь, пожалуйста, и заполните его<br><a href='"; echo $_SESSION['url']; echo "'>Вернуться назад</a>";
-}
-unset($_SESSION['url']);
-include ($_SERVER["DOCUMENT_ROOT"]."/modules/counters.html");?>
+            include($_SERVER["DOCUMENT_ROOT"] . "/modules/mail/call.php");
+
+            if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){
+
+                if (mail("goldirev12@yandex.ru", "Звонок с сайта!!!",
+                    $call, "From: autobagaz@yandex.ru \r\n")){
+                    echo "<div class='good_message'>";
+                    echo "<p>Мы Вам обязательно перезвоним!</p>";
+                    echo "</div>";
+                    echo "<p class='page__text page__text--notification'>Через 5 секунд Вы будете перенаправлены на предыдущую страницу</p>";
+                    echo "<p class='page__text page__text--notification'>Если этого не произошло, то нажмите на ссылку:</p>";
+                    echo "<a class='page__link page__link--notification page__link--sitemap' href='" . $url . "'>Вернуться назад</a>";
+                    header('Refresh: 5; URL='.$url);
+                }
+                else {
+                    echo "<div class='good_message good_message--wrong'>";
+                    echo "<p class='page__text page__text--notification'>При отправке данных возникли проблемы :(</p>";
+                    echo "</div>";
+                    echo "<a class='page__link page__link--notification page__link--sitemap' href='".$url."'>Вернуться назад</a>";
+                }
+            }
+            else {
+                echo "<div class='good_message good_message--wrong'>";
+                echo "<p class='page__text page__text--notification'>Вы не заполнили одно из обязательных полей формы, вернитесь, пожалуйста, и заполните его</p>";
+                echo "</div>";
+                echo "<a class='page__link page__link--notification page__link--sitemap' href='".$url."'>Вернуться назад</a>";
+            }
+            unset($_SESSION['url']);
+            ?>
+        </div>
+    </div>
+    <?php  include ($_SERVER["DOCUMENT_ROOT"]."/modules/counters.html"); ?>
+</div>
