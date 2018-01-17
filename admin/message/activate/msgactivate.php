9fb184aeb80a11b807b14645b1566c0f7baf6656
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-17 18:44:20 +0500

    Внесены правки в макет с формой

--- admin/message/activate/msgactivate.php
+++ admin/message/activate/msgactivate.php
@@ -6,12 +6,13 @@ include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
     <h1 class="page__title-h1">Выключение объявления на главной странице</h1>
 
     <div class="form__container">
+        <p class="page__text" style="margin-bottom: 15px">Если галочка установлена - то блок отображается на странице. Если она убрана - то не отображается.</p>
         <form action="/admin/message/activate/activate.php" method="post" class="form">
             <div class="form__input-block clearfix">
                 <input type="checkbox" name="msg_on" class="form__checkbox" id="msgon" <?php echo $checkedmsgon ?>>
-                <label for="msgon" class="form__label--checkbox">Выключить блок</label>
+                <label for="msgon" class="form__label--checkbox">Включить/выключить блок</label>
             </div>
-            <input type="submit" class="button button__zakaz" value="Выключить блок">
+            <input type="submit" class="button button__zakaz" value="Включить/выключить блок">
         </form>
     </div>
     <div class="admin__link-down clearfix">
