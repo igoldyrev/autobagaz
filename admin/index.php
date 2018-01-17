1eeb9938901a3bff86f76e9b43946637cfbbca4a
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-17 16:58:13 +0500

    Добавлена ссылка на страницу с выключением блока

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,7 @@ echo "<title>Админка сайта Автобагаж.ру!</title>"; ?>
         <?php echo "<h3 class='page__title-h3'>Панель управления обьявлением</h3>"; ?>
         <p class="page__text">Это та запись на главной странице, в которой говорится, что мы не работаем тогда-то...</p>
         <a class="admin__link" href="/admin/message/update/msgupdate.php">Редактировать обьявление</a>
+        <a class="admin__link" href="/admin/message/activate/msgactivate.php">Выключить обьявление</a>
     </div>
 
     <div class="admin clearfix">
