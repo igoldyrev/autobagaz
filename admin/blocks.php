13f14ed68140cb9e56f1f26d5750983a199ffa2f
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-18 13:36:10 +0500

    Модуль, который назначает стили нужным блокам

--- admin/blocks.php
+++ admin/blocks.php
@@ -1,7 +1,12 @@
 <?php
 include ($_SERVER["DOCUMENT_ROOT"]."/admin/config.php");
-if ($checkedmsgon = 'checked'){
-
-}elseif ($checkedmsgon = 'none'){
-
+if ($checkedmsgon == 'checked'){
+    $style = 'style="display: flex; "';
+}elseif ($checkedmsgon == 'none'){
+    $style = 'style="display: none; "';
+}
+if ($checkedstock == 'checked'){
+    $style = 'style="display: block; "';
+}elseif ($checkedstock == 'none'){
+    $style = 'style="display: none; "';
 }
