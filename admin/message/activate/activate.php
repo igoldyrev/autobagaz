557eea179199256be91544976f11de0c12c6c7e8
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-18 13:35:34 +0500

    Написаны условия для активации и деактивации блока с акцией

--- admin/message/activate/activate.php
+++ admin/message/activate/activate.php
@@ -10,19 +10,30 @@ include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
     $head = "<?php \n";
     file_put_contents($config, $head);
     if(isset($_POST['msg_on']) == true ){
-        echo '<h1 class="page__title-h1">Блок включен</h1>';
+        echo '<h2 class="page__title-h2">Блок с обьявлением включен</h2>';
         $checkedmsgon = '$checkedmsgon';
         $content = file_get_contents($config);
         $content .= "$checkedmsgon = 'checked';\n ";
         file_put_contents($config, $content);
-        echo "$content";
-    } else {
-        echo '<h1 class="page__title-h1">Блок выключен</h1>';
+    } elseif(isset($_POST['msg_on']) == false ) {
+        echo '<h2 class="page__title-h2">Блок с обьявлением выключен</h2>';
         $checkedmsgon = '$checkedmsgon';
         $content = file_get_contents($config);
         $content .= "$checkedmsgon = 'none';\n ";
         file_put_contents($config, $content);
-        echo "$content";
+    }
+    if(isset($_POST['stock_on']) == true ){
+        echo '<h2 class="page__title-h2">Блок с акцией включен</h2>';
+        $checkedstock = '$checkedstock';
+        $contents = file_get_contents($config);
+        $contents .= "$checkedstock = 'checked';\n ";
+        file_put_contents($config, $contents);
+    } elseif (isset($_POST['stock_on']) == false){
+        echo '<h2 class="page__title-h2">Блок с акцией выключен</h2>';
+        $checkedstock = '$checkedstock';
+        $contents = file_get_contents($config);
+        $contents .= "$checkedstock = 'none';\n ";
+        file_put_contents($config, $contents);
     }
     ?>
     <div class="admin__link-down clearfix">
