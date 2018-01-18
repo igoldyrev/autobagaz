35d1afb42614db71ff0e98909e7a12b0088006d6
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-18 13:34:54 +0500

    Добавлены условия проверки включен ли блок и в зависимости от этого ставится чекбокс. Добавлен чексбокс для акьтивации блока с акцией

--- admin/message/activate/msgactivate.php
+++ admin/message/activate/msgactivate.php
@@ -1,6 +1,20 @@
 <?php
 echo "<title>Выключить обьявление</title>";
-include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
+include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/admin/config.php");
+
+if ($checkedmsgon == 'checked'){
+    $checkedmsgon = "checked";
+}elseif ($checkedmsgon == 'none'){
+    $checkedmsgon = "";
+}elseif ($checkedstock == 'checked'){
+    $checkedstock = "checked";
+}elseif ($checkedstock == 'none'){
+    $checkedstock = "";
+}
+
+
+?>
 
 <div class="admin__container">
     <h1 class="page__title-h1">Выключение объявления на главной странице</h1>
@@ -10,9 +24,13 @@ include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
         <form action="/admin/message/activate/activate.php" method="post" class="form">
             <div class="form__input-block clearfix">
                 <input type="checkbox" name="msg_on" class="form__checkbox" id="msgon" <?php echo $checkedmsgon ?>>
-                <label for="msgon" class="form__label--checkbox">Включить/выключить блок</label>
+                <label for="msgon" class="form__label--checkbox">Включить/выключить сообщение</label>
             </div>
-            <input type="submit" class="button button__zakaz" value="Включить/выключить блок">
+            <div class="form__input-block clearfix">
+                <input type="checkbox" name="stock_on" class="form__checkbox" id="stockon" <?php echo $checkedstock ?>>
+                <label for="stockon" class="form__label--checkbox">Включить/выключить блок с акцией</label>
+            </div>
+            <input type="submit" class="button button__zakaz" value="Включить/выключить блоки">
         </form>
     </div>
     <div class="admin__link-down clearfix">
