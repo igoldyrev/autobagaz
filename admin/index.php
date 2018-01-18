b2fa057c86257579c7a366900adcf8f003b6d193
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-18 12:52:43 +0500

    Правки в карточку с блоками

--- admin/index.php
+++ admin/index.php
@@ -12,10 +12,10 @@ echo "<title>Админка сайта Автобагаж.ру!</title>"; ?>
     </div>
 
     <div class="admin clearfix">
-        <?php echo "<h3 class='page__title-h3'>Панель управления обьявлением</h3>"; ?>
-        <p class="page__text">Это та запись на главной странице, в которой говорится, что мы не работаем тогда-то...</p>
+        <?php echo "<h3 class='page__title-h3'>Панель управления блоками</h3>"; ?>
+        <p class="page__text">Блок Акция и блок с обьявлением на главной странице.</p>
         <a class="admin__link" href="/admin/message/update/msgupdate.php">Редактировать обьявление</a>
-        <a class="admin__link" href="/admin/message/activate/msgactivate.php">Выключить обьявление</a>
+        <a class="admin__link" href="/admin/message/activate/msgactivate.php">Выключить/включить блоки</a>
     </div>
 
     <div class="admin clearfix">
