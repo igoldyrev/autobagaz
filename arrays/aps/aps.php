0eb4576f925c1b4740f9a2f64249ca1045dd019c
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-31 13:50:42 +0500

    features / aps. Массив с данными для рейлингов моделей Niva, Kia, Opel, Land Rover

---
+++ arrays/aps/aps.php
@@ -0,0 +1,154 @@
+<?php
+$niva = [
+    //Niva M
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'nivamblack',
+        'id2' => 'nivamgray',
+        'name1' => 'Рейлинг для Chevrolet Niva M полимер черный',
+        'name2' => 'Рейлинг для Chevrolet Niva M анод серый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Chevrolet Niva M',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/chevrolet/niva-m/polymer-black.gif" srcset="/images/aps/chevrolet/niva-m/polymer-black.gif 800w" alt="Рейлинги для Chevrolet Niva M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/chevrolet/niva-m/anod-gray.gif" srcset="/images/aps/chevrolet/niva-m/anod-gray.gif 800w" alt="Рейлинги для Chevrolet Niva M" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        //Цена без монтажа
+        'price' => '5 700 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 100 рублей',
+    ],
+
+    //Niva L
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'nivalblack',
+        'id2' => 'nivalgray',
+        'id3' => 'nivalwhite',
+        'name1' => 'Рейлинг для Chevrolet Niva L полимер черный',
+        'name2' => 'Рейлинг для Chevrolet Niva L анод серый',
+        'name3' => 'Рейлинг для Chevrolet Niva L серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Chevrolet Niva L',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/chevrolet/niva-l/polymer-black.png" srcset="/images/aps/chevrolet/niva-l/polymer-black.png 800w" alt="Рейлинги для Chevrolet Niva L" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/chevrolet/niva-l/anod-gray.png" srcset="/images/aps/chevrolet/niva-l/anod-gray.png 800w" alt="Рейлинги для Chevrolet Niva L" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/chevrolet/niva-l/white.png" srcset="/images/aps/chevrolet/niva-l/white.png 800w" alt="Рейлинги для Chevrolet Niva L" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '5 700 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 100 рублей',
+    ],
+];
+
+$kia = [
+    //ceed 2006-2012
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'ceed2006black',
+        'id2' => 'ceed2006gray',
+        'id3' => 'ceed2006white',
+        'name1' => 'Рейлинг для KIA CEED (2006-2012) полимер черный',
+        'name2' => 'Рейлинг для KIA CEED (2006-2012) анод серый',
+        'name3' => 'Рейлинг для KIA CEED (2006-2012) серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для KIA CEED (2006-2012)',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/kia/ceed-2006-2012/polymer-black.png" srcset="/images/aps/kia/ceed-2006-2012/polymer-black.png 800w" alt="Рейлинги для KIA CEED" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/kia/ceed-2006-2012/anod-gray.png" srcset="/images/aps/kia/ceed-2006-2012/anod-gray.png 800w" alt="Рейлинги для KIA CEED" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/kia/ceed-2006-2012/wnite.png" srcset="/images/aps/kia/ceed-2006-2012/wnite.png 800w" alt="Рейлинги для KIA CEED" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '5 950 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 350 рублей',
+    ],
+
+    //ceed 2012
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'ceed2012black',
+        'id2' => 'ceed2012gray',
+        'id3' => 'ceed2012white',
+        'name1' => 'Рейлинг для KIA CEED (2012-н.в.) полимер черный',
+        'name2' => 'Рейлинг для KIA CEED (2012-н.в.) анод серый',
+        'name3' => 'Рейлинг для KIA CEED (2012-н.в.) серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для KIA CEED (2012-н.в.)',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/kia/ceed-new/polymer-black-vkladka.png" srcset="/images/aps/kia/ceed-new/polymer-black-vkladka.png 800w" alt="Рейлинги для KIA CEED" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/kia/ceed-new/anod-gray-vkladka.png" srcset="/images/aps/kia/ceed-new/anod-gray-vkladka.png 800w" alt="Рейлинги для KIA CEED" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/kia/ceed-new/wnite.png" srcset="/images/aps/kia/ceed-new/wnite.png 800w" alt="Рейлинги для KIA CEED" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный с вкладкой',
+        'color2' => 'анод серый с вкладкой',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '6 150 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 550 рублей',
+    ],
+];
+
+$opel = [
+    //ASTRA (H) FAMILY (2004-2014)
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'astrablack',
+        'id2' => 'astragray',
+        'id3' => 'astrawhite',
+        'name1' => 'Рейлинг для ASTRA (H) FAMILY (2004-2014) полимер черный',
+        'name2' => 'Рейлинг для ASTRA (H) FAMILY (2004-2014) анод серый',
+        'name3' => 'Рейлинг для ASTRA (H) FAMILY (2004-2014) серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для ASTRA (H) FAMILY (2004-2014)',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/opel/astra-2004-2014/polymer-black.png" srcset="/images/aps/opel/astra-2004-2014/polymer-black.png 800w" alt="Рейлинги для OPEL ASTRA" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/opel/astra-2004-2014/anod-gray.png" srcset="/images/aps/opel/astra-2004-2014/anod-gray.png 800w" alt="Рейлинги для OPEL ASTRA" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/opel/astra-2004-2014/white.png" srcset="/images/aps/opel/astra-2004-2014/white.png 800w" alt="Рейлинги для OPEL ASTRA" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '6 150 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 550 рублей',
+    ],
+];
+
+$landrover = [
+    //freelander 2 2006 - н.в.
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'freelanderblack',
+        'id2' => 'freelandergray',
+        'id3' => 'freelanderwhite',
+        'name1' => 'Рейлинг для Land Rover Freelander 2 (2006 - н.в.) полимер черный',
+        'name2' => 'Рейлинг для Land Rover Freelander 2 (2006 - н.в.) анод серый',
+        'name3' => 'Рейлинг для Land Rover Freelander 2 (2006 - н.в.) серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Land Rover Freelander 2 (2006 - н.в.)',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/landrover/freelander-2/polymer-black.png" srcset="/images/aps/landrover/freelander-2/polymer-black.png 800w" alt="Рейлинги для Land Rover Freelander 2" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/landrover/freelander-2/anod-gray.png" srcset="/images/aps/landrover/freelander-2/anod-gray.png 800w" alt="Рейлинги для Land Rover Freelander 2" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/landrover/freelander-2/white.png" srcset="/images/aps/landrover/freelander-2/white.png 800w" alt="Рейлинги для Land Rover Freelander 2" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '8 200 рублей',
+        //Цена с монтажом
+        'price_montazh' => '8 600 рублей',
+    ],
+];
