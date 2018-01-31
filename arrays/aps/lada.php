35d40df71c9a4b839db73bbc2a7c4006ee240a4e
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-31 11:49:07 +0500

    features / aps. Массив с данными для рейлингов моделей Лада

---
+++ arrays/aps/lada.php
@@ -0,0 +1,237 @@
+<?php
+$lada = [
+    //Лада XRAY
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'xrayblack',
+        'id2' => 'xraygray',
+        'id3' => 'xraywhite',
+        'name1' => 'Рейлинг для Лада XRAY полимер черный',
+        'name2' => 'Рейлинг для Лада XRAY анод серый',
+        'name3' => 'Рейлинг для Лада XRAY серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Лада XRAY',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/xray/polymer-black.png" srcset="/images/aps/lada/xray/polymer-black.png 800w" alt="Рейлинги для Лада XRAY" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/xray/anod-gray.png" srcset="/images/aps/lada/xray/anod-gray.png 800w" alt="Рейлинги для Лада XRAY" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/lada/xray/white.png" srcset="/images/aps/lada/xray/white.png 800w" alt="Рейлинги для Лада XRAY" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '7 300 рублей',
+        //Цена с монтажом
+        'price_montazh' => '7 700 рублей',
+    ],
+
+    //Лада LARGUS
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'largusblack',
+        'id2' => 'largusgray',
+        'id3' => 'larguswhite',
+        'name1' => 'Рейлинг для Лада LARGUS полимер черный',
+        'name2' => 'Рейлинг для Лада LARGUS анод серый',
+        'name3' => 'Рейлинг для Лада LARGUS серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Лада LARGUS',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/largus/polymer-black.png" srcset="/images/aps/lada/largus/polymer-black.png 800w" alt="Рейлинги для Лада LARGUS" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/largus/anod-gray.png" srcset="/images/aps/lada/largus/anod-gray.png 800w" alt="Рейлинги для Лада LARGUS" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/lada/largus/white.png" srcset="/images/aps/lada/largus/white.png 800w" alt="Рейлинги для Лада LARGUS" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '6 650 рублей',
+        //Цена с монтажом
+        'price_montazh' => '7 050 рублей',
+    ],
+
+    //Гранта Лифтбек
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'liftbekblack',
+        'id2' => 'liftbekgray',
+        'id3' => 'liftbekwhite',
+        'name1' => 'Рейлинг для Гранта Лифтбек полимер черный',
+        'name2' => 'Рейлинг для Гранта Лифтбек анод серый',
+        'name3' => 'Рейлинг для Гранта Лифтбек серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Гранта Лифтбек',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/granta-liftbek/polymer-black.png" srcset="/images/aps/lada/granta-liftbek/polymer-black.png 800w" alt="Рейлинги для Гранта Лифтбек" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/granta-liftbek/anod-gray.png" srcset="/images/aps/lada/granta-liftbek/anod-gray.png 800w" alt="Рейлинги для Гранта Лифтбек" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/lada/granta-liftbek/white.png" srcset="/images/aps/lada/granta-liftbek/white.png 800w" alt="Рейлинги для Гранта Лифтбек" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '3 750 рублей',
+        //Цена с монтажом
+        'price_montazh' => '4 150 рублей',
+    ],
+
+    //Гранта "E" седан
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'grantaeblack',
+        'id2' => 'grantaegray',
+        'id3' => 'grantaeblackpoperechina',
+        'id4' => 'grantaegraypoperechina',
+        'name1' => 'Рейлинг для Гранта "Е" седан полимер черный',
+        'name2' => 'Рейлинг для Гранта "Е" седан анод серый',
+        'name3' => 'Рейлинг для Гранта "Е" седан полимер черный с поперечиной',
+        'name4' => 'Рейлинг для Гранта "Е" седан анод серый с поперечиной',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Гранта "Е" седан',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/granta-e/polymer-black.png" srcset="/images/aps/lada/granta-liftbek/polymer-black.png 800w" alt="Рейлинги для Гранта E седан" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/granta-e/anod-gray.png" srcset="/images/aps/lada/granta-liftbek/anod-gray.png 800w" alt="Рейлинги для Гранта E седан" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/lada/granta-e/polymer-black-poperechina.png" srcset="/images/aps/lada/granta-e/polymer-black-poperechina.png 800w" alt="Рейлинги для Гранта E седан" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img4' => '<img class="img_main" src="/images/aps/lada/granta-e/anod-gray-poperechina.png" srcset="/images/aps/lada/granta-e/anod-gray-poperechina.png 800w" alt="Рейлинги для Гранта E седан" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'полимер черный с поперечиной',
+        'color4' => 'анод серый с поперечиной',
+        //Цена без монтажа
+        'price' => '2 950 рублей',
+        'price_poperechina' => '3 950 рублей',
+        //Цена с монтажом
+        'price_montazh' => '3 350 рублей',
+        'price_montazh_poperechina' => '4 450 рублей',
+    ],
+
+    //Гранта Седан, Калина Седан
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'grantablack',
+        'id2' => 'grantagray',
+        'id3' => 'grantawhite',
+        'name1' => 'Рейлинг для Лада Гранта седан / Лада Калина седан полимер черный',
+        'name2' => 'Рейлинг для Лада Гранта седан / Лада Калина седан анод серый',
+        'name3' => 'Рейлинг для Лада Гранта седан / Лада Калина седан серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Лада Гранта седан / Лада Калина седан',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/granta-sedan/polymer-black.gif" srcset="/images/aps/lada/granta-sedan/polymer-black.gif 800w" alt="Рейлинги для Гранта Седан" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/granta-sedan/anod-gray.gif" srcset="/images/aps/lada/granta-sedan/anod-gray.gif 800w" alt="Рейлинги для Гранта Седан" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/lada/granta-sedan/white.png" srcset="/images/aps/lada/granta-sedan/white.png 800w" alt="Рейлинги для Гранта Седан" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '3 500 рублей',
+        //Цена с монтажом
+        'price_montazh' => '3 900 рублей',
+    ],
+
+    //Калина Хэтчбек "S"
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'kalinaxetchbekblack',
+        'id2' => 'kalinaxetchbekgray',
+        'id3' => 'kalinaxetchbekwhite',
+        'name1' => 'Рейлинг для Лада Калина Хэтчбек "S" полимер черный',
+        'name2' => 'Рейлинг для Лада Калина Хэтчбек "S" анод серый',
+        'name3' => 'Рейлинг для Лада Калина Хэтчбек "S" серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Лада Калина Хэтчбек "S"',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/kalina-xetchbek/polymer-black.gif" srcset="/images/aps/lada/kalina-xetchbek/polymer-black.gif 800w" alt="Рейлинги для Калина Хэтчбек" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/kalina-xetchbek/anod-gray.gif" srcset="/images/aps/lada/kalina-xetchbek/anod-gray.gif 800w" alt="Рейлинги для Калина Хэтчбек" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/lada/kalina-xetchbek/white.png" srcset="/images/aps/lada/kalina-xetchbek/white.png 800w" alt="Рейлинги для Калина Хэтчбек" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '3 500 рублей',
+        //Цена с монтажом
+        'price_montazh' => '3 900 рублей',
+    ],
+
+    //Калина Универсал "М"
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'kalinauniversal2008black',
+        'id2' => 'kalinauniversal2008gray',
+        'id3' => 'kalinauniversal2013black',
+        'id4' => 'kalinauniversal2013gray',
+        'id5' => 'kalinauniversal2008cleanblack',
+        'id6' => 'kalinauniversal2013cleangray',
+        'name1' => 'Рейлинг для Лада Калина Универсал "М" полимер черный, а/м 2008-2012г.' ,
+        'name2' => 'Рейлинг для Лада Калина Универсал "М" анод серый, а/м 2008-2012г.',
+        'name3' => 'Рейлинг для Лада Калина Универсал "М" полимер черный, а/м 2013г.' ,
+        'name4' => 'Рейлинг для Лада Калина Универсал "М" анод серый, а/м 2013г.',
+        'name5' => 'Рейлинг для Лада Калина Универсал "М" полимер черный, крыша гладкая, а/м до 2008г.' ,
+        'name6' => 'Рейлинг для Лада Калина Универсал "М" анод серый, крыша гладкая, а/м до 2008г.',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Лада Калина Универсал "М"',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/kalina-universal/polymer-black-2008-2012.gif" srcset="/images/aps/lada/kalina-universal/polymer-black-2008-2012.gif 800w" alt="Рейлинги для Калина Универсал" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/kalina-universal/anod-gray-2008-2012.gif" srcset="/images/aps/lada/kalina-universal/anod-gray-2008-2012.gif 800w" alt="Рейлинги для Калина Универсал" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/lada/kalina-universal/polymer-black-2013.gif" srcset="/images/aps/lada/kalina-universal/polymer-black-2013.gif 800w" alt="Рейлинги для Калина Универсал" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img4' => '<img class="img_main" src="/images/aps/lada/kalina-universal/anod-gray-2013.gif" srcset="/images/aps/lada/kalina-universal/anod-gray-2013.gif 800w" alt="Рейлинги для Калина Универсал" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img5' => '<img class="img_main" src="/images/aps/lada/kalina-universal/polymer-black-do-2008-gladkaya-krysha.gif" srcset="/images/aps/lada/kalina-universal/polymer-black-do-2008-gladkaya-krysha.gif 800w" alt="Рейлинги для Калина Универсал" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img6' => '<img class="img_main" src="/images/aps/lada/kalina-universal/anod-gray-do-2008-gladkaya-krysha.gif" srcset="/images/aps/lada/kalina-universal/anod-gray-do-2008-gladkaya-krysha.gif 800w" alt="Рейлинги для Калина Универсал" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный, а/м 2008-2012г.',
+        'color2' => 'анод серый, а/м 2008-2012г.',
+        'color3' => 'полимер черный, а/м 2013г.',
+        'color4' => 'анод серый, а/м 2013г.',
+        'color5' => 'полимер черный, крыша гладкая, а/м до 2008г.',
+        'color6' => 'анод серый, крыша гладкая, а/м до 2008г.',
+        //Цена без монтажа
+        'price' => '3 900 рублей',
+        //Цена с монтажом
+        'price_montazh' => '4 300 рублей',
+    ],
+
+    //4x4
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => '4x4black',
+        'id2' => '4x4gray',
+        'name1' => 'Рейлинг для Лада 4x4 полимер черный',
+        'name2' => 'Рейлинг для Лада 4x4 анод серый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Лада 4x4',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/4x4/polymer-black.gif" srcset="/images/aps/lada/4x4/polymer-black.gif 800w" alt="Рейлинги для Лада 4x4" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/4x4/anod-gray.gif" srcset="/images/aps/lada/4x4/anod-gray.gif 800w" alt="Рейлинги для Лада 4x4" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        //Цена без монтажа
+        'price' => '5 700 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 100 рублей',
+    ],
+
+    //4x4 L
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => '4x4lblack',
+        'id2' => '4x4lgray',
+        'name1' => 'Рейлинг для Лада 4x4 L полимер черный',
+        'name2' => 'Рейлинг для Лада 4x4 L анод серый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Лада 4x4 L',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/lada/4x4-l/polymer-black.gif" srcset="/images/aps/lada/4x4-l/polymer-black.gif 800w" alt="Рейлинги для Лада 4x4 L" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/lada/4x4-l/anod-gray.gif" srcset="/images/aps/lada/4x4-l/anod-gray.gif 800w" alt="Рейлинги для Лада 4x4 L" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        //Цена без монтажа
+        'price' => '5 750 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 150 рублей',
+    ],
+];
