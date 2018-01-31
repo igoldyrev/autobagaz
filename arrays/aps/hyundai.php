6a231dd6b867471665db44bb00569a2f56804578
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-31 17:49:32 +0500

    features / aps. Массив с данными для рейлингов моделей Hyundai

---
+++ arrays/aps/hyundai.php
@@ -0,0 +1,77 @@
+<?php
+$hyundai = [
+    //Hyundai Creta
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'cretablack',
+        'id2' => 'cretagray',
+        'id3' => 'cretawhite',
+        'name1' => 'Рейлинг для Hyundai Creta полимер черный',
+        'name2' => 'Рейлинг для Hyundai Creta анод серый',
+        'name3' => 'Рейлинг для Hyundai Creta серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Hyundai Creta',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/hyundai/creta/polymer-black.png" srcset="/images/aps/hyundai/creta/polymer-black.png 800w" alt="Рейлинги для Hyundai Creta" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/hyundai/creta/anod-gray.png" srcset="/images/aps/hyundai/creta/anod-gray.png 800w" alt="Рейлинги для Hyundai Creta" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/hyundai/creta/white.png" srcset="/images/aps/hyundai/creta/white.png 800w" alt="Рейлинги для Hyundai Creta" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '7 400 рублей',
+        //Цена с монтажом
+        'price_montazh' => '7 800 рублей',
+    ],
+
+    //Hyundai Solaris
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'solarisblack',
+        'id2' => 'solarisgray',
+        'id3' => 'solariswhite',
+        'name1' => 'Рейлинг для Hyundai Solaris полимер черный',
+        'name2' => 'Рейлинг для Hyundai Solaris анод серый',
+        'name3' => 'Рейлинг для Hyundai Solaris серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Hyundai Solaris',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/hyundai/solaris/polymer-black.png" srcset="/images/aps/hyundai/solaris/polymer-black.png 800w" alt="Рейлинги для Hyundai Solaris" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/hyundai/solaris/anod-gray.png" srcset="/images/aps/hyundai/solaris/anod-gray.png 800w" alt="Рейлинги для Hyundai Solaris" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/hyundai/solaris/white.png" srcset="/images/aps/hyundai/solaris/white.png 800w" alt="Рейлинги для Hyundai Solaris" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '6 150 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 550 рублей',
+    ],
+
+    //Hyundai i30
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'i30black',
+        'id2' => 'i30gray',
+        'id3' => 'i30white',
+        'name1' => 'Рейлинг для Hyundai i30 полимер черный',
+        'name2' => 'Рейлинг для Hyundai i30 анод серый',
+        'name3' => 'Рейлинг для Hyundai i30 серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Hyundai i30 (2012 - н.в.)',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/hyundai/i-30/polymer-black.png" srcset="/images/aps/hyundai/i-30/polymer-black.png 800w" alt="Рейлинги для Hyundai i30" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/hyundai/i-30/anod-gray.png" srcset="/images/aps/hyundai/i-30/anod-gray.png 800w" alt="Рейлинги для Hyundai i30" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/hyundai/i-30/white.png" srcset="/images/aps/hyundai/i-30/white.png 800w" alt="Рейлинги для Hyundai i30" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '6 150 рублей',
+        //Цена с монтажом
+        'price_montazh' => '6 550 рублей',
+    ],
+];
