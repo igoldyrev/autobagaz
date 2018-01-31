68bc9502c61c3bdd3dd6626d9c5891daea5676fb
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-01-31 18:05:09 +0500

    features / aps. Массив с данными для рейлингов моделей Toyota

---
+++ arrays/aps/toyota.php
@@ -0,0 +1,77 @@
+<?php
+$toyota = [
+    //Rav 4 (2006 - 2013)
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'rav42006black',
+        'id2' => 'rav42006gray',
+        'id3' => 'rav42006white',
+        'name1' => 'Рейлинг для Toyota Rav 4 (2006 - 2013) полимер черный',
+        'name2' => 'Рейлинг для Toyota Rav 4 (2006 - 2013) анод серый',
+        'name3' => 'Рейлинг для Toyota Rav 4 (2006 - 2013) серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Toyota Rav 4 (2006 - 2013)',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/toyota/rav4-2006-2013/polymer-black.png" srcset="/images/aps/toyota/rav4-2006-2013/polymer-black.png 800w" alt="Рейлинги для Toyota Rav 4" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/toyota/rav4-2006-2013/anod-gray.png" srcset="/images/aps/toyota/rav4-2006-2013/anod-gray.png 800w" alt="Рейлинги для Toyota Rav 4" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/toyota/rav4-2006-2013/white.png" srcset="/images/aps/toyota/rav4-2006-2013/white.png 800w" alt="Рейлинги для Toyota Rav 4" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '7 700 рублей',
+        //Цена с монтажом
+        'price_montazh' => '8 100 рублей',
+    ],
+
+    //Rav 4 (2013 - н.в.)
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'rav42013black',
+        'id2' => 'rav42013gray',
+        'id3' => 'rav42013white',
+        'name1' => 'Рейлинг для Toyota Rav 4 (2013 - н.в.) полимер черный',
+        'name2' => 'Рейлинг для Toyota Rav 4 (2013 - н.в.) анод серый',
+        'name3' => 'Рейлинг для Toyota Rav 4 (2013 - н.в.) серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Toyota Rav 4 (2013 - н.в.)',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/toyota/rav4-2013/polymer-black.png" srcset="/images/aps/toyota/rav4-2013/polymer-black.png 800w" alt="Рейлинги для Toyota Rav 4" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/toyota/rav4-2013/anod-gray.png" srcset="/images/aps/toyota/rav4-2013/anod-gray.png 800w" alt="Рейлинги для Toyota Rav 4" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/toyota/rav4-2013/white.png" srcset="/images/aps/toyota/rav4-2013/white.png 800w" alt="Рейлинги для Toyota Rav 4" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '7 700 рублей',
+        //Цена с монтажом
+        'price_montazh' => '8 100 рублей',
+    ],
+
+    //PRADO 150 (2009 - н.в.)
+    [
+        //Имена товаров и номера для создания ссылок
+        'id1' => 'prado150black',
+        'id2' => 'prado150gray',
+        'id3' => 'prado150white',
+        'name1' => 'Рейлинг для Toyota PRADO 150 (2009 - н.в.) полимер черный',
+        'name2' => 'Рейлинг для Toyota PRADO 150 (2009 - н.в.) анод серый',
+        'name3' => 'Рейлинг для Toyota PRADO 150 (2009 - н.в.) серебристый',
+        //Заголовок, который отображается на странице
+        'title' => 'Рейлинги для Toyota PRADO 150 (2009 - н.в.)',
+        //Изображения
+        'img1' => '<img class="img_main" src="/images/aps/toyota/prado-150/polymer-black.png" srcset="/images/aps/toyota/prado-150/polymer-black.png 800w" alt="Рейлинги для Toyota PRADO 150" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img2' => '<img class="img_main" src="/images/aps/toyota/prado-150/anod-gray.png" srcset="/images/aps/toyota/prado-150/anod-gray.png 800w" alt="Рейлинги для Toyota PRADO 150" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        'img3' => '<img class="img_main" src="/images/aps/toyota/prado-150/white.png" srcset="/images/aps/toyota/prado-150/white.png 800w" alt="Рейлинги для Toyota PRADO 150" sizes="(max-width: 2000px) 150px, 300px, 350px">',
+        //Цвета
+        'color1' => 'полимер черный',
+        'color2' => 'анод серый',
+        'color3' => 'серебристый',
+        //Цена без монтажа
+        'price' => '8 200 рублей',
+        //Цена с монтажом
+        'price_montazh' => '8 600 рублей',
+    ],
+];
