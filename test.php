7a1aba3154612955d4e0356825c518af02698a39
igoldyrev <goldirev12@gmail.com>
2018-02-18 17:18:18 +0500

    feature / develop Тестовая страница для макета

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/headtags.php");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/header/header.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/proposition/proposition.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/navigation/navigation.html"); ?>
+
+<div class="wrapper">
+    <?php include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/left-nav/left-nav.html"); ?>
+    <div class="wrapper__content">
+
+    </div>
+</div>
