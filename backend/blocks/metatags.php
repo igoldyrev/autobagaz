cae99c3ddcb5f03e4a1ae2fa197d00db2d4dfe65
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-02-19 17:28:33 +0500

    feature/develop Новые файлы с метатегами для страниц

---
+++ backend/blocks/metatags.php
@@ -0,0 +1,30 @@
+<?php session_start();
+$titleconst = "Автобагаж - купить автобагажники и автобоксы на крышу автомобиля в Перми - "; ?>
+<html lang="ru">
+
+<!-- meta tags -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="robots" content="index,follow">
+
+<!-- meta property -->
+<meta property="place:location:latitude" content="58.03593904782619">
+<meta property="place:location:longitude" content="56.2000031453342">
+<meta property="business:contact_data:street_address" content="Ул. Спешилова 102/29">
+<meta property="business:contact_data:locality" content="Пермь">
+<meta property="business:contact_data:email" content="autobagaz@yandex.ru">
+<meta property="business:contact_data:website" content="https://autobagaz.ru">
+<meta property="og:url" content="https://autobagaz.ru">
+<meta property="og:site_name" content="Автобагаж">
+
+//todo сделать иконки в новом формате
+
+<link rel="stylesheet" href="../build/autobagaz.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<script type="text/javascript" src="//vk.com/js/api/openapi.js?139"></script>
+<script type="text/javascript">
+    VK.init({apiId: 5866168, onlyWidgets: true});
+</script>
---
+++ backend/blocks/metatagslight.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+<html lang="ru">
+
+<!-- meta tags -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<link rel="stylesheet" href="../build/autobagaz.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<script type="text/javascript" src="//vk.com/js/api/openapi.js?139"></script>
+<script type="text/javascript">
+    VK.init({apiId: 5866168, onlyWidgets: true});
+</script>
