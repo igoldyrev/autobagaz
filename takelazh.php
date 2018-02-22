df1f8543ed95da3fa76d6b96a0b82a23b4d4da48
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-02-22 14:17:48 +0500

    feature/develop Переделанный раздел текстилььных строп

--- takelazh.php
+++ takelazh.php
@@ -1,92 +1,91 @@
-<?php
-include($_SERVER["DOCUMENT_ROOT"] . "/modules/keywords.php");
-include ($_SERVER["DOCUMENT_ROOT"]."/modules/headtags.php"); ?>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatags.php");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/header/header.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/proposition/proposition.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/navigation/navigation.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/content/takelazh/backend/keywords.php"); ?>
 
 <div class="wrapper">
-    <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/header/header.php");?>
-    <div class="wrapper-content">
-        <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/left-nav/left-nav.html");?>
-        <div class="content">
-            <?php
-            $takelazh = $_GET['takelazh'];
-            $_SESSION['url'] = $_SERVER['REQUEST_URI'];
+    <?php include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/left-nav/left-nav.html"); ?>
+    <div class="wrapper__content">
+        <?php
+        $takelazh = $_GET['takelazh'];
+        $_SESSION['url'] = $_SERVER['REQUEST_URI'];
 
-            if (!isset($takelazh)) {
-                echo "<title> $titleconst"; echo $keywords[35][title]; echo "</title>";
-                echo "<meta name='description' content='"; echo $keywords[35][description];      echo "'/>";
-                echo "<meta name='keywords' content='"; echo $keywords[35][keywords]; echo "'/>";
+        if (!isset($takelazh)) {
+            echo "<title> $titleconst"; echo $keywords[0][title]; echo "</title>";
+            echo "<meta name='description' content='"; echo $keywords[0][description];      echo "'/>";
+            echo "<meta name='keywords' content='"; echo $keywords[0][keywords]; echo "'/>";
 
-                echo "<h1 class='page__title-h1'>Такелажная продукция</h1>";?>
+                echo "<h1 class='title title-h1'>Такелажная продукция</h1>";?>
                 <div class="catalog">
                     <div class="catalog__item">
                         <a href="/takelazh/dynamic_strops" class="catalog__item-link"></a>
-                        <div class="item__image">
-                            <a href="/takelazh/dynamic_strops"><img class="item__image-img" src="/images/autotuns/dynamic/1.JPG" alt="динамические стропы"></a>
+                        <div class="catalog__image-wrap">
+                            <img class="catalog__image" src="/content/takelazh/img/dynamic/1.JPG" alt="динамические стропы">
                         </div>
-                        <div class="item__link">
-                            <a href="/takelazh/dynamic_strops">Динамические стропы</a>
+                        <div class="catalog__text">
+                            <p class="text">Динамические стропы</p>
                         </div>
                     </div>
                     <div class="catalog__item">
                         <a href="/takelazh/textil_strops" class="catalog__item-link"></a>
-                        <div class="item__image">
-                            <a href="/takelazh/textil_strops"><img class="item__image-img" src="/images/autotuns/textil/3.JPG" alt="текстильные стропы"></a>
+                        <div class="catalog__image-wrap">
+                            <a href="/takelazh/textil_strops"><img class="catalog__image" src="/content/takelazh/img/textil/3.JPG" alt="текстильные стропы"></a>
                         </div>
-                        <div class="item__link">
-                            <a href="/takelazh/textil_strops">Текстильные стропы</a>
+                        <div class="catalog__text">
+                            <p class="text">Текстильные стропы</p>
                         </div>
                     </div>
                 </div> <?php
             } elseif ($takelazh == 'dynamic_strops') {
-                echo "<title> $titleconst"; echo $keywords[36][title]; echo "</title>";
-                echo "<meta name='description' content='"; echo $keywords[36][description];      echo "'/>";
-                echo "<meta name='keywords' content='"; echo $keywords[36][keywords]; echo "'/>";
+                echo "<title> $titleconst"; echo $keywords[1][title]; echo "</title>";
+                echo "<meta name='description' content='"; echo $keywords[1][description];      echo "'/>";
+                echo "<meta name='keywords' content='"; echo $keywords[1][keywords]; echo "'/>";
 
-            echo "<h1 class='page__title-h1'>Динамические стропы</h1>";
-            echo "<p class='page__text'>Динамический строп предназначен для эвакуации транспортных средств за счет значительной силы инерции. Основное отличие от текстильной стропы, быстрое восстановление после растяжения.</p>"; ?>
-            <div class="img_div">
-                <img class="img_main" src="/images/autotuns/dynamic/1.JPG" srcset="/images/autotuns/dynamic/1.JPG 4000w" alt="динамические стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                <img class="img_main" src="/images/autotuns/dynamic/2.JPG" srcset="/images/autotuns/dynamic/2.JPG 4000w" alt="динамические стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                <img class="img_main" src="/images/autotuns/dynamic/3.JPG" srcset="/images/autotuns/dynamic/3.JPG 4000w" alt="динамические стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                <img class="img_main" src="/images/autotuns/dynamic/4.JPG" srcset="/images/autotuns/dynamic/4.JPG 4000w" alt="динамические стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                <img class="img_main" src="/images/autotuns/dynamic/5.JPG" srcset="/images/autotuns/dynamic/5.JPG 4000w" alt="динамические стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                <img class="img_main" src="/images/autotuns/dynamic/6.JPG" srcset="/images/autotuns/dynamic/6.JPG 4000w" alt="динамические стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                <img class="img_main" src="/images/autotuns/dynamic/7.JPG" srcset="/images/autotuns/dynamic/7.JPG 4000w" alt="динамические стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
+            echo "<h1 class='title title-h1'>Динамические стропы</h1>";
+            echo "<p class='text'>Динамический строп предназначен для эвакуации транспортных средств за счет значительной силы инерции. Основное отличие от текстильной стропы, быстрое восстановление после растяжения.</p>"; ?>
+            <div class="img__wrap">
+                <img class="img good__img" src="/content/takelazh/img/dynamic/1.JPG" alt="динамические стропы">
+                <img class="img good__img" src="/content/takelazh/img/dynamic/2.JPG" alt="динамические стропы">
+                <img class="img good__img" src="/content/takelazh/img/dynamic/3.JPG" alt="динамические стропы">
+                <img class="img good__img" src="/content/takelazh/img/dynamic/4.JPG" alt="динамические стропы">
+                <img class="img good__img" src="/content/takelazh/img/dynamic/5.JPG" alt="динамические стропы">
+                <img class="img good__img" src="/content/takelazh/img/dynamic/6.JPG" alt="динамические стропы">
+                <img class="img good__img" src="/content/takelazh/img/dynamic/7.JPG" alt="динамические стропы">
             </div>
-            <p class="page__text">Имеются в наличии стропы с разрывом на 4 т (а/м до 1,4 т), 7 т (для а/м 1,8 т), 8 т (для а/м 1,8 т), 9 т (для а/м 1,5-2,5 т), 10 т (для а/м 1,8 т), 12 т (для а/м 2-3 т) и 15 т (для а/м 3 - 4 т)</p>
-            <p class="page__text">Также для строп предлагается сумка для удобного хранения и перевозки.</p>
-            <div class="img_div">
-                <img class="img_main" src="/images/autotuns/bug/1.jpg" srcset="/images/autotuns/bug/1.jpg 1000w" alt="сумка-перевозка" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                <img class="img_main" src="/images/autotuns/bug/2.jpg" srcset="/images/autotuns/bug/2.jpg 1000w" alt="сумка-перевозка" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                <img class="img_main" src="/images/autotuns/bug/3.jpg" srcset="/images/autotuns/bug/3.jpg 1000w" alt="сумка-перевозка" sizes="(max-width: 2000px) 150px, 300px, 350px">
+            <p class="text">Имеются в наличии стропы с разрывом на 4 т (а/м до 1,4 т), 7 т (для а/м 1,8 т), 8 т (для а/м 1,8 т), 9 т (для а/м 1,5-2,5 т), 10 т (для а/м 1,8 т), 12 т (для а/м 2-3 т) и 15 т (для а/м 3 - 4 т)</p>
+            <p class="text">Также для строп предлагается сумка для удобного хранения и перевозки.</p>
+            <div class="img__wrap">
+                <img class="img good__img" src="/content/takelazh/img/bug/1.jpg" alt="сумка-перевозка">
+                <img class="img good__img" src="/content/takelazh/img/bug/2.jpg" alt="сумка-перевозка">
+                <img class="img good__img" src="/content/takelazh/img/bug/3.jpg" alt="сумка-перевозка">
             </div>
-            <p class="page__text">Цены на данную продукцию Вы можете узнать <a class="page__link" href="tel:+73422889969">по телефону</a> или заполнив форму ниже на этой же странице.</p> <?php
-                include ($_SERVER["DOCUMENT_ROOT"]."/modules/forms/helpform.php");
+            <p class="text">Цены на данную продукцию Вы можете узнать <a class="link" href="tel:+73422889969">по телефону</a> или заполнив форму ниже на этой же странице.</p>
+            <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/forms/helpform.php");
             } elseif ($takelazh == 'textil_strops') {
-                echo "<title> $titleconst"; echo $keywords[37][title]; echo "</title>";
-                echo "<meta name='description' content='"; echo $keywords[37][description];      echo "'/>";
-                echo "<meta name='keywords' content='"; echo $keywords[37][keywords]; echo "'/>";
+                echo "<title> $titleconst"; echo $keywords[2][title]; echo "</title>";
+                echo "<meta name='description' content='"; echo $keywords[2][description];      echo "'/>";
+                echo "<meta name='keywords' content='"; echo $keywords[2][keywords]; echo "'/>";
 
-                echo "<h1 class='page__title-h1'>Текстильные стропы</h1>"; ?>
-                <div class="img_div">
-                    <img class="img_main" src="/images/autotuns/textil/1.jpg" srcset="/images/autotuns/textil/1.jpg 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                    <img class="img_main" src="/images/autotuns/textil/2.JPG" srcset="/images/autotuns/textil/2.JPG 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                    <img class="img_main" src="/images/autotuns/textil/3.JPG" srcset="/images/autotuns/textil/3.JPG 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                    <img class="img_main" src="/images/autotuns/textil/4.JPG" srcset="/images/autotuns/textil/4.JPG 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                    <img class="img_main" src="/images/autotuns/textil/5.JPG" srcset="/images/autotuns/textil/5.JPG 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                    <img class="img_main" src="/images/autotuns/textil/6.JPG" srcset="/images/autotuns/textil/6.JPG 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                    <img class="img_main" src="/images/autotuns/textil/7.JPG" srcset="/images/autotuns/textil/7.JPG 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                    <img class="img_main" src="/images/autotuns/textil/8.JPG" srcset="/images/autotuns/textil/8.JPG 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
-                    <img class="img_main" src="/images/autotuns/textil/9.JPG" srcset="/images/autotuns/textil/9.JPG 4000w" alt="текстильные стропы" sizes="(max-width: 2000px) 150px, 300px, 350px">
+                echo "<h1 class='title title-h1'>Текстильные стропы</h1>"; ?>
+                <div class="img__wrap">
+                    <img class="img good__img" src="/content/takelazh/img/textil/1.jpg" alt="текстильные стропы">
+                    <img class="img good__img" src="/content/takelazh/img/textil/2.JPG" alt="текстильные стропы">
+                    <img class="img good__img" src="/content/takelazh/img/textil/3.JPG" alt="текстильные стропы">
+                    <img class="img good__img" src="/content/takelazh/img/textil/4.JPG" alt="текстильные стропы">
+                    <img class="img good__img" src="/content/takelazh/img/textil/5.JPG" alt="текстильные стропы">
+                    <img class="img good__img" src="/content/takelazh/img/textil/6.JPG" alt="текстильные стропы">
+                    <img class="img good__img" src="/content/takelazh/img/textil/7.JPG" alt="текстильные стропы">
+                    <img class="img good__img" src="/content/takelazh/img/textil/8.JPG" alt="текстильные стропы">
+                    <img class="img good__img" src="/content/takelazh/img/textil/9.JPG" alt="текстильные стропы">
                 </div>
-                <p class="page__text">Имеются в наличии стропы с разрывом на 1 т, 2 т, 3 т, 4 т, 5 т, 6 т, 8 т и 10 т. </p>
-                <p class="page__text">Цены на данную продукцию Вы можете узнать <a class="page__link" href="tel:+73422889969">по телефону</a> или заполнив форму ниже на этой же странице.</p> <?php
-                include ($_SERVER["DOCUMENT_ROOT"]."/modules/forms/helpform.php");
+                <p class="text">Имеются в наличии стропы с разрывом на 1 т, 2 т, 3 т, 4 т, 5 т, 6 т, 8 т и 10 т. </p>
+                <p class="text">Цены на данную продукцию Вы можете узнать <a class="link" href="tel:+73422889969">по телефону</a> или заполнив форму ниже на этой же странице.</p>
+            <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/forms/helpform.php");
             } ?>
-        </div>
     </div>
-    <?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/footer/footer.html");
-    include ($_SERVER["DOCUMENT_ROOT"]."/modules/counters.html"); ?>
 </div>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/footer/footer.html");
+include ($_SERVER["DOCUMENT_ROOT"]."backend/blocks/counters.html"); ?>
 
 
