44e8c28b051f98b60ee43818700b07e57a102efe
igoldyrev <goldirev12@gmail.com>
2018-02-23 23:43:20 +0500

    feature / develop Правки отзывов на глвной странице

--- backend/reviews.php
+++ backend/reviews.php
@@ -1,19 +1,19 @@
 <?php
 $dbname = "9082410193_zakaz";
 
-include ($_SERVER["DOCUMENT_ROOT"]."/modules/connectdb.php");
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/connectdb.php");
 
 $query = "SELECT * FROM guestbook ORDER BY id DESC LIMIT 5"; // запрос на выборку
 
-echo "<h2 class='page__title-h2'>Последние отзывы о нас</h2>";
+echo "<h2 class= 'title                        title-h2'>Последние отзывы о нас</h2>";
 if ($result = mysqli_query($connect, $query)) {
     while ($row = mysqli_fetch_assoc($result)) {
         echo '<div class="rewiew">';
         echo '<div class="rewiew__name">';
         echo '<span>'.$row['name'].'</span>';
         echo '</div>';
-        echo '<div class="rewiew__text">';
-        echo '<p class="page__text page__text--rewiew">'.$row['rewiew'].'</p>';
+        echo '<div class="rewiew__text-wrap">';
+        echo '<p class="text rewiew__text">'.$row['rewiew'].'</p>';
         echo '</div>';
         echo '</div>';
     }
