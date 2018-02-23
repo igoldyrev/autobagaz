c7af158132a2f4a5c8a2dc85937dd65502941427
igoldyrev <goldirev12@gmail.com>
2018-02-23 20:59:07 +0500

    feature / develop Верстка и стили для формы помощи

--- backend/forms/helpform.php
+++ backend/forms/helpform.php
@@ -1,24 +1,24 @@
 <div class="help-block">
-    <h3 class="page__title-h3">Не можете определиться с выбором? Заполните форму ниже, мы Вам поможем с этим!</h3>
+    <h3 class="title title-h3">У Вас остались вопросы? Задайте их ниже и мы постараемся на них ответить!</h3>
     <div class="form__container">
         <form action="/help" method="post" class="form" onsubmit="yaCounter40650914.reachGoal('post_help'); return true">
             <span class="form__label">Ваше имя:</span>
-            <div class="form__input-block">
+            <div class="form__input-wrap">
                 <input type="text" name="name" required class="form__input" placeholder="Введите Ваше имя">
                 <label for="name" class="form__label--shown">Введите Ваше имя</label>
             </div>
             <span class="form__label">Ваш телефон:</span>
-            <div class="form__input-block">
+            <div class="form__input-wrap">
                 <input type="text" name="phone" required class="form__input" placeholder="Введите номер телефона">
                 <label for="phone" class="form__label--shown">Введите номер телефона</label>
             </div>
             <span class="form__label">Ваше сообщение:</span>
-            <div class="form__input-block">
+            <div class="form__input-wrap">
                 <textarea name="message" class="form__input form__textarea" placeholder="Введите Ваше сообщение"></textarea>
                 <label for="text" class="form__label--shown">Введите Ваше сообщение</label>
             </div>
             <div class="g-recaptcha" data-sitekey="6LenJjcUAAAAALv0jIybM3O45CnsEYa4X58EZ1uH"></div>
-            <button class="button button__help">Помогите с выбором!</button>
+            <button class="button button__zakaz button__zakaz--help">Задать вопрос</button>
         </form>
     </div>
 </div>
