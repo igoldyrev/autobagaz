0d2ceaabb83daf6b68b55525c5cfef5604ad9caa
igoldyrev <goldirev12@gmail.com>
2018-02-25 18:09:19 +0500

    feature / develop Поправлены ошибки верстки

--- backend/forms/rewiewform.php
+++ backend/forms/rewiewform.php
@@ -1,21 +1,21 @@
 <div class="form__container">
     <form action="/rewiew" method="post" class="form" onsubmit="yaCounter40650914.reachGoal('post_rewiew'); return true">
         <span class="form__label">Ваше имя:</span>
-        <div class="form__input-block">
+        <div class="form__input-wrap">
             <input type="text" name="name" required autofocus class="form__input" placeholder="Введите Ваше имя">
             <label for="name" class="form__label--shown">Введите Ваше имя</label>
         </div>
         <span class="form__label">Ваш телефон:</span>
-        <div class="form__input-block">
+        <div class="form__input-wrap">
             <input type="text" name="phone" class="form__input" placeholder="Введите номер телефона. Номер телефона НЕ публикуется">
             <label for="phone" class="form__label--shown">Введите номер телефона. Номер телефона НЕ публикуется</label>
         </div>
         <span class="form__label">Ваш отзыв:</span>
-        <div class="form__input-block">
+        <div class="form__input-wrap">
             <textarea name="rewiew" class="form__input form__textarea" placeholder="Введите Ваш отзыв о нас"></textarea>
             <label for="rewiew" class="form__label--shown">Введите Ваш отзыв о нас</label>
         </div>
         <div class="g-recaptcha" data-sitekey="6LenJjcUAAAAALv0jIybM3O45CnsEYa4X58EZ1uH"></div>
-        <button class="button button__rewiew">Оставить отзыв</button>
+        <button class="button button__zakaz">Оставить отзыв</button>
     </form>
 </div>
