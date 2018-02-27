139d1b32df0de66abc989a4e43d67c38bdbe5e22
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-02-27 12:24:15 +0500

    feature/develop Правки верстки страниц ошибок

--- backend/errors/404.php
+++ backend/errors/404.php
@@ -1,17 +1,19 @@
 <?php
-include ($_SERVER["DOCUMENT_ROOT"]."/modules/keywords.php");
-include ($_SERVER["DOCUMENT_ROOT"]."/modules/headtags.php");
-	echo "<title> $titleconst"; echo $keywords[26][title]; echo "</title>";
-	echo "<meta name='description' content='"; echo $keywords[26][description]; echo "'/>"; ?>
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/keywords/keywords.php");
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatagslight.php");
+	echo "<title> $titleconst"; echo $keywords[10][title]; echo "</title>";
+	echo "<meta name='description' content='"; echo $keywords[10][description]; echo "'/>"; ?>
 
-<div class="error">
-    <div class="error__link">
-        <a href="/"><img class="error__img" src="/images/logo.jpg" alt="autobagaz.ru"></a>
-    </div>
-    <div class="error__text">
-        <h2 class="page__title-h2">Ошибка 404. Файл не найден.</h2>
-        <p class="page__text page__text--error">Возможно вы ошиблись при наборе адреса, или ссылка, по которой вы прошли, устарела.</p>
-        <p class="page__text page__text--error">Но вы также можете перейти на <a class="page__link" href="/">главную страницу</a> сайта.</p>
+<div class="wrapper">
+    <div class="error">
+        <div class="error__link-wrap">
+            <a class="error__link" href="/"><img class="error__img" src="/src/common.blocks/error/img/logo.jpg" alt="autobagaz.ru"></a>
+        </div>
+        <div class="error__text">
+            <h2 class="title title-h2">Ошибка 404. Файл не найден.</h2>
+            <p class="text">Возможно вы ошиблись при наборе адреса, или ссылка, по которой вы прошли, устарела.</p>
+            <p class="text">Но вы также можете перейти на <a class="link" href="/">главную страницу</a> сайта.</p>
+        </div>
     </div>
 </div>
-<?php include ($_SERVER["DOCUMENT_ROOT"]."/modules/counters.html");?>
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/counters.html");?>
