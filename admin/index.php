34dbcaa0505a272373896c39295f38d5990a6619
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-02-28 16:43:54 +0500

    feature/develop Правка верстки главной страницы админки

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,12 @@
 <?php
-include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html");
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatagslight.php");
 echo "<title>Админка сайта Автобагаж.ру!</title>"; ?>
 
 <div class="admin__container">
-    <h1 class='page__title-h1'>Админка сайта Автобагаж.ру!</h1>
+    <h1 class='title title-h1'>Админка сайта Автобагаж.ру!</h1>
 
     <div class="admin clearfix">
-        <?php echo "<h3 class='page__title-h3'>Панель управления отзывами</h3>"; ?>
+        <?php echo "<h3 class='title title-h3'>Панель управления отзывами</h3>"; ?>
         <a class="admin__link" href="/admin/guestbook/update/guestupdate.php">Редактировать отзывы</a>
         <a class="admin__link" href="/admin/guestbook/answers/rewiewanswer.php">Ответить на отзывы</a>
         <a class="admin__link" href="/admin/guestbook/answers/answerchoice.php">Редактировать ответы на отзывы</a>
@@ -14,20 +14,15 @@ echo "<title>Админка сайта Автобагаж.ру!</title>"; ?>
     </div>
 
     <div class="admin clearfix">
-        <?php echo "<h3 class='page__title-h3'>Панель управления блоками</h3>"; ?>
-        <p class="page__text">Блок Акция и блок с обьявлением на главной странице.</p>
+        <?php echo "<h3 class='title title-h3'>Панель управления блоками</h3>"; ?>
+        <p class="text">Блок Акция и блок с обьявлением на главной странице.</p>
         <a class="admin__link" href="/admin/message/update/msgupdate.php">Редактировать обьявление</a>
         <a class="admin__link" href="/admin/message/activate/msgactivate.php">Выключить/включить блоки</a>
     </div>
 
     <div class="admin clearfix">
-        <?php echo "<h3 class='page__title-h3'>Панель управления новостями</h3>"; ?>
+        <?php echo "<h3 class='title title-h3'>Панель управления новостями</h3>"; ?>
         <a class="admin__link" href="/admin/news/newsdelete.php">Удалить новости</a>
-    </div>
-
-    <div class="admin clearfix">
-        <?php echo "<h3 class='page__title-h3'>Скачать данные сайта</h3>"; ?>
-        <a class="admin__link" href="/admin/files/keywords.zip">Скачать</a>
     </div>
 
     <a class="admin__link admin__link--back" href="/">Вернуться на главную страницу сайта</a>
