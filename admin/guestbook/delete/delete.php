042ac808dfaf78a69dba61f35bbc76c9665e85ad
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-02-28 16:57:49 +0500

    feature/develop Правка верстки страниц удаления отзывов

--- admin/guestbook/delete/delete.php
+++ admin/guestbook/delete/delete.php
@@ -1,18 +1,18 @@
 <?php echo "<title>Отзыв удален!</title>";
-include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatagslight.php"); ?>
 
 <div class="admin__container">
     <?php
     $dbname = "9082410193_zakaz";
 
-    include ($_SERVER["DOCUMENT_ROOT"]."/modules/connectdb.php");
+    include ($_SERVER["DOCUMENT_ROOT"]."/backend/connectdb.php");
 
     $id=$_REQUEST['rewiew'];
     $delete_sql = "DELETE FROM guestbook WHERE id=$id";
-    mysqli_query($connect, $delete_sql) or die("<p class='page__text'>При удалении отзыва произошла ошибка</p>". mysqli_error());
-    echo "<h3 class='page__title-h3'>Отзыв успешно удален!</h3>";
+    mysqli_query($connect, $delete_sql) or die("<p class='text'>При удалении отзыва произошла ошибка</p>". mysqli_error());
+    echo "<h3 class='title title-h3'>Отзыв успешно удален!</h3>";
     ?>
-    <div class="admin__link-down clearfix">
+    <div class="admin__link-wrap clearfix">
         <a class="admin__link" href="/admin/guestbook/delete/guestdelete.php">Вернуться к выбору отзывов</a>
         <a class="admin__link" href="/admin/index.php">Вернуться на главную админки</a>
     </div>
--- admin/guestbook/delete/guestdelete.php
+++ admin/guestbook/delete/guestdelete.php
@@ -1,13 +1,13 @@
 <?php echo "<title>Выбор отзыва для удаления</title>";
-include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatagslight.php"); ?>
 
 <div class="admin__container">
-    <h1 class="page__title-h1">Выбор отзыва для удаления</h1>
+    <h1 class="title title-h1">Выбор отзыва для удаления</h1>
 
     <?php
     $dbname = "9082410193_zakaz";
 
-    include ($_SERVER["DOCUMENT_ROOT"]."/modules/connectdb.php"); ?>
+    include ($_SERVER["DOCUMENT_ROOT"]."/backend/connectdb.php"); ?>
 
     <div class="form__container">
         <form action="/admin/guestbook/delete/delete.php" method="post">
@@ -18,14 +18,14 @@ include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
 
             do
             {
-                printf("<div class='admin__item'><input class='admin__radio' type='radio' name='rewiew' value='%s'>%s", $row['id'], '<span class="page__text">'.$row['name'].'</span></div>');
+                printf("<div class='admin__item'><input class='admin__radio' type='radio' name='rewiew' value='%s'>%s", $row['id'], '<span class="text">'.$row['name'].'</span></div>');
             }
             while($row = mysqli_fetch_array($result))
             ?>
             <input class="button button__zakaz" type="submit" value="Удалить отзыв">
         </form>
     </div>
-    <div class="admin__link-down clearfix">
+    <div class="admin__link-wrap clearfix">
         <a class="admin__link" href="/admin/index.php">Вернуться на главную админки</a>
     </div>
 </div>
