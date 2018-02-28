ef0f7cc58e3a56a559ce5a1aa8fedefe6ae44410
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-02-28 17:39:22 +0500

    feature/develop Правка верстки страниц включения блоков

--- admin/message/activate/activate.php
+++ admin/message/activate/activate.php
@@ -1,6 +1,6 @@
 <?php
 echo "<title>Состояние блока изменено</title>";
-include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatagslight.php"); ?>
 
 <div class="admin__container">
     <?php
@@ -10,33 +10,32 @@ include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html"); ?>
     $head = "<?php \n";
     file_put_contents($config, $head);
     if(isset($_POST['msg_on']) == true ){
-        echo '<h2 class="page__title-h2">Блок с обьявлением включен</h2>';
+        echo '<h2 class="title title-h2">Блок с обьявлением включен</h2>';
         $checkedmsgon = '$checkedmsgon';
         $content = file_get_contents($config);
         $content .= "$checkedmsgon = 'checked';\n ";
         file_put_contents($config, $content);
     } elseif(isset($_POST['msg_on']) == false ) {
-        echo '<h2 class="page__title-h2">Блок с обьявлением выключен</h2>';
+        echo '<h2 class="title title-h2">Блок с обьявлением выключен</h2>';
         $checkedmsgon = '$checkedmsgon';
         $content = file_get_contents($config);
         $content .= "$checkedmsgon = 'none';\n ";
         file_put_contents($config, $content);
     }
     if(isset($_POST['stock_on']) == true ){
-        echo '<h2 class="page__title-h2">Блок с акцией включен</h2>';
+        echo '<h2 class="title title-h2">Блок с акцией включен</h2>';
         $checkedstock = '$checkedstock';
         $contents = file_get_contents($config);
         $contents .= "$checkedstock = 'checked';\n ";
         file_put_contents($config, $contents);
     } elseif (isset($_POST['stock_on']) == false){
-        echo '<h2 class="page__title-h2">Блок с акцией выключен</h2>';
+        echo '<h2 class="title title-h2">Блок с акцией выключен</h2>';
         $checkedstock = '$checkedstock';
         $contents = file_get_contents($config);
         $contents .= "$checkedstock = 'none';\n ";
         file_put_contents($config, $contents);
-    }
-    ?>
-    <div class="admin__link-down clearfix">
+    } ?>
+    <div class="admin__link-wrap clearfix">
         <a class="admin__link" href="/admin/message/activate/msgactivate.php">Вернуться на предыдущую страницу</a>
         <a class="admin__link" href="/admin/index.php">Вернуться на главную админки</a>
     </div>
--- admin/message/activate/msgactivate.php
+++ admin/message/activate/msgactivate.php
@@ -1,7 +1,7 @@
 <?php
-echo "<title>Выключить обьявление</title>";
-include ($_SERVER["DOCUMENT_ROOT"]."/admin/headtags.html");
-include ($_SERVER["DOCUMENT_ROOT"]."/admin/config.php");
+echo "<title>Выключить / включить блоки</title>";
+include ($_SERVER["DOCUMENT_ROOT"]."/backend/blocks/metatagslight.php");
+include ($_SERVER["DOCUMENT_ROOT"]."/admin/message/activate/configmessage.php");
 
 if ($checkedmsgon == 'checked'){
     $checkedmsgon = "checked";
@@ -11,29 +11,26 @@ if ($checkedmsgon == 'checked'){
     $checkedstock = "checked";
 }elseif ($checkedstock == 'none'){
     $checkedstock = "";
-}
-
-
-?>
+} ?>
 
 <div class="admin__container">
-    <h1 class="page__title-h1">Выключение объявления на главной странице</h1>
+    <h1 class="title title-h1">Выключение блоков на главной странице</h1>
 
     <div class="form__container">
-        <p class="page__text" style="margin-bottom: 15px">Если галочка установлена - то блок отображается на странице. Если она убрана - то не отображается.</p>
+        <p class="text">Если галочка установлена - то блок отображается на странице. Если она убрана - то не отображается.</p>
         <form action="/admin/message/activate/activate.php" method="post" class="form">
-            <div class="form__input-block clearfix">
+            <div class="form__input-wrap clearfix">
                 <input type="checkbox" name="msg_on" class="form__checkbox" id="msgon" <?php echo $checkedmsgon ?>>
                 <label for="msgon" class="form__label--checkbox">Включить/выключить сообщение</label>
             </div>
-            <div class="form__input-block clearfix">
+            <div class="form__input-wrap clearfix">
                 <input type="checkbox" name="stock_on" class="form__checkbox" id="stockon" <?php echo $checkedstock ?>>
                 <label for="stockon" class="form__label--checkbox">Включить/выключить блок с акцией</label>
             </div>
             <input type="submit" class="button button__zakaz" value="Включить/выключить блоки">
         </form>
     </div>
-    <div class="admin__link-down clearfix">
+    <div class="admin__link-wrap clearfix">
         <a class="admin__link" href="/admin/index.php">Вернуться на главную админки</a>
     </div>
 </div>
