1afe4befa73344787211ef3861642bbd908a347e
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-01 19:06:24 +0500

    feature/develop Массив с адресами страниц 3-го и 4-го уровней

---
+++ backend/breadcrumbs/arrays/level3.php
@@ -0,0 +1,261 @@
+<?php
+$autobagazhniki = [
+    [
+        'name' => 'Багажник для рейлингов',
+        'link' => '/product/ab1',
+    ],
+    [
+        'name' => 'Багажник в штатные места',
+        'link' => '/product/ab2',
+    ],
+    [
+        'name' => 'Багажник на плоскую крышу',
+        'link' => '/product/ab3',
+    ],
+    [
+        'name' => 'Багажник для водостоков',
+        'link' => '/product/ab4',
+    ],
+];
+
+$level3 = [
+    [
+        'name' => 'Автомобильные боксы Vetlan (Пермь)',
+        'link' => '/autobox/vetlan',
+    ],
+    [
+        'name' => 'Автобоксы Атлант',
+        'link' => '/autobox/atlant',
+    ],
+    [
+        'name' => 'Автобоксы Yuago',
+        'link' => '/autobox/yuago',
+    ],
+    [
+        'name' => 'Автобоксы Турино',
+        'link' => '/autobox/turino',
+    ],
+    [
+        'name' => 'Автобоксы Lux',
+        'link' => '/autobox/lux',
+    ],
+    [
+        'name' => 'Велокрепления на крышу',
+        'link' => '/velokreplenya/krysha',
+    ],
+    [
+        'name' => 'Велокрепления на фаркоп',
+        'link' => '/velokreplenya/farkop',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Lada',
+        'link' => '/reelings/lada',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Chevrolet',
+        'link' => '/reelings/chevrolet',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Renault',
+        'link' => '/reelings/renault',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей KIA',
+        'link' => '/reelings/kia',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Mazda',
+        'link' => '/reelings/mazda',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Hyundai',
+        'link' => '/reelings/hyundai',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Opel',
+        'link' => '/reelings/opel',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Toyota',
+        'link' => '/reelings/toyota',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Land Rover',
+        'link' => '/reelings/landrover',
+    ],
+    [
+        'name' => 'Рейлинги для автомобилей Datsun',
+        'link' => '/reelings/datsun',
+    ],
+    [
+        'name' => 'Базовые багажники Inno',
+        'link' => '/inno/inno-basic',
+    ],
+    [
+        'name' => 'Автомобильные боксы Inno',
+        'link' => '/inno/inno-boxes',
+    ],
+    [
+        'name' => 'Динамические стропы',
+        'link' => '/takelazh/dynamic_strops',
+    ],
+    [
+        'name' => 'Текстильные стропы',
+        'link' => '/takelazh/textil_strops',
+    ],
+];
+
+$reelingslada = [
+    [
+        'name' => 'Рейлинги АПС для Лада XRAY',
+        'link' => '/reelings/xray',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Лада LARGUS',
+        'link' => '/reelings/largus',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Лада GRANTA Лифтбек',
+        'link' => '/reelings/granta-liftback',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Лада GRANTA E',
+        'link' => '/reelings/granta-e',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Лада Гранта / Калина Седан',
+        'link' => '/reelings/granta-kalina-sedan',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Лада Калина Хэтчбек S',
+        'link' => '/reelings/kalina-xetchbek',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Лада Калина Универсал M',
+        'link' => '/reelings/kalina-universal',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Лада 4x4',
+        'link' => '/reelings/4x4',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Лада 4x4 L',
+        'link' => '/reelings/l4x4',
+    ],
+];
+$reelingschevi = [
+    [
+        'name' => 'Рейлинги АПС для Chevrolet Niva M',
+        'link' => '/reelings/chevi-m',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Chevrolet Niva L',
+        'link' => '/reelings/chevi-l',
+    ],
+];
+$reelingsrenault = [
+    [
+        'name' => 'Рейлинги АПС для Renault Kaptur',
+        'link' => '/reelings/kaptur',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Renault Logan (2004 - 2014)',
+        'link' => '/reelings/logan',
+    ],
+    [
+        'name' => 'Рейлинги АПС для новый Renault Logan',
+        'link' => '/reelings/new-logan',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Renault Sandero',
+        'link' => '/reelings/sandero',
+    ],
+    [
+        'name' => 'Рейлинги АПС для новый Renault Sandero',
+        'link' => '/reelings/new-sandero',
+    ],
+];
+$reelingskia = [
+    [
+        'name' => 'Рейлинги АПС для Kia Ceed',
+        'link' => '/reelings/ceed',
+    ],
+    [
+        'name' => 'Рейлинги АПС для новый Kia Ceed',
+        'link' => '/reelings/new-ceed',
+    ],
+];
+$reelingsmazda = [
+    [
+        'name' => 'Рейлинги АПС для Mazda 3',
+        'link' => '/reelings/three',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Mazda CX-5 I',
+        'link' => '/reelings/cx5-1',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Mazda CX-5 II',
+        'link' => '/reelings/cx5-2',
+    ],
+];
+$reelingshyundai = [
+    [
+        'name' => 'Рейлинги АПС для Hyundai Creta',
+        'link' => '/reelings/creta',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Hyundai Solaris',
+        'link' => '/reelings/solaris',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Hyundai i30',
+        'link' => '/reelings/i30',
+    ],
+];
+$reelingsopel = [
+    [
+        'name' => 'Рейлинги АПС для Opel Astra',
+        'link' => '/reelings/astra',
+    ],
+];
+$reelingstoyota = [
+    [
+        'name' => 'Рейлинги АПС для Toyota RAV-4 III',
+        'link' => '/reelings/rav4-3',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Toyota RAV-4 IV',
+        'link' => '/reelings/rav4-4',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Toyota PRADO 150',
+        'link' => '/reelings/prado150',
+    ],
+];
+$reelingsrover = [
+    [
+        'name' => 'Рейлинги АПС для Land Rover Freelander 2',
+        'link' => '/reelings/freelander2',
+    ],
+];
+$reelingsdatsun = [
+    [
+        'name' => 'Рейлинги АПС для Datsun ON-DO',
+        'link' => '/reelings/on-do',
+    ],
+    [
+        'name' => 'Рейлинги АПС для Datsun MI-DO',
+        'link' => '/reelings/mi-do',
+    ],
+];
+$innoboxes = [
+    [
+        'name' => 'Автобоксы New Shadow',
+        'link' => '/inno/new-shadow',
+    ],
+    [
+        'name' => 'Автобоксы Roofbox',
+        'link' => '/inno/roofbox',
+    ],
+];
