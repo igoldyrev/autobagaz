3535d54a74972120089e5fbbd5e378947b1b14a1
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-01 16:45:18 +0500

    feature/develop Данные хлебных крошек для галереи

---
+++ backend/breadcrumbs/arrays/gallery.php
@@ -0,0 +1,143 @@
+<?php
+$gallery = [
+    [
+        'name' => 'Lada Xray',
+        'link' => '/gallery?auto=xray0303',
+    ],
+    [
+        'name' => 'BMW X5',
+        'link' => '/gallery?auto=bmwx5',
+    ],
+    [
+        'name' => 'Mazda MPV',
+        'link' => '/gallery?auto=mazdampv',
+    ],
+    [
+        'name' => 'UAZ Patriot',
+        'link' => '/gallery?auto=uazpatriot',
+    ],
+    [
+        'name' => 'Volkswagen Jetta',
+        'link' => '/gallery?auto=wvjetta',
+    ],
+    [
+        'name' => 'Nissan X-Trail',
+        'link' => '/gallery?auto=xtrail',
+    ],
+    [
+        'name' => 'Lada 2111',
+        'link' => '/gallery?auto=lada2111',
+    ],
+    [
+        'name' => 'Lada Largus',
+        'link' => '/gallery?auto=ladalargus',
+    ],
+    [
+        'name' => 'Ford Focus 2',
+        'link' => '/gallery?auto=focus2',
+    ],
+    [
+        'name' => 'Kia Seed',
+        'link' => '/gallery?auto=seed',
+    ],
+    [
+        'name' => 'Lada Xray',
+        'link' => '/gallery?auto=xray0602',
+    ],
+    [
+        'name' => 'Niva Chevrolet',
+        'link' => '/gallery?auto=chevrolet',
+    ],
+    [
+        'name' => 'Kia Rio',
+        'link' => '/gallery?auto=kiario',
+    ],
+    [
+        'name' => 'Lada 2107',
+        'link' => '/gallery?auto=lada2107',
+    ],
+    [
+        'name' => 'Mitsubisi Lancer',
+        'link' => '/gallery?auto=lancer',
+    ],
+    [
+        'name' => 'Subaru Outback',
+        'link' => '/gallery?auto=outback',
+    ],
+    [
+        'name' => 'Lada 2111',
+        'link' => '/gallery?auto=21110621',
+    ],
+    [
+        'name' => 'Volkswagen Polo',
+        'link' => '/gallery?auto=polo',
+    ],
+    [
+        'name' => 'BMW X5',
+        'link' => '/gallery?auto=bmwx50621',
+    ],
+    [
+        'name' => 'Chevrolet Niva',
+        'link' => '/gallery?auto=nivachevrolet',
+    ],
+    [
+        'name' => 'Lifan X50',
+        'link' => '/gallery?auto=lifanx50',
+    ],
+    [
+        'name' => 'Reno Logan',
+        'link' => '/gallery?auto=renologan',
+    ],
+    [
+        'name' => 'Lada Xray',
+        'link' => '/gallery?auto=ladaxray2707',
+    ],
+    [
+        'name' => 'Skoda Rapid',
+        'link' => '/gallery?auto=rapid2807',
+    ],
+    [
+        'name' => 'Lada Xray',
+        'link' => '/gallery?auto=ladaxray3007',
+    ],
+    [
+        'name' => 'Hyundai Creta',
+        'link' => '/gallery?auto=creta0108',
+    ],
+    [
+        'name' => 'Toyota Land Cryiser',
+        'link' => '/gallery?auto=cryiser0809',
+    ],
+    [
+        'name' => 'Reno Duster',
+        'link' => '/gallery?auto=duster0810',
+    ],
+    [
+        'name' => 'Reno Sandero',
+        'link' => '/gallery?auto=sandero0820',
+    ],
+    [
+        'name' => 'Skoda Ieti',
+        'link' => '/gallery?auto=ieti0826',
+    ],
+    [
+        'name' => 'Hyundai ix35',
+        'link' => '/gallery?auto=ix358831',
+    ],
+    [
+        'name' => 'Lada Xray',
+        'link' => '/gallery?auto=xray0902',
+    ],
+    [
+        'name' => 'Opel Mokka',
+        'link' => '/gallery?auto=mokka0908',
+    ],
+    [
+        'name' => 'Reno Kaptur',
+        'link' => '/gallery?auto=kaptur0802',
+    ],
+    [
+        'name' => 'Lada Приора',
+        'link' => '/gallery?auto=priora0802',
+    ],
+];
