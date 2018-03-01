539f9d20965e5dbdd3cdd8cdb8da9f815dcb31fc
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-01 16:44:46 +0500

    feature/develop Данные хлебных крошек для второго уровня ссылок

---
+++ backend/breadcrumbs/arrays/level2.php
@@ -0,0 +1,79 @@
+<?php
+$level2 = [
+    [
+        'name' => 'Новости',
+        'link' => '/news',
+    ],
+    [
+        'name' => 'Новости',
+        'link' => '/news/1',
+    ],
+    [
+        'name' => 'Новости',
+        'link' => '/news/2',
+    ],
+    [
+        'name' => 'Автомобильные багажники',
+        'link' => '/autobagazhniki',
+    ],
+    [
+        'name' => 'Автомобильные боксы',
+        'link' => '/autobox',
+    ],
+    [
+        'name' => 'Велокрепления',
+        'link' => '/velokreplenya',
+    ],
+    [
+        'name' => 'Крепления для лыж и сноубордов',
+        'link' => '/kreplenya-dlya-lyzh-shoubord',
+    ],
+    [
+        'name' => 'Рейлинги',
+        'link' => '/reelings',
+    ],
+    [
+        'name' => 'Браслеты противоскольжения',
+        'link' => '/braslets',
+    ],
+    [
+        'name' => 'Фаркопы',
+        'link' => '/farkops',
+    ],
+    [
+        'name' => 'Багажные системы Inno',
+        'link' => '/inno',
+    ],
+    [
+        'name' => 'Такелажная продукция',
+        'link' => '/takelazh',
+    ],
+    [
+        'name' => 'Наши партнеры',
+        'link' => '/partners',
+    ],
+    [
+        'name' => 'Прокат багажников и боксов в Перми',
+        'link' => '/prokat',
+    ],
+    [
+        'name' => 'Галерея работ',
+        'link' => '/gallery',
+    ],
+    [
+        'name' => 'Специальные предложения',
+        'link' => '/special-offers',
+    ],
+    [
+        'name' => 'Страница отзывов',
+        'link' => '/guestbook',
+    ],
+    [
+        'name' => 'Контактная информация',
+        'link' => '/contacts',
+    ],
+    [
+        'name' => 'Карта сайта  ',
+        'link' => '/sitemap',
+    ],
+];
