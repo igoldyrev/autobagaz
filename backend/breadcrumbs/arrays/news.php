26753399b1f126feee1b0fe8f340ec3ee3e6c4b3
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-01 16:45:32 +0500

    feature/develop Данные хлебных крошек для новостей

---
+++ backend/breadcrumbs/arrays/news.php
@@ -0,0 +1,67 @@
+<?php
+$news = [
+    [
+        'name' => 'Новинка! Новые поступления лыжных креплений и багажников',
+        'link' => '/news/postuplenya_amos',
+    ],
+    [
+        'name' => 'Новинка! Рейлинги на X-Ray!',
+        'link' => '/news/reelings_xray',
+    ],
+    [
+        'name' => 'Розыгрыш багажника от autobagaz.ru',
+        'link' => '/news/rozygryzh_bagazhnika',
+    ],
+    [
+        'name' => 'Новинка! В продаже появились браслеты противоскольжения!',
+        'link' => '/news/braslety',
+    ],
+    [
+        'name' => 'Итоги розыгрыша багажника от autobagaz.ru',
+        'link' => '/news/itogi_rozygryzha',
+    ],
+    [
+        'name' => 'Розыгрыш велокрепления от autobagaz.ru',
+        'link' => '/news/rozygryzh_velokreplenya',
+    ],
+    [
+        'name' => 'Поступления автобоксов Vetlan',
+        'link' => '/news/postuplenya_avtoboksov',
+    ],
+    [
+        'name' => 'Расписание работы в майские праздники',
+        'link' => '/news/mayskie_prazdniki',
+    ],
+    [
+        'name' => 'Горячее предложение от магазина!',
+        'link' => '/news/akcia_na_boksy',
+    ],
+    [
+        'name' => 'Расписание работы 12 июня',
+        'link' => '/news/12_june',
+    ],
+    [
+        'name' => 'AUTOBAGAZный ценопад!',
+        'link' => '/news/cenopad',
+    ],
+    [
+        'name' => 'Акция в магазине "AutobagaZ"!!!',
+        'link' => '/news/akcia_na_braslets',
+    ],
+    [
+        'name' => 'Как подобрать бокс на крышу автомобиля?',
+        'link' => '/podbor',
+    ],
+    [
+        'name' => 'ОХОТА НА AUTOBAGAZ!',
+        'link' => '/news/oxota_na_autobagaz',
+    ],
+    [
+        'name' => 'Розыгрыш лыжного крепления Mont Blanc',
+        'link' => '/news/rozygryzh_montblanc',
+    ],
+    [
+        'name' => 'Итоги розыгрыша лыжного крепления Mont Blanc',
+        'link' => '/news/result_rozygryzh_montblanc',
+    ],
+];
