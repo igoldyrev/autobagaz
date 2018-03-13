8fab81ad0a7ed71c10b57dbef7a3c2ba04acadfa
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2018-03-13 11:53:43 +0500

    featire / braslets На странице обновлены фотографии, а также для каждого товара сделаны отделььные карточки

--- braslets.php
+++ braslets.php
@@ -15,30 +15,47 @@ $_SESSION['url'] = $_SERVER['REQUEST_URI']; ?>
 <div class="wrapper">
     <?php include ($_SERVER["DOCUMENT_ROOT"]."/src/common.blocks/left-nav/left-nav.html"); ?>
     <div class="wrapper__content">
+        <h1 class="title title-h1"><?php echo $braslet['title']; ?></h1>
+        <p class="text"><?php echo $braslet['desc']; ?></p>
         <div class="good">
-            <h1 class="title title-h1"><?php echo $braslet['title']; ?></h1>
+            <h2 class="title title-h2"><?php echo $braslet['name1']; ?></h2>
             <div class="good__description">
                 <div class="img__wrap">
-                    <?php echo $braslet['img']; ?>
+                    <?php echo $braslet['img1']; echo $braslet['img2']; echo $braslet['img3']; ?>
                 </div>
-                <p class="text"><?php echo $braslet['desc']; ?></p>
             </div>
             <div class="good__price">
-                <span class="text"><?php echo $braslet['desc1']; ?> </span><span class="text"><strong><?php echo $braslet['price1']; ?></strong></span>
+                <span class="text"><?php echo $braslet['desc1']; ?> <span class="text"><strong><?php echo $braslet['price1']; ?></strong></span></span>
                 <div class="good__buttons">
                     <a onclick="yaCounter40650914.reachGoal('click_zakaz'); return true" href="/buy/<?php echo $braslet['id1']; ?>" class="button button__buy" >Заказать</a>
                     <a onclick="yaCounter40650914.reachGoal('click_prokat'); return true" href="/prokat/<?php echo $braslet['id1']; ?>" class="button button__buy button__buy--prokat">Взять в прокат</a>
                 </div>
             </div>
+        </div>
+        <div class="good">
+            <h2 class="title title-h2"><?php echo $braslet['name2']; ?></h2>
+            <div class="good__description">
+                <div class="img__wrap">
+                    <?php echo $braslet['img4']; echo $braslet['img5']; ?>
+                </div>
+            </div>
             <div class="good__price">
-                <span class="text"><?php echo $braslet['desc2']; ?> </span><span class="text"><strong><?php echo $braslet['price2']; ?></strong></span>
+                <span class="text"><?php echo $braslet['desc2']; ?> <span class="text"><strong><?php echo $braslet['price2']; ?></strong></span></span>
                 <div class="good__buttons">
                     <a onclick="yaCounter40650914.reachGoal('click_zakaz'); return true" href="/buy/<?php echo $braslet['id2']; ?>" class="button button__buy" >Заказать</a>
                     <a onclick="yaCounter40650914.reachGoal('click_prokat'); return true" href="/prokat/<?php echo $braslet['id2']; ?>" class="button button__buy button__buy--prokat">Взять в прокат</a>
                 </div>
             </div>
+        </div>
+        <div class="good">
+            <h2 class="title title-h2"><?php echo $braslet['name3']; ?></h2>
+            <div class="good__description">
+                <div class="img__wrap">
+                    <?php echo $braslet['img4']; echo $braslet['img5']; ?>
+                </div>
+            </div>
             <div class="good__price">
-                <span class="page__text"><?php echo $braslet['desc3']; ?> </span><span class="page__text"><strong><?php echo $braslet['price3']; ?></strong></span>
+                <span class="text"><?php echo $braslet['desc3']; ?> <span class="text"><strong><?php echo $braslet['price3']; ?></strong></span></span>
                 <div class="good__buttons">
                     <a onclick="yaCounter40650914.reachGoal('click_zakaz'); return true" href="/buy/<?php echo $braslet['id3']; ?>" class="button button__buy" >Заказать</a>
                     <a onclick="yaCounter40650914.reachGoal('click_prokat'); return true" href="/prokat/<?php echo $braslet['id3']; ?>" class="button button__buy button__buy--prokat">Взять в прокат</a>
--- content/braslets/backend/array.php
+++ content/braslets/backend/array.php
@@ -11,7 +11,11 @@ $braslet = [
     'title' => 'Браслеты противоскольжения',
     'desc' => 'Всесезонные, всепогодные. Устанавливаются без наезда, домкрата и инструмента в 1 минуту. Даже для полного привода достаточно 2-х браслетов. Отличный подарок активным автолюбителям.',
     //Изображение
-    'img' => '<img class="img good__img" src="/content/braslets/img/braslet.jpg" alt="Браслеты противоскольжения">',
+    'img1' => '<img class="img good__img" src="/content/braslets/img/braslet-r12-1.jpg" alt="Браслеты противоскольжения R12-R15">',
+    'img2' => '<img class="img good__img" src="/content/braslets/img/braslet-r12-2.jpg" alt="Браслеты противоскольжения R12-R15">',
+    'img3' => '<img class="img good__img" src="/content/braslets/img/braslet-r12-3.jpg" alt="Браслеты противоскольжения R12-R15">',
+    'img4' => '<img class="img good__img" src="/content/braslets/img/braslet-r16-1.jpg" alt="Браслеты противоскольжения R16-R22,5">',
+    'img5' => '<img class="img good__img" src="/content/braslets/img/braslet-r16-2.jpg" alt="Браслеты противоскольжения R16-R22,5">',
     //Цены, менять можно
     'desc1' => 'R12-R15',
     'price1' => '500рублей/штука',
