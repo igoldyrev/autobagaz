2e5c3fce3cc069ed282961041f7087f176c25635
igoldyrev <goldirev12@gmail.com>
2018-03-15 23:01:49 +0500

    feature / gallery Добавлена функция перевода имени в транслит для генерации ссылок

--- admin/gallery/galleryadd/addpost.php
+++ admin/gallery/galleryadd/addpost.php
@@ -34,6 +34,14 @@ include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/metatagslight.php"); ?>
 
     $namemini = mb_strtolower(str_replace(" ","",$name)); // делаем введенное имя подходящим для имени файла
 
+    //Функция для транслита русских названий в транслит для ссылок
+    function translit($str) {
+        $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+        $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', '', '', '', 'e', 'yu', 'ya');
+        return str_replace($rus, $lat, $str);
+    }
+    $translitname = mb_strtolower(translit($namemini));
+
     $path = '/uploads/gallery/'; // директория для загрузки
 
     $total = count($_FILES['photos']['name']); //счетчик файлов
@@ -78,7 +86,7 @@ include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/metatagslight.php"); ?>
     // Создаем уникальный адрес для страниц
     $dateurl = $date;
     $dateurlmini = str_replace("-", "", $dateurl);
-    $url = $namemini.'-'.$dateurlmini;
+    $url = $translitname.'-'.$dateurlmini;
 
     // Условия записи в БД в зависимости от количества отправленных файлов
     if ($total == '1') {
