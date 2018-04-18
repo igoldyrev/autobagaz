fb90b58f38808c5f8633611b6c7321c90a7f9e18
Ilya Goldyrev <goldirev12@gmail.com>
2018-04-18 19:51:31 +0500

    feature/modal-call Обновлен скрипт формы отправки модального звонка, переписан на чистом js, добавлено управление с клавиатуры

--- src/common.blocks/modal-call/modal-call.js
+++ src/common.blocks/modal-call/modal-call.js
@@ -1,19 +1,41 @@
-$(function() {
-    $('.modal-call__button').click(function()
-    {
-        $('.modal-call').addClass('modal-call--active');
-        $('.modal-call__overlay').addClass('modal-call__overlay--active');
-    });
+'use strict';
+(function () {
+    var ESC_KEYCODE = 27;
+    var modalCall = document.querySelector('.modal-call');
+    var modalCallOverlay = document.querySelector('.modal-call__overlay');
+    var modalCallClose = document.querySelector('.modal-call__close');
+    var modalCallButton = document.querySelector('.modal-call__button');
+    var modalCallInput = document.querySelector('.form__input--call');
 
-    $('.modal-call__close').click(function()
-    {
-        $('.modal-call').removeClass('modal-call--active');
-        $('.modal-call__overlay').removeClass('modal-call__overlay--active');
-    });
+    var onModalCallEscPress = function (evt) {
+        if (evt.keyCode === ESC_KEYCODE) {
+            onModalCallClose();
+        }
+    }
+    
+    var onModalCallClose = function () {
+        modalCall.classList.remove('modal-call--active');
+        modalCallOverlay.classList.remove('modal-call__overlay--active');
+    };
+    
+    var onModalCallInputFocus = function () {
+        document.removeEventListener('keydown', onModalCallEscPress);
+    };
 
-    $('.modal-call__overlay').click(function()
-    {
-        $('.modal-call').removeClass('modal-call--active');
-        $('.modal-call__overlay').removeClass('modal-call__overlay--active');
-    });
-});
+    var onModalCallInputFocusLost = function () {
+        document.addEventListener('keydown', onModalCallEscPress);
+    };
+
+    var onModalCallButtonClick = function () {
+        modalCall.classList.add('modal-call--active');
+        modalCallOverlay.classList.add('modal-call__overlay--active');
+
+        modalCallOverlay.addEventListener('click', onModalCallClose);
+        modalCallClose.addEventListener('click', onModalCallClose);
+        document.addEventListener('keydown', onModalCallEscPress);
+    };
+
+    modalCallButton.addEventListener('click', onModalCallButtonClick);
+    modalCallInput.addEventListener('focus', onModalCallInputFocus);
+    modalCallInput.addEventListener('blur', onModalCallInputFocusLost);
+})();
