53ced3e4978563cb50cd7af49d7374439559cef5
Ilya Goldyrev <goldirev12@gmail.com>
2018-04-27 16:16:21 +0500

    feature/tabs Скрипт для работы с local storage переписан на чистом JS без JQuery

--- build/autobagaz.js
+++ build/autobagaz.js
@@ -82,24 +82,21 @@ module.exports = __webpack_require__(6);
 "use strict";
 
 
-console.log('Это сайт компании Автобагаж');
-console.log('Сейчас сайт работает нормально');
-
-if (window.localStorage) {
+(function () {
+  if (window.localStorage) {
     var elements = document.querySelectorAll('[name]');
 
     for (var i = 0, length = elements.length; i < length; i++) {
-        (function (element) {
-            var name = element.getAttribute('name');
-
-            element.value = localStorage.getItem(name) || '';
-
-            element.onkeyup = function () {
-                localStorage.setItem(name, element.value);
-            };
-        })(elements[i]);
+      (function (element) {
+        var name = element.getAttribute('name');
+        element.value = localStorage.getItem(name) || '';
+        element.onkeyup = function () {
+          localStorage.setItem(name, element.value);
+        };
+      })(elements[i]);
     }
-};
+  }
+})();
 
 /***/ }),
 /* 2 */
