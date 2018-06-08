a0b537feed7e2be548bf7f647ece0ea875e30762
Ilya Goldyrev <goldirev12@gmail.com>
2018-06-08 20:25:36 +0500

    edit-header JS-модуль с базовыми функциями для блока, расположенного над шапкой сайта

---
+++ src/common.blocks/header/top-header.js
@@ -0,0 +1,27 @@
+'use strict';
+(function () {
+  var topHeader = document.querySelector('.top-header');
+  var topHeaderInner = document.querySelector('.top-header__inner');
+  var topHeaderAddress = document.querySelector('.top-header__address');
+  var topHeaderShopAddress = document.querySelectorAll('.top-header__shop-address');
+  var topHeaderShopLast = document.querySelector('.top-header__shop-last');
+  window.onscroll = function () {
+    if (document.body.scrollTop > 60) {
+      topHeader.classList.add('top-header__height-30');
+      topHeaderAddress.classList.add('top-header__hidden');
+      topHeaderInner.classList.add('top-header__center');
+      topHeaderShopAddress.forEach(function (adr) {
+        adr.classList.add('top-header__hidden');
+      });
+      topHeaderShopLast.classList.remove('top-header__shop-last-child');
+    } else {
+      topHeader.classList.remove('top-header__height-30');
+      topHeaderAddress.classList.remove('top-header__hidden');
+      topHeaderInner.classList.remove('top-header__center');
+      topHeaderShopAddress.forEach(function (adr) {
+        adr.classList.remove('top-header__hidden');
+      });
+      topHeaderShopLast.classList.add('top-header__shop-last-child');
+    }
+  };
+})();
