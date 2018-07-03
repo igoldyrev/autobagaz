da8051d1f0369d7f0576ddfd7876f234eb716d25
Ilya Goldyrev <goldirev12@gmail.com>
2018-07-03 23:22:35 +0500

    feature/admin-login Частичный скрипт для входа в админку

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,53 @@
 <?php
+session_start();
 include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/metatagslight.php");
-echo "<title>Войти в админ-панель</title>"; ?>
+echo "<title>Войти в админ-панель</title>";
+
+$dbname = "9082410193_zakaz";
+include($_SERVER["DOCUMENT_ROOT"] . "/backend/connectdb.php");
+
+if (isset($_SESSION['session_username'])) {
+  header('Refresh: 3; Url=intro.php');
+}
+
+if ((isset($_POST['login']) && $_POST['login'] != '') && (isset($_POST['password']) && $_POST['password'] != '')) {
+  $login = $_POST['login'];
+  $password = $_POST['password'];
+
+  $login = htmlspecialchars($login);
+  $password = htmlspecialchars($password);
+
+  $login = urldecode($login);
+  $password = urldecode($password);
+
+  $login = trim($login);
+  $password = trim($password);
+
+  $login = $_REQUEST['login'];
+  $password = $_REQUEST['password'];
+
+  $query = "SELECT user_login, user_password FROM users WHERE user_login =" . $login;
+  mysqli_query($connect, $query);
+  $numrows = mysqli_num_rows($query);
+
+  if ($numrows == 0) {
+    while ($row = mysqli_fetch_assoc($query)) {
+      $dblogin = $row['login'];
+      $dbpassword = $row['password'];
+    }
+
+    if ($login = $dblogin) {
+      $_SESSION['session_username'] = $login;
+      header('Refresh: 3; Url=intro.php');
+    }
+  } else {
+    $message = "Invalid username or password!";
+  }
+} else {
+  $message = "All fields are required!";
+}
+
+?>
 
 <div class="admin__container admin__container--center">
   <h1 class="title title-h1">Войти в админ-панель</h1>
@@ -18,3 +65,7 @@ echo "<title>Войти в админ-панель</title>"; ?>
     <button class="button button__zakaz">Войти</button>
   </form>
 </div>
+
+<?php if (!empty($message)) {
+  echo "<p class=\"error\">" . "MESSAGE: " . $message . "</p>";
+} ?>
