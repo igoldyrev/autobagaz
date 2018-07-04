27faaad328714f2ec71b28d0dfb0aad6f0752664
Ilya Goldyrev <goldirev12@gmail.com>
2018-07-04 22:43:59 +0500

    feature/admin-login Исправлены ошибки и внесены множественные доработки авторизации

--- admin/login.php
+++ admin/login.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
 include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/metatagslight.php");
-echo "<title>Войти в админ-панель</title>";
+echo "<title>Войти</title>";
 
 $dbname = "9082410193_zakaz";
 include($_SERVER["DOCUMENT_ROOT"] . "/backend/connectdb.php");
-
-if (isset($_SESSION['session_username'])) {
-  header('Refresh: 3; Url=intro.php');
-}
 
 if ((isset($_POST['login']) && $_POST['login'] != '') && (isset($_POST['password']) && $_POST['password'] != '')) {
   $login = $_POST['login'];
@@ -26,32 +22,37 @@ if ((isset($_POST['login']) && $_POST['login'] != '') && (isset($_POST['password
   $login = $_REQUEST['login'];
   $password = $_REQUEST['password'];
 
-  $query = "SELECT user_login, user_password FROM users WHERE user_login =" . $login;
-  mysqli_query($connect, $query);
-  $numrows = mysqli_num_rows($query);
+  $query = "SELECT user_login, user_password FROM users WHERE user_login='" . $login . "'";
+  $res = mysqli_query($connect, $query);
+  $numrows = mysqli_num_rows($res);
 
-  if ($numrows == 0) {
-    while ($row = mysqli_fetch_assoc($query)) {
-      $dblogin = $row['login'];
-      $dbpassword = $row['password'];
+  if ($numrows != 0) {
+    while ($row = mysqli_fetch_assoc($res)) {
+      $dblogin = $row['user_login'];
+      $dbpassword = $row['user_password'];
     }
 
     if ($login = $dblogin) {
       $_SESSION['session_username'] = $login;
-      header('Refresh: 3; Url=intro.php');
+      header('Refresh: 1; Url=intro');
     }
-  } else {
-    $message = "Invalid username or password!";
   }
+//  else {
+//    $message = "Invalid username or password!";
+//  }
 } else {
   $message = "All fields are required!";
 }
 
+//if (isset($_SESSION['session_username'])) {
+//  header('Refresh: 3; Url=intro.php');
+//}
+
 ?>
 
 <div class="admin__container admin__container--center">
-  <h1 class="title title-h1">Войти в админ-панель</h1>
-  <form action="#" method="post" class="form">
+  <h1 class="title title-h1">Войти</h1>
+  <form method="post" class="form">
     <span class="form__label">Введите логин:</span>
     <div class="form__input-wrap">
       <input type="text" name="login" required autofocus class="form__input" placeholder="Введите логин">
