d55861d4faeddb83612726fafc5bfef2679fe094
Ilya Goldyrev <goldirev12@gmail.com>
2018-07-08 01:12:59 +0500

    feature/admin-login Правки верстки и обработчика для страницы входа

--- backend/authorization/login/login.php
+++ backend/authorization/login/login.php
@@ -9,6 +9,7 @@ include($_SERVER["DOCUMENT_ROOT"] . "/backend/connectdb.php");
 if ((isset($_POST['login']) && $_POST['login'] != '') && (isset($_POST['password']) && $_POST['password'] != '')) {
   $login = $_POST['login'];
   $password = $_POST['password'];
+  echo $password;
 
   $login = htmlspecialchars($login);
   $password = htmlspecialchars($password);
@@ -22,53 +23,79 @@ if ((isset($_POST['login']) && $_POST['login'] != '') && (isset($_POST['password
   $login = $_REQUEST['login'];
   $password = $_REQUEST['password'];
 
-  $query = "SELECT user_login, user_password FROM users WHERE user_login='" . $login . "'";
-  $res = mysqli_query($connect, $query);
+  $password = md5($password);
+  echo $password;
+
+  $querylogin = "SELECT user_login FROM users WHERE user_login='" . $login . "'";
+  $res = mysqli_query($connect, $querylogin);
   $numrows = mysqli_num_rows($res);
 
   if ($numrows != 0) {
+
     while ($row = mysqli_fetch_assoc($res)) {
       $dblogin = $row['user_login'];
-      $dbpassword = $row['user_password'];
+      $dbpassword = $row['user_hash'];
     }
 
     if ($login = $dblogin) {
-      $_SESSION['session_username'] = $login;
-      //header('Refresh: 1; Url=intro');
+      if ($password = $dbpassword) {
+        function generateRandomString($length = 32)
+        {
+          $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+          $charactersLength = strlen($characters);
+          $randomString = '';
+          for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+          }
+          return $randomString;
+        }
+
+        $randomUserString = generateRandomString();
+        $queryString = "UPDATE users SET user_string='$randomUserString' WHERE user_login='$login'";
+        $resString = mysqli_query($connect, $queryString);
+
+        $cookieName = "userString";
+        $cookieValue = $randomUserString;
+        setcookie($cookieName, $cookieValue, time() + (21600), "/"); //6h
+
+        $_SESSION['session_username'] = $login;
+        //header('Refresh: 1; Url=intro');
+      } else {
+        $message = "Введенный пароль неверный!";
+      }
     }
+  } else {
+    $message = "Такого пользователя не существует";
   }
-//  else {
-//    $message = "Invalid username or password!";
-//  }
-} else {
-  $message = "All fields are required!";
-}
+} ?>
 
-//if (isset($_SESSION['session_username'])) {
-//  header('Refresh: 3; Url=intro.php');
-//}
-
-?>
-
-<div class="admin__container admin__container--center">
-  <h1 class="title title-h1">Войти</h1>
-  <form method="post" class="form js-authorization-form">
-    <span class="form__label">Введите логин:</span>
-    <div class="form__input-wrap">
-      <input type="text" name="login" required autofocus class="form__input" placeholder="Введите логин">
-      <label for="login" class="form__label--shown">Введите логин</label>
+<div class="auth__wrapper">
+  <div class="auth__content">
+    <h2 class="title title-h2 auth__title">Войти</h2>
+    <div class="auth__form-wrap">
+      <form method="post" class="form auth__form">
+        <span class="form__label">Введите логин:</span>
+        <div class="form__input-wrap">
+          <input type="text" name="login" required autofocus maxlength="30" class="form__input auth__input"
+                 placeholder="Введите логин">
+          <label for="login" class="form__label--shown">Введите логин</label>
+        </div>
+        <span class="form__label">Введите пароль:</span>
+        <div class="form__input-wrap">
+          <input type="password" name="password" required maxlength="30" class="form__input auth__input"
+                 placeholder="Введите пароль">
+          <label for="password" class="form__label--shown">Введите пароль</label>
+        </div>
+        <button class="button button__zakaz auth__input">Войти</button>
+        <p class="text auth__license">Нет аккаунта? <a class="link auth__license" href="/registration">Зарегистрироваться</a>
+        </p>
+      </form>
     </div>
-    <span class="form__label">Введите пароль:</span>
-    <div class="form__input-wrap">
-      <input type="password" name="password" required class="form__input" placeholder="Введите пароль">
-      <label for="password" class="form__label--shown">Введите пароль</label>
-    </div>
-    <button class="button button__zakaz">Войти</button>
-  </form>
-</div>
-
-<div class='good-message good-message--auth js-authorization-form-message'>
-  <p class='text'>Вход успешно выполнен!</p>
+  </div>
+  <?php
+  if (!empty($message)) {
+    echo "<p class='good-message good-message--wrong auth__error'>" . $message . "</p>";
+  } ?>
 </div>
 
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/counters.html"); ?>
