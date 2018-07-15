318b21bfade917ccde92146c638410d1920e584b
Ilya Goldyrev <goldirev12@gmail.com>
2018-07-15 14:55:01 +0500

    feature/admin-login Добавление страницы для настройки аккаунта при регистрации

--- backend/authorization/registration/registration.php
+++ backend/authorization/registration/registration.php
@@ -39,9 +39,54 @@ if ((isset($_POST['login']) && $_POST['login'] != '') && (isset($_POST['email'])
         $sqladd = "INSERT INTO users (user_login, user_email, user_hash, user_rank) VALUES ('$login', '$useremail', '$passwordHash', 3)";
         $resadd = mysqli_query($connect, $sqladd);
 
+        $queryid = "SELECT user_id FROM users WHERE user_login='" . $login . "'";
+        $resid = mysqli_query($connect, $queryid);
+
+        while ($row = mysqli_fetch_assoc($resid)) {
+          $dblogin = $row['user_login'];
+          $dbId = $row['user_id'];
+          $dbRank = $row['user_rank'];
+        }
+
+        $_SESSION['reguser'] = array(
+          'login' => $dblogin,
+          'id' => $dbId,
+        );
+
         mail($useremail, "Регистрация на сайте autobagaz.ru", $registrationmail, "From: autobagaz@yandex.ru \r\n");
         mail("goldirev12@mail.ru", "Новая регистрация на сайте autobagaz.ru", $registrationmailme, "From: autobagaz@yandex.ru \r\n");
         $messageSuccess = 'Вы успешно зарегистрированы!';
+
+        function generateRandomString($length = 32)
+        {
+          $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+          $charactersLength = strlen($characters);
+          $randomString = '';
+          for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+          }
+          return $randomString;
+        }
+
+        $randomUserString = generateRandomString();
+        $queryString = "UPDATE users SET user_string='$randomUserString' WHERE user_login='$login'";
+        $resString = mysqli_query($connect, $queryString);
+
+        $cookieNameLogin = "userLogin";
+        $cookieValueLogin = $login;
+
+        $cookieNameString = "userString";
+        $cookieValueString = $randomUserString;
+
+        setcookie($cookieNameLogin, $cookieValueLogin, time() + (21600), "/"); //6h
+        setcookie($cookieNameString, $cookieValueString, time() + (21600), "/"); //6h
+
+        $_SESSION['user'] = array(
+          'login' => $login,
+          'rank' => $dbRank,
+        );
+
+        header('Refresh: 1; Url=reg-settings');
       } else {
         $message = 'Такой email уже зарегистрирован';
       }
---
+++ backend/authorization/settings.php
@@ -0,0 +1,100 @@
+<?php
+include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/metatagslight.php");
+include($_SERVER["DOCUMENT_ROOT"] . "/src/common.blocks/header/top-header.html");
+echo "<title>Настройки пользователя</title>";
+
+$dbname = "9082410193_zakaz";
+include($_SERVER["DOCUMENT_ROOT"] . "/backend/connectdb.php");
+
+$user_id = $_SESSION['reguser']['id'];
+
+if (!isset($_SESSION["user"])) {
+  echo "<title>Необходимо войти</title>";
+  echo "<div class='good-message good-message--wrong'>";
+  echo "<p class='text'>Необходимо войти в аккаунт</p>";
+  echo "</div>";
+  echo "<div class='notification-wrap'>";
+  echo "<a class='link link--green-hover' href='/login'>Вернуться назад</a>";
+  echo "</div>";
+  header('Refresh: 1; Url=login');
+} else {
+  if ((isset($_POST['name']) && $_POST['name'] != '') && (isset($_POST['surname']) && $_POST['surname'] != '') && (isset($_POST['phone']) && $_POST['phone'] != '')) {
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $birthday = $_POST['birthday'];
+    $auto = $_POST['auto'];
+    $phone = $_POST['phone'];
+
+    $name = htmlspecialchars($name);
+    $surname = htmlspecialchars($surname);
+    $birthday = htmlspecialchars($birthday);
+    $auto = htmlspecialchars($auto);
+    $phone = htmlspecialchars($phone);
+
+    $name = urldecode($name);
+    $surname = urldecode($surname);
+    $birthday = urldecode($birthday);
+    $auto = urldecode($auto);
+    $phone = urldecode($phone);
+
+    $name = trim($name);
+    $surname = trim($surname);
+    $birthday = trim($birthday);
+    $auto = trim($auto);
+    $phone = trim($phone);
+
+    $querysettings = "INSERT INTO users_settings (user_s_id, user_s_name, user_s_surname, user_s_birthday, user_s_auto, user_s_phone) VALUES ('$user_id', '$name', '$surname', '$birthday', '$auto', '$phone')";
+    $ressettings = mysqli_query($connect, $querysettings);
+
+    $messageSuccess = 'Настройка аккаунта завершена!';
+    //header('Refresh: 1; Url=profile');
+
+  }
+}
+
+?>
+
+<div class="auth__wrapper">
+  <div class="auth__content">
+    <h2 class="title title-h2 auth__title">Настройки пользователя</h2>
+    <div class="auth__form-wrap">
+      <form method="post" class="form auth__form">
+        <div class="form__input-wrap">
+          <input type="text" name="name" required autofocus maxlength="100" class="form__input auth__input"
+                 placeholder="Ваше имя">
+          <label for="login" class="form__label--shown">Ваше имя</label>
+        </div>
+        <div class="form__input-wrap">
+          <input type="text" name="surname" required maxlength="100" class="form__input auth__input"
+                 placeholder="Ваша фамилия">
+          <label for="login" class="form__label--shown">Ваша фамилия</label>
+        </div>
+        <div class="form__input-wrap">
+          <input type="text" name="birthday" class="form__input auth__input" placeholder="Дата рождения">
+          <label for="login" class="form__label--shown">Дата рождения</label>
+        </div>
+        <div class="form__input-wrap">
+          <input type="text" name="auto" class="form__input auth__input" placeholder="Ваш автомобиль">
+          <label for="login" class="form__label--shown">Ваш автомобиль</label>
+        </div>
+        <div class="form__input-wrap">
+          <input type="text" name="phone" required maxlength="12" class="form__input auth__input"
+                 placeholder="Ваш телефон">
+          <label for="login" class="form__label--shown">Ваш телефон</label>
+        </div>
+
+        <button class="button button__zakaz auth__input">Перейти в профиль</button>
+      </form>
+    </div>
+  </div>
+  <?php
+  if (!empty($message)) {
+    echo "<p class='good-message good-message--wrong auth__error'>" . $message . "</p>";
+  } elseif (!empty($messageSuccess)) {
+    echo "<p class='good-message auth__error'>" . $messageSuccess . "</p>";
+  } ?>
+</div>
+<div class="auth__footer">
+  <?php include($_SERVER["DOCUMENT_ROOT"] . "/src/common.blocks/footer/footer.html"); ?>
+</div>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/counters.html"); ?>
