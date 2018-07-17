c20894e3f448fa1605c6cf1f84a083cf9e6f7528
Ilya Goldyrev <goldirev12@gmail.com>
2018-07-17 23:11:08 +0500

    feature/admin-login Тестирование шапки для страниц авторизации

--- backend/authorization/registration/registration.php
+++ backend/authorization/registration/registration.php
@@ -1,6 +1,6 @@
 <?php
 include($_SERVER["DOCUMENT_ROOT"] . "/backend/blocks/metatagslight.php");
-include($_SERVER["DOCUMENT_ROOT"] . "/src/common.blocks/header/top-header.html");
+include($_SERVER["DOCUMENT_ROOT"] . "/src/common.blocks/header/top-header-auth.html");
 echo "<title>Зарегистрироваться</title>";
 
 $dbname = "9082410193_zakaz";
