55c422d6d8069de5cb61543ed58cdb474255cb18
Ilya Goldyrev <goldirev12@gmail.com>
2018-08-23 22:25:58 +0500

    edits-2018-08 На формы добавлена ссылка на лицензионное соглашение

--- backend/forms/helpform.php
+++ backend/forms/helpform.php
@@ -1,24 +1,28 @@
 <div class="help-block">
-    <h3 class="title title-h3">У Вас остались вопросы? Задайте их ниже и мы постараемся на них ответить!</h3>
-    <div class="form__container">
-        <form action="/help" method="post" class="form" onsubmit="yaCounter40650914.reachGoal('post_help'); return true">
-            <span class="form__label">Ваше имя:</span>
-            <div class="form__input-wrap">
-                <input type="text" name="name" required class="form__input" placeholder="Введите Ваше имя">
-                <label for="name" class="form__label--shown">Введите Ваше имя</label>
-            </div>
-            <span class="form__label">Ваш телефон:</span>
-            <div class="form__input-wrap">
-                <input type="text" name="phone" required class="form__input" placeholder="Введите номер телефона">
-                <label for="phone" class="form__label--shown">Введите номер телефона</label>
-            </div>
-            <span class="form__label">Ваше сообщение:</span>
-            <div class="form__input-wrap">
-                <textarea name="message" class="form__input form__textarea" placeholder="Введите Ваше сообщение"></textarea>
-                <label for="text" class="form__label--shown">Введите Ваше сообщение</label>
-            </div>
-            <div class="g-recaptcha" data-sitekey="6LenJjcUAAAAALv0jIybM3O45CnsEYa4X58EZ1uH"></div>
-            <button class="button button__zakaz button__zakaz--help">Задать вопрос</button>
-        </form>
-    </div>
+  <h3 class="title title-h3">У Вас остались вопросы? Задайте их ниже и мы на них ответим!</h3>
+  <div class="form__container">
+    <form action="/help" method="post" class="form" onsubmit="yaCounter40650914.reachGoal('post_help'); return true">
+      <span class="form__label">Ваше имя:</span>
+      <div class="form__input-wrap">
+        <input type="text" name="name" required class="form__input" placeholder="Введите Ваше имя">
+        <label for="name" class="form__label--shown">Введите Ваше имя</label>
+      </div>
+      <span class="form__label">Ваш телефон:</span>
+      <div class="form__input-wrap">
+        <input type="text" name="phone" required class="form__input" placeholder="Введите номер телефона">
+        <label for="phone" class="form__label--shown">Введите номер телефона</label>
+      </div>
+      <span class="form__label">Ваше сообщение:</span>
+      <div class="form__input-wrap">
+        <textarea name="message" class="form__input form__textarea" placeholder="Введите Ваше сообщение"></textarea>
+        <label for="text" class="form__label--shown">Введите Ваше сообщение</label>
+      </div>
+      <div class="g-recaptcha" data-sitekey="6LenJjcUAAAAALv0jIybM3O45CnsEYa4X58EZ1uH"></div>
+      <button class="button button__zakaz button__zakaz--help">Задать вопрос</button>
+      <p class="text auth__license">Нажимая кнопку «Задать вопрос» вы соглашаетесь с <a
+          class="link auth__license" target="_blank" href="/user-agreement">Пользовательским соглашением</a> и даете
+        согласие на
+        обработку персональных данных.</p>
+    </form>
+  </div>
 </div>
