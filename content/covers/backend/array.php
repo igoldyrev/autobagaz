41762a8e94856ed825437d1a54f0637e2220da9d
Ilya Goldyrev <goldirev12@gmail.com>
2018-12-03 19:19:37 +0500

    feature / covers Данные для чехлов Cayman и Орегон Ромб

---
+++ content/covers/backend/array.php
@@ -0,0 +1,104 @@
+<?php
+
+// Коллекции чехлов
+$covers = [
+	[
+		'id' => '1',
+		'name' => 'CAYMAN',
+		'img' => '<<img class="img good__img" src="/content/covers/img/Cayman_Black_Alkantara_Face.jpg" alt="Чехлы Cayman">',
+	],
+	[
+		'id' => '2',
+		'name' => 'Орегон',
+		'img' => '',
+	],
+	[
+		'id' => '3',
+		'name' => 'Тренд',
+		'img' => '',
+	],
+];
+
+$cayman = [
+	[
+		'id' => '1',
+		'name' => 'CAYMAN черный-белый, экокожа',
+		'desc1' => 'Вариант1: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'desc2' => 'Вариант2: Экокожа - боковые части, Алькантара - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Cayman_Black_White_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Cayman_Black_White_Front.jpg"',
+		'price' => '5000 рублей',
+	],
+	[
+		'id' => '2',
+		'name' => 'CAYMAN черный-красный, экокожа',
+		'desc1' => 'Вариант1: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'desc2' => 'Вариант2: Экокожа - боковые части, Алькантара - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Cayman_Black_Red_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Cayman_Black_Red_Front.jpg"',
+		'price' => '5000 рублей',
+	],
+	[
+		'id' => '3',
+		'name' => 'CAYMAN черный-серый, экокожа',
+		'desc1' => 'Вариант1: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'desc2' => 'Вариант2: Экокожа - боковые части, Алькантара - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Cayman_Black_Gray_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Cayman_Black_Gray_Front.jpg"',
+		'price' => '5000 рублей',
+	],
+	[
+		'id' => '4',
+		'name' => 'CAYMAN черный-синий, экокожа',
+		'desc1' => 'Вариант1: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'desc2' => 'Вариант2: Экокожа - боковые части, Алькантара - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Cayman_Black_Blue_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Cayman_Black_Blue_Front.jpg"',
+		'price' => '5000 рублей',
+	],
+	[
+		'id' => '5',
+		'name' => 'CAYMAN черный, экокожа',
+		'desc1' => 'Вариант1: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'desc2' => 'Вариант2: Экокожа - боковые части, Алькантара - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Cayman_Black_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Cayman_Black_Front.jpg"',
+		'price' => '5000 рублей',
+	],
+	[
+		'id' => '6',
+		'name' => 'CAYMAN черный, экокожа-алькантара',
+		'desc1' => 'Вариант1: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'desc2' => 'Вариант2: Экокожа - боковые части, Алькантара - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Cayman_Black_Alkantara_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Cayman_Black_Alkantara_Front.jpg"',
+		'price' => '5000 рублей',
+	],
+];
+
+$oregonromb = [
+	[
+		'id' => '1',
+		'name' => 'Орегон Ромб черный-оранжевый, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Oregon_Romb_Black_Orange_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Oregon_Romb_Black_Orange_Front.jpg"',
+		'price' => '4800 рублей',
+	],
+	[
+		'id' => '2',
+		'name' => 'Орегон Ромб черный-серый, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Oregon_Romb_Black_Gray_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Oregon_Romb_Black_Gray_Front.jpg"',
+		'price' => '4800 рублей',
+	],
+	[
+		'id' => '3',
+		'name' => 'Орегон Ромб черный, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Oregon_Romb_Black_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Oregon_Romb_Black_Front.jpg"',
+		'price' => '4800 рублей',
+	],
+];
