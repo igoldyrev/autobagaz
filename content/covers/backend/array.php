3b05e9681c29e13403529762af69f6876dfd1e68
Ilya Goldyrev <goldirev12@gmail.com>
2018-12-05 19:56:04 +0500

    feature / covers Данные для чехлов Тренд

--- content/covers/backend/array.php
+++ content/covers/backend/array.php
@@ -196,3 +196,89 @@ $oregonuniversal = [
 		'price' => '3100 рублей',
 	],
 ];
+
+$trendromb = [
+	[
+		'id' => '1',
+		'name' => 'Тренд "Двойной ромб", черный-белый, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Romb_Black_White_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Romb_Black_White_Front.jpg"',
+		'price' => '5500 рублей',
+	],
+	[
+		'id' => '2',
+		'name' => 'Тренд "Двойной ромб", черный-синий, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Romb_Black_Blue_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Romb_Black_Blue_Front.jpg"',
+		'price' => '5500 рублей',
+	],
+	[
+		'id' => '3',
+		'name' => 'Тренд "Двойной ромб", черный, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Romb_Black_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Romb_Black_Front.jpg"',
+		'price' => '5500 рублей',
+	],
+	[
+		'id' => '4',
+		'name' => 'Тренд "Двойной ромб", черный-серый, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Romb_Black_Gray_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Romb_Black_Gray_Front.jpg"',
+		'price' => '5500 рублей',
+	],
+	[
+		'id' => '5',
+		'name' => 'Тренд "Двойной ромб", черный-красный, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Romb_Black_Red_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Romb_Black_Red_Front.jpg"',
+		'price' => '5500 рублей',
+	],
+	[
+		'id' => '6',
+		'name' => 'Тренд "Двойной ромб", шоколадный, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Romb_Choko_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Romb_Choko_Front.jpg"',
+		'price' => '5500 рублей',
+	],
+];
+
+$trend = [
+	[
+		'id' => '1',
+		'name' => 'Тренд, черный-серая алькантара, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Black_Gray_Alk_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Black_Gray_Alk_Front.jpg"',
+		'price' => '5600 рублей',
+	],
+	[
+		'id' => '2',
+		'name' => 'Тренд, черный-белый, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Black_White_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Black_White_Front.jpg"',
+		'price' => '5600 рублей',
+	],
+	[
+		'id' => '3',
+		'name' => 'Тренд, черный-паприка, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Black_Paprika_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Black_Paprika_Front.jpg"',
+		'price' => '5600 рублей',
+	],
+	[
+		'id' => '4',
+		'name' => 'Тренд, черный-серый, экокожа',
+		'desc1' => 'Используемые материалы: Экокожа - боковые части, Перфорированная экокожа - центральная часть',
+		'img1' => '<img class="img product__img" src="/content/covers/img/Trend_Black_Gray_Face.jpg"',
+		'img2' => '<img class="img product__img product__img--small" src="/content/covers/img/Trend_Black_Gray_Front.jpg"',
+		'price' => '5600 рублей',
+	],
+];
