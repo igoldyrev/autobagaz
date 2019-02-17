3e333bd98b4ed8ed9eead71344c1ffc40ba4a274
Ilya Goldyrev <goldirev12@gmail.com>
2019-02-17 19:11:29 +0500

    feature/expiditions Частично обновленные массивы с информацией по экспедиционным багажникам

---
+++ content/expiditions/backend/arrays.php
@@ -0,0 +1,144 @@
+<?php
+$cheviniva = [
+  [
+    'id' => 'B0103',
+    'name' => 'Багажник экспедиционный на Niva Chevrolet 1600х1200х120 с сеткой и креплениями на крышу',
+    'img1' => '/content/expiditions/img/Chevrole_Niva/B01.03/B01.03-01.jpg',
+    'img2' => '/content/expiditions/img/Chevrole_Niva/B01.03/B01.03-02.jpg',
+    'img3' => '/content/expiditions/img/Chevrole_Niva/B01.03/B01.03-03.jpg',
+    'img4' => '/content/expiditions/img/Chevrole_Niva/B01.03/B01.03-04.jpg',
+    'price' => '18 900 рублей',
+  ],
+  [
+    'id' => 'B0101',
+    'name' => 'Багажник экспедиционный на Niva Chevrolet (корзина) 1600х1200х120',
+    'img1' => '/content/expiditions/img/Chevrole_Niva/B01.01/B01.01-01.jpg',
+    'img2' => '/content/expiditions/img/Chevrole_Niva/B01.01/B01.01-02.jpg',
+    'price' => '16 000 рублей',
+  ],
+  [
+    'id' => 'B5205',
+    'name' => 'Багажник экспедиционный на Niva Chevrolet 1100х1100х100 с креплениями на рейлинги',
+    'img1' => '/content/expiditions/img/Chevrole_Niva/B52.05/B52.05-01.jpg',
+    'img2' => '/content/expiditions/img/Chevrole_Niva/B52.05/B52.05-02.jpg',
+    'img3' => '/content/expiditions/img/Chevrole_Niva/B52.05/B52.05-03.jpg',
+    'price' => '14 500 рублей',
+  ],
+  [
+    'id' => 'B0102',
+    'name' => 'Багажник экспедиционный на Niva Chevrolet 1600х1200х120 с креплениями на крышу',
+    'img1' => '/content/expiditions/img/Chevrole_Niva/B01.02/B01.02-01.jpg',
+    'img2' => '/content/expiditions/img/Chevrole_Niva/B01.02/B01.02-02.jpg',
+    'price' => '16 650 рублей',
+  ],
+];
+
+$greatwall = [
+  [
+    'id' => 'B8703',
+    'name' => 'Багажник экспедиционный на GW Hover 3 1900х1100х120 с сеткой и креплениями на крышу',
+    'img1' => '/content/expiditions/img/Great_Wall/B87.03/B87.03-01.jpg',
+    'img2' => '/content/expiditions/img/Great_Wall/B87.03/B87.03-02.jpg',
+    'img3' => '/content/expiditions/img/Great_Wall/B87.03/B87.03-03.jpg',
+    'img4' => '/content/expiditions/img/Great_Wall/B87.03/B87.03-04.jpg',
+    'img5' => '/content/expiditions/img/Great_Wall/B87.03/B87.03-05.jpg',
+    'img6' => '/content/expiditions/img/Great_Wall/B87.03/B87.03-06.jpg',
+    'img7' => '/content/expiditions/img/Great_Wall/B87.03/B87.03-07.jpg',
+    'price' => '20 100 рублей',
+  ],
+  [
+    'id' => 'B8903',
+    'name' => 'Багажник экспедиционный на GW Hover 3 1900х1100х120 с сеткой и креплениями на крышу',
+    'img1' => '/content/expiditions/img/Great_Wall/B89.03/B89.03-01.jpg',
+    'img2' => '/content/expiditions/img/Great_Wall/B89.03/B89.03-02.jpg',
+    'price' => '20 500 рублей',
+  ],
+  [
+    'id' => 'B8802',
+    'name' => 'Багажник экспедиционный на GW Hover 3 1900х1100х120 с креплениями на крышу',
+    'img1' => '/content/expiditions/img/Great_Wall/B88.02/B88.02-01.jpg',
+    'price' => '19 100 рублей',
+  ],
+];
+
+$isuzu = [
+  [
+    'id' => 'B11305',
+    'name' => 'Багажник экспедиционный на ISUZU Trooper 2001 1900х1200х120 с сеткой и креплениями',
+    'img1' => '/content/expiditions/img/ISUZU/B113.05/B113.05-01.jpg',
+    'img2' => '/content/expiditions/img/ISUZU/B113.05/B113.05-02.jpg',
+    'img3' => '/content/expiditions/img/ISUZU/B113.05/B113.05-03.jpg',
+    'price' => '22 700 рублей',
+  ],
+  [
+    'id' => 'B10205',
+    'name' => 'Багажник экспедиционный на ISUZU Bighorn 97 1900х1200х120 с сеткой и креплениями на рейлинги',
+    'img1' => '/content/expiditions/img/ISUZU/B102.05/B102.05-01.jpg',
+    'img2' => '/content/expiditions/img/ISUZU/B102.05/B102.05-02.jpg',
+    'price' => '23 500 рублей',
+  ],
+];
+
+$jeep = [
+  [
+    'id' => 'B2501',
+    'name' => 'Багажник экспедиционный (корзина) на JEEP 1700х1150х120 с сеткой',
+    'img1' => '/content/expiditions/img/JEEP/B25.01/B25.01-01.jpg',
+    'price' => '16 700 рублей',
+  ],
+  [
+    'id' => 'B0303',
+    'name' => 'Багажник экспедиционный на JEEP Cherokee XJ 1700х1300х120 с сеткой и креплениями на водостоки',
+    'img1' => '/content/expiditions/img/JEEP/B03.03/B03.03-01.jpg',
+    'img2' => '/content/expiditions/img/JEEP/B03.03/B03.03-02.jpg',
+    'price' => '18 600 рублей',
+  ],
+  [
+    'id' => 'B25',
+    'name' => 'Багажник экспедиционный на Jeep Cherokee ZJ (корзина) 1900 х 1100',
+    'img1' => '/content/expiditions/img/JEEP/B25/B25-01.jpg',
+    'price' => '15 500 рублей',
+  ],
+];
+
+$kia = [
+  [
+    'id' => 'B10805',
+    'name' => 'Багажник экспедиционный Б108.05 на KIA Sorento (2005) 1650х1150х120 с сеткой, на рейлинги',
+    'img1' => '/content/expiditions/img/KIA/B108.05/B108.05-01.jpg',
+    'img2' => '/content/expiditions/img/KIA/B108.05/B108.05-02.jpg',
+    'price' => '19 900 рублей',
+  ]
+];
+
+$landrover = [
+  [
+    'id' => 'B28',
+    'name' => 'Багажник экспедиционный на Land Rover Discovery II (корзина) 2150х1450x150',
+    'img1' => '/content/expiditions/img/Land_Rover/B28/B28-01.jpg',
+    'price' => '18 700 рублей',
+  ],
+  [
+    'id' => 'B1101',
+    'name' => 'Багажник экспедиционный на Land Rover Defender 110 (корзина) 2500x1400x150',
+    'img1' => '/content/expiditions/img/Land_Rover/B11.01/B11.01-01.jpg',
+    'price' => '21 500 рублей',
+  ],
+  [
+    'id' => 'B2803',
+    'name' => 'Багажник экспедиционный на Land Rover Discovery II 2150х1450x120 с сеткой и креплениями',
+    'img1' => '/content/expiditions/img/Land_Rover/B28.03/B28.03-01.jpg',
+    'img2' => '/content/expiditions/img/Land_Rover/B28.03/B28.03-02.jpg',
+    'img3' => '/content/expiditions/img/Land_Rover/B28.03/B28.03-03.jpg',
+    'price' => '22 400 рублей',
+  ],
+  [
+    'id' => 'B1103',
+    'name' => 'Багажник экспедиционный на Land Rover Defender 110 2500x1400x120 с сеткой и креплениями',
+    'img1' => '/content/expiditions/img/Land_Rover/B11.03/B28.03-01.jpg',
+    'img2' => '/content/expiditions/img/Land_Rover/B11.03/B28.03-02.jpg',
+    'img3' => '/content/expiditions/img/Land_Rover/B11.03/B28.03-03.jpg',
+    'img4' => '/content/expiditions/img/Land_Rover/B11.03/B28.03-04.jpg',
+    'price' => '27 200 рублей',
+  ],
+];
