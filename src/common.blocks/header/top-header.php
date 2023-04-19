421fdc72aba71df6d2c55ff7b1733d0dfe53d93b
Ilya Goldyrev <goldirev12@gmail.com>
2023-04-19 12:50:23 +0500

    Скрыт магазин на Лебедева 34

--- src/common.blocks/header/top-header.php
+++ src/common.blocks/header/top-header.php
@@ -8,17 +8,17 @@
       <a onclick="yaCounter40650914.reachGoal('call_phone'); return true" class="top-header__shop-phone"
          href="tel:+7 342 288 99 29">+7 342 288 99 29</a>
     </div>
-    <div class="top-header__shop">
+    <div class="top-header__shop top-header__shop-last top-header__shop-last-child">
       <a class="top-header__shop-address" href="/contacts#vasileva" title="Посмотреть на карте">г.Пермь, ул. Васильева 1/3</a>
       <a onclick="yaCounter40650914.reachGoal('call_phone'); return true" class="top-header__shop-phone"
          href="tel:+7 342 288 99 69">+7 342 288 99 69</a>
     </div>
-    <div class="top-header__shop top-header__shop-last top-header__shop-last-child">
+    <!-- <div class="top-header__shop top-header__shop-last top-header__shop-last-child">
       <a class="top-header__shop-address" href="/contacts#lebedeva" title="Посмотреть на карте">г.Пермь, ул.
         Лебедева, 34</a>
       <a onclick="yaCounter40650914.reachGoal('call_phone'); return true" class="top-header__shop-phone"
          href="tel:+73422889939">+7 342 288 99 39 </a>
-    </div>
+    </div> -->
     <?php
     if (!empty($_COOKIE['userString'])) {
       //echo '<a href="/profile?id=' . $_COOKIE['userId'] . '" class="auth__link">Профиль</a>';
